bd8f4aa9fff01978a171abae0e94718734a8e3bc
sebastientromp <sebastien.tromp@gmail.com>
2018-02-07 17:20:59 +0100

    Implement basic real-time notifications

--- Files/src/js/components/real-time-notifications.component.ts
+++ Files/src/js/components/real-time-notifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { Http } from "@angular/http";
 
 import { Events } from '../services/events.service';
 
@@ -8,13 +9,42 @@ declare var overwolf: any;
 	selector: 'real-time-notifications',
 	styleUrls: [`../../css/component/real-time-notifications.component.scss`],
 	template: `
-		<div class="real-time-notifications">Fake notification</div>
+		<div class="real-time-notifications" *ngIf="notifications">{{notifications[currentNotificationIndex].text}}</div>
 	`,
 })
 
 export class RealTimeNotificationsComponent {
 
-	constructor(private events: Events) {
+	private readonly URL = 'https://iej6sdi74d.execute-api.us-west-2.amazonaws.com/prod/get-status';
 
+	private notifications: string[];
+	private currentNotificationIndex = 0;
+
+	constructor(private http: Http) {
+		console.log('init real time notifications');
+
+		this.getStatus();
+		setInterval(() => {
+			this.getStatus();
+		}, 60 * 1000)
+
+		setInterval(() => {
+			if (this.notifications) {
+				this.currentNotificationIndex = (this.currentNotificationIndex + 1) % this.notifications.length;
+			}
+		}, 15 * 1000)
+	}
+
+	private getStatus() {
+		console.log('getting status');
+		this.http.get(this.URL).subscribe(
+			(res: any) => {
+				if (res.ok) {
+					let status = JSON.parse(res._body);
+					this.notifications = status[0].status;
+					console.log('received status', status, this.notifications);
+				}
+			}
+		)
 	}
 }
