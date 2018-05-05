7dd5043c7af34c4fbc4fc6aef08f42374640fd6c
sebastientromp <sebastien.tromp@gmail.com>
2018-05-05 12:55:21 +0200

    Update welcome status based on important notifications

--- Files/src/js/components/home/home-screen-info-text.component.ts
+++ Files/src/js/components/home/home-screen-info-text.component.ts
@@ -1,4 +1,7 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { RealTimeNotificationService } from '../../services/real-time-notifications.service';
+
 
 const HEARTHSTONE_GAME_ID = 9898;
 
@@ -7,16 +10,17 @@ declare var overwolf: any;
 @Component({
 	selector: 'home-screen-info-text',
 	styleUrls: [`../../../css/component/home/home-screen-info-text.component.scss`],
+	encapsulation: ViewEncapsulation.None,
 	template: `
 		<div class="home-screen-info">
 			<div class="hearthlore">
-				<i class="i-35 gold-theme left">
+				<i class="i-35 gold-theme left" *ngIf="!importantAnnouncement">
 					<svg class="svg-icon-fill">
 						<use xlink:href="/Files/assets/svg/sprite.svg#title_decor"/>
 					</svg>
 				</i>
-				<span class="title">Welcome to Hearthlore</span>
-				<i class="i-35 gold-theme right">
+				<span class="title">{{importantAnnouncement ? 'Update ' : 'Welcome to Hearthlore'}}</span>
+				<i class="i-35 gold-theme right" *ngIf="!importantAnnouncement">
 					<svg class="svg-icon-fill">
 						<use xlink:href="/Files/assets/svg/sprite.svg#title_decor"/>
 					</svg>
@@ -29,7 +33,15 @@ declare var overwolf: any;
 
 export class HomeScreenInfoTextComponent implements OnInit {
 
+	private currentNotificationIndex = 0;
+	private notifications: any[];
+	private importantAnnouncement;
+
 	private status;
+
+	constructor(private notificationService: RealTimeNotificationService) {
+		setTimeout(() => this.refresh(), 1000);
+	}
 
 	ngOnInit() {
 		overwolf.games.getRunningGameInfo((res: any) => {
@@ -41,5 +53,19 @@ export class HomeScreenInfoTextComponent implements OnInit {
 				this.status = "No Hearthstone session detected.<br /> Choose an ability:";
 			}
 		});
+	}
+
+	private refresh() {
+		this.notifications = this.notificationService.notifications;
+		if (this.notifications) {
+			for (let i = 0; i < this.notifications.length; i++) {
+				let currentNotif = this.notifications[this.currentNotificationIndex];
+				console.log('current notif', currentNotif);
+				if (currentNotif.important) {
+					this.importantAnnouncement = currentNotif.text;
+					this.status = this.importantAnnouncement;
+				}
+			}
+		}
 	}
 }
