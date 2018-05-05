5cea5def1aecb89687037d1d894d1f3f7d082372
sebastientromp <sebastien.tromp@gmail.com>
2018-05-05 09:55:25 +0200

    Add warning sign when real time status is a warning

--- Files/src/js/components/real-time-notifications.component.ts
+++ Files/src/js/components/real-time-notifications.component.ts
@@ -7,9 +7,19 @@ declare var overwolf: any;
 
 @Component({
 	selector: 'real-time-notifications',
-	styleUrls: [`../../css/component/real-time-notifications.component.scss`],
+	styleUrls: [
+		`../../css/global/components-global.scss`,
+		`../../css/component/real-time-notifications.component.scss`,
+	],
 	template: `
-		<div class="real-time-notifications" *ngIf="notifications">{{notifications[currentNotificationIndex].text}}</div>
+		<div class="real-time-notifications {{notifications[currentNotificationIndex].type}}" *ngIf="notifications">
+			<i class="i-30 error-theme warning-icon">
+				<svg class="svg-icon-fill">
+					<use xlink:href="/Files/assets/svg/sprite.svg#error"/>
+				</svg>
+			</i>
+			<span>{{notifications[currentNotificationIndex].text}}</span>
+		</div>
 	`,
 })
 
