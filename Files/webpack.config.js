0d64a821c1782a59e6f4c267d55d8ee1b4f48bc2
sebastientromp <sebastien.tromp@gmail.com>
2018-07-10 15:53:19 +0200

    Clean webpack config

--- Files/webpack.config.js
+++ Files/webpack.config.js
@@ -30,12 +30,6 @@ module.exports = function(env, argv) {
     optimization: {
       splitChunks: {
         cacheGroups: {
-          // commons: {
-          //   chunks: "initial",
-          //   minChunks: 2,
-          //   maxInitialRequests: 5, // The default limit is too small to showcase the effect
-          //   minSize: 0 // This is example is too small to create commons chunks
-          // },
           vendor: {
             test: /node_modules/,
             chunks: "initial",
@@ -54,7 +48,6 @@ module.exports = function(env, argv) {
     watch: true,
 
     watchOptions: {
-      // poll: true,
       ignored: ['node_modules']
     },
 
