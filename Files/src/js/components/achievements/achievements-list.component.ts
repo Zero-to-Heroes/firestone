7b3e949a3c1022af6556910d082a3609f1192f6d
sebastientromp <sebastien.tromp@gmail.com>
2018-07-19 17:21:11 +0200

    Add achievements pagination

--- Files/src/js/components/achievements/achievements-list.component.ts
+++ Files/src/js/components/achievements/achievements-list.component.ts
@@ -11,9 +11,26 @@ declare var overwolf: any;
 	encapsulation: ViewEncapsulation.None,
 	template: `
 		<div class="achievements-container">
-			<ul class="achievements-list" *ngIf="achievements">
-				<li *ngFor="let achievement of achievements">
+			<ul class="achievements-list" *ngIf="activeAchievements">
+				<li *ngFor="let achievement of activeAchievements">
 					<achievement-view [achievement]="achievement">/</achievement-view>
+				</li>
+			</ul>
+			<ul class="pagination" *ngIf="numberOfPages > 1">
+				<li class="arrow previous" (click)="previousPage()" [ngClass]="currentPage == 0 ? 'disabled' : ''">
+					<i class="i-30">
+						<svg class="svg-icon-fill">
+							<use xlink:href="/Files/assets/svg/sprite.svg#arrow"/>
+						</svg>
+					</i>
+				</li>
+				<li *ngFor="let page of pages" [ngClass]="currentPage == page ? 'active' : ''" (click)="goToPage(page)">{{page + 1}}</li>
+				<li class="arrow next" (click)="nextPage()" [ngClass]="currentPage >= numberOfPages - 1 ? 'disabled' : ''">
+					<i class="i-30">
+						<svg class="svg-icon-fill">
+							<use xlink:href="/Files/assets/svg/sprite.svg#arrow"/>
+						</svg>
+					</i>
 				</li>
 			</ul>
 		</div>
@@ -21,14 +38,54 @@ declare var overwolf: any;
 })
 export class AchievementsListComponent {
 
+	readonly MAX_ACHIEVEMENTS_DISPLAYED_PER_PAGE = 18;
+
 	achievements: Achievement[];
+	activeAchievements: Achievement[];
 	_achievementSet: AchievementSet;
+	numberOfPages: number;
+	currentPage = 0;
+	pages: number[] = [];
+
+	private achievementsIndexRangeStart = 0;
 
 	@Input('achievementSet') set achievementSet(achievementSet: AchievementSet) {
+		this.currentPage = 0;
 		this._achievementSet = achievementSet;
 	}
 
 	@Input('achievementsList') set achievementsList(achievementsList: Achievement[]) {
-		this.achievements = achievementsList;
+		this.currentPage = 0;
+		this.achievements = achievementsList || [];
+		this.updateShownAchievements();
+	}
+
+
+	previousPage() {
+		this.currentPage = Math.max(0, this.currentPage - 1);
+		this.updateShownAchievements();
+	}
+
+	nextPage() {
+		this.currentPage = Math.min(this.numberOfPages - 1, this.currentPage + 1);
+		this.updateShownAchievements();
+	}
+
+	goToPage(page: number) {
+		this.currentPage = page;
+		this.updateShownAchievements();
+	}
+
+	private updateShownAchievements() {
+		// console.log('updating card list', this._cardList);
+		this.achievementsIndexRangeStart = this.currentPage * this.MAX_ACHIEVEMENTS_DISPLAYED_PER_PAGE;
+		this.pages = [];
+		this.numberOfPages = Math.ceil(this.achievements.length / this.MAX_ACHIEVEMENTS_DISPLAYED_PER_PAGE);
+		for (let i = 0; i < this.numberOfPages; i++) {
+			this.pages.push(i);
+		}
+		this.activeAchievements = this.achievements.slice(
+			this.achievementsIndexRangeStart,
+			this.achievementsIndexRangeStart + this.MAX_ACHIEVEMENTS_DISPLAYED_PER_PAGE);
 	}
 }
