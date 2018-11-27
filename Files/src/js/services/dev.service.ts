ea5580037aad70f865842ab3b9bfe05f47ec8b8e
sebastientromp <sebastien.tromp@gmail.com>
2018-11-27 10:23:57 +0100

    technical: add dev option to add several replayInfos

--- Files/src/js/services/dev.service.ts
+++ Files/src/js/services/dev.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { AchievementsMonitor } from './achievement/achievements-monitor.service';
 import { Achievement } from '../models/achievement';
+import { AchievementsStorageService } from './achievement/achievements-storage.service';
+import { CompletedAchievement } from '../models/completed-achievement';
 
 declare var overwolf: any;
 
 @Injectable()
 export class DevService {
 
-	constructor(private achievementMonitor: AchievementsMonitor) {
+	constructor(private achievementMonitor: AchievementsMonitor, private storage: AchievementsStorageService) {
 		this.addTestCommands();
 	}
 
@@ -27,6 +29,28 @@ export class DevService {
 		}
 		window['addAchievementHistory'] = () => {
 			this.achievementMonitor.storeNewAchievementHistory(achievement, 1);
+		}
+		window['addReplayInfos'] = async () => {
+			const achievements = await this.storage.loadAchievements();
+			const achievement = achievements
+					.filter((ach) => ach.replayInfo && ach.replayInfo.length > 0)
+					[0];
+			const newReplays = [
+				...achievement.replayInfo, 
+				achievement.replayInfo[0],
+				achievement.replayInfo[0],
+				achievement.replayInfo[0],
+				achievement.replayInfo[0],
+				achievement.replayInfo[0],
+				achievement.replayInfo[0],
+			];
+			const newAchievement = new CompletedAchievement(
+				achievement.id,
+				achievement.numberOfCompletions,
+				newReplays
+			);
+			const updated = await this.storage.saveAchievement(newAchievement);
+			console.log('added lots of replays to achievement', updated.id, updated);
 		}
 	}
 }
