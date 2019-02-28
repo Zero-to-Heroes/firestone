3474db6610c775f6c0f02c1a19b514afeabb3023
sebastientromp <sebastien.tromp@gmail.com>
2019-02-28 07:41:32 +0100

    log: improve error reporting in case of exception in plugin

--- Files/src/js/services/debug.service.ts
+++ Files/src/js/services/debug.service.ts
@@ -9,9 +9,7 @@ export class DebugService {
 		let debugMode = false;
 		console.log = this.override(console.log, debugMode);
 		console.warn = this.override(console.warn, debugMode);
-		// console.error = this.override(console.error, debugMode);
-
-		// this.addTestCommands();
+		console.error = this.overrideError(console.error, debugMode);
 	}
 
 	private override(oldConsoleLogFunc: any, debugMode: boolean) {
@@ -39,18 +37,29 @@ export class DebugService {
 		return oldConsoleLogFunc;
 	}
 
-	// private addTestCommands() {
-	// 	overwolf.settings.registerHotKey(
-	// 		"test_login",
-	// 		(result) => {
-	// 			console.log('hotkey pressed')
-	// 			if (result.status === 'success') {
-	// 				this.sync.login("sebastien.tromp@gmail.com", "sQuAlL007!");
-	// 			}
-	// 			else {
-	// 				console.log('error registering hotkey', result);
-	// 			}
-	// 		}
-	// 	)
-	// }
+	private overrideError(oldConsoleLogFunc: any, debugMode: boolean) {
+		if (debugMode) {
+			return function() {
+				let argsString = "";
+				for (let i = 0; i < arguments.length; i++) {
+					let cache = [];
+					argsString += (JSON.stringify(arguments[i], function(key, value) {
+						if (typeof value === 'object' && value !== null) {
+							if (cache.indexOf(value) !== -1) {
+								// Circular reference found, discard key
+								return;
+							}
+							// Store value in our collection
+							cache.push(value);
+						}
+						return value;
+					}) || '') + ' | ';
+					cache = null; // Enable garbage collection + " | "
+				}
+				oldConsoleLogFunc.apply(console, [argsString]);
+				oldConsoleLogFunc.apply(console, arguments);
+			};
+		}
+		return oldConsoleLogFunc;
+	}
 }
