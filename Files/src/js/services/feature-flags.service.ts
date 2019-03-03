bed52174225c4b7e820e37f147d6f447687eb773
sebastientromp <sebastien.tromp@gmail.com>
2019-03-03 11:26:09 +0100

    refactor: remove member field in feature-flags.service.ts

--- Files/src/js/services/feature-flags.service.ts
+++ Files/src/js/services/feature-flags.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FeatureFlags {
-
-    private _decktracker = false;
-
+    
 	public decktracker() {
-        return this._decktracker;
+        return false;
     }
 
 	public achievements() {
