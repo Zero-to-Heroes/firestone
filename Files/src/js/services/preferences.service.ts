6978e5bef599c08d1f908e964c2d4e780cfd9e06
sebastientromp <sebastien.tromp@gmail.com>
2019-03-05 18:34:17 +0100

    technical(sett): emit event when decktracker prefs are changed

--- Files/src/js/services/preferences.service.ts
+++ Files/src/js/services/preferences.service.ts
@@ -1,11 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { GenericIndexedDbService } from './generic-indexed-db.service';
 import { Preferences } from '../models/preferences';
+
+declare var overwolf: any;
 
 @Injectable()
 export class PreferencesService {
 
+    public static readonly DECKTRACKER_OVERLAY_DISPLAY = 'DECKTRACKER_OVERLAY_DISPLAY';
+
+    private preferencesEventBus = new EventEmitter<any>();
+    
 	constructor(private indexedDb: GenericIndexedDbService) {
+        // It will create one per window that uses the service, but we don't really care 
+        // We just have to always use the one from the MainWindow
+		window['preferencesEventBus'] = this.preferencesEventBus;
     }
     
     public getPreferences(): Promise<Preferences> {
@@ -39,29 +48,47 @@ export class PreferencesService {
 	public async setDecktrackerShowArena(pref: boolean) {
         const prefs = await this.getPreferences();
         const newPrefs = { ...prefs, decktrackerShowArena: pref} as Preferences;
-        this.savePreferences(newPrefs);
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
     }
     
 	public async setDecktrackerShowRanked(pref: boolean) {
         const prefs = await this.getPreferences();
         const newPrefs = { ...prefs, decktrackerShowRanked: pref} as Preferences;
-        this.savePreferences(newPrefs);
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
 	}
     
 	public async setDecktrackerShowTavernBrawl(pref: boolean) {
         const prefs = await this.getPreferences();
         const newPrefs = { ...prefs, decktrackerShowTavernBrawl: pref} as Preferences;
-        this.savePreferences(newPrefs);
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
 	}
     
 	public async setDecktrackerShowPractice(pref: boolean) {
         const prefs = await this.getPreferences();
         const newPrefs = { ...prefs, decktrackerShowPractice: pref} as Preferences;
-        this.savePreferences(newPrefs);
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
 	}
     
-    private savePreferences(userPrefs: Preferences) {
+	public async setDecktrackerShowFriendly(pref: boolean) {
+        const prefs = await this.getPreferences();
+        const newPrefs = { ...prefs, decktrackerShowFriendly: pref} as Preferences;
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
+	}
+    
+	public async setDecktrackerShowCasual(pref: boolean) {
+        const prefs = await this.getPreferences();
+        const newPrefs = { ...prefs, decktrackerShowCasual: pref} as Preferences;
+        this.savePreferences(newPrefs, PreferencesService.DECKTRACKER_OVERLAY_DISPLAY);
+	}
+    
+    private savePreferences(userPrefs: Preferences, eventName: string = null) {
         this.indexedDb.saveUserPreferences(userPrefs);
-        // TODO: send event to prefs event bus
+        if (eventName) {
+            const eventBus: EventEmitter<any> = overwolf.windows.getMainWindow().preferencesEventBus;
+            eventBus.next({
+                name: eventName,
+                preferences: userPrefs
+            });
+        }
     }
 }
