8c5b1c7678e139bf49ede7074331db6ad5c651fa
sebastientromp <sebastien.tromp@gmail.com>
2019-03-11 11:18:53 +0100

    ux(trck): replace close button with a settings button, skinned as a close

--- Files/src/js/components/decktracker/decktracker-title-bar.component.ts
+++ Files/src/js/components/decktracker/decktracker-title-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, AfterViewInit, ElementRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 declare var overwolf: any;
 
@@ -16,13 +17,33 @@ declare var overwolf: any;
 				</svg>
 			</i>
 			<div class="controls">
-				<control-settings [settingsApp]="'decktracker'" [windowId]="windowId"></control-settings>
+				<control-settings 
+					[settingsApp]="'decktracker'" 
+					[shouldMoveSettingsWindow]="false"
+					[windowId]="windowId">
+				</control-settings>
+				<control-settings 
+					id="fakeClose"
+					[settingsApp]="'decktracker'" 
+					[shouldMoveSettingsWindow]="false"
+					[windowId]="windowId">
+				</control-settings>
 			</div>
 		</div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DeckTrackerTitleBarComponent {
+export class DeckTrackerTitleBarComponent implements AfterViewInit {
 
 	@Input() windowId: string;
+
+	constructor(private el: ElementRef) { }
+
+	ngAfterViewInit() {
+		let singleEl: HTMLElement = this.el.nativeElement.querySelector('#fakeClose button');
+		singleEl.innerHTML = `
+			<svg class="svg-icon-fill">
+				<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/Files/assets/svg/sprite.svg#window-control_close"></use>
+			</svg>`;
+	}
 }
