c88b85c7805b18fd2ec92d2d81ea7dd7e8203b53
sebastientromp <sebastien.tromp@gmail.com>
2019-03-12 17:03:19 +0100

    ux(trck): add collapsible sections in overlay

--- Files/src/js/components/decktracker/deck-zone.component.ts
+++ Files/src/js/components/decktracker/deck-zone.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, ChangeDetectorRef } from '@angular/core';
 import { DeckZone } from '../../models/decktracker/view/deck-zone';
 import { DeckCard } from '../../models/decktracker/deck-card';
 
@@ -11,12 +11,17 @@ import { DeckCard } from '../../models/decktracker/deck-card';
 	],
 	template: `
 		<div class="deck-zone {{className}}">
-			<div class="zone-name-container" *ngIf="zoneName">
+			<div class="zone-name-container" *ngIf="zoneName" (click)="toggleZone()">
 				<span class="zone-name">{{zoneName}}</span>
 				<!-- TODO: collapse caret -->
+				<i class="collapse-caret {{open ? 'open' : 'close'}}">
+					<svg class="svg-icon-fill">
+						<use xlink:href="/Files/assets/svg/sprite.svg#collapse_caret"/>
+					</svg>
+				</i>
 				<div class="dim-overlay" *ngIf="activeTooltip"></div>
 			</div>
-			<ul class="card-list">
+			<ul class="card-list" *ngIf="open">
 				<li *ngFor="let card of cards; trackBy: trackCard">
 					<deck-card [activeTooltip]="activeTooltip" [card]="card"></deck-card>
 				</li>
@@ -31,6 +36,11 @@ export class DeckZoneComponent {
 	className: string;
 	zoneName: string;
 	cards: ReadonlyArray<DeckCard>;
+	open: boolean = true;
+
+	constructor(private cdr: ChangeDetectorRef) {
+
+	}
 
 	@Input('zone') set zone(zone: DeckZone) {
 		this.className = zone.id;
@@ -38,6 +48,11 @@ export class DeckZoneComponent {
 		this.cards = zone.sortingFunction
 				? [...zone.cards].sort(zone.sortingFunction)
 				: zone.cards;
+	}
+
+	toggleZone() {
+		this.open = !this.open;
+		this.cdr.detectChanges();
 	}
 
 	trackCard(index, card: DeckCard) {
