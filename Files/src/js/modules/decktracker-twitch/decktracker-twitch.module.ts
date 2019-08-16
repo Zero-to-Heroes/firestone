4916147bb4aae4ef356d77e89847b114f1d9c312
sebastientromp <sebastien.tromp@gmail.com>
2019-08-16 15:55:39 +0200

    decktracker-twitch: updated sentry DSN

--- Files/src/js/modules/decktracker-twitch/decktracker-twitch.module.ts
+++ Files/src/js/modules/decktracker-twitch/decktracker-twitch.module.ts
@@ -1,24 +1,23 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { AngularResizedEventModule } from 'angular-resize-event';
-
-import { SharedModule } from '../shared/shared.module';
 import { init } from '@sentry/browser';
-import { Events } from '../../services/events.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedDeckTrackerModule } from '../shared-decktracker/shared-dectracker.module';
-import { DeckTrackerOverlayStandaloneComponent } from '../../components/decktracker/overlay/twitch/decktracker-overlay-standalone.component';
-import { HttpClientModule } from '@angular/common/http';
-import { DeckTrackerOverlayContainerComponent } from '../../components/decktracker/overlay/twitch/decktracker-overlay-container.component.ts';
-import { DeckTrackerTwitchTitleBarComponent } from '../../components/decktracker/overlay/twitch/decktracker-twitch-title-bar.component';
-import { StateMouseOverComponent } from '../../components/decktracker/overlay/twitch/state-mouse-over.component';
-import { EmptyCardComponent } from '../../components/decktracker/overlay/twitch/empty-card.component';
+import { AngularResizedEventModule } from 'angular-resize-event';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { DeckTrackerOverlayContainerComponent } from '../../components/decktracker/overlay/twitch/decktracker-overlay-container.component.ts';
+import { DeckTrackerOverlayStandaloneComponent } from '../../components/decktracker/overlay/twitch/decktracker-overlay-standalone.component';
+import { DeckTrackerTwitchTitleBarComponent } from '../../components/decktracker/overlay/twitch/decktracker-twitch-title-bar.component';
+import { EmptyCardComponent } from '../../components/decktracker/overlay/twitch/empty-card.component';
+import { StateMouseOverComponent } from '../../components/decktracker/overlay/twitch/state-mouse-over.component';
+import { Events } from '../../services/events.service';
+import { SharedDeckTrackerModule } from '../shared-decktracker/shared-dectracker.module';
+import { SharedModule } from '../shared/shared.module';
 
 init({
-	dsn: 'https://53b0813bb66246ae90c60442d05efefe@sentry.io/1338840',
+	dsn: 'https://8513431ee4344eb3b6626df27552c70e@sentry.io/1532694', // Different project for the extension
 	enabled: process.env.NODE_ENV === 'production',
 	release: process.env.APP_VERSION,
 });
