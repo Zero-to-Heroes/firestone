f727f42fd4873c0329fefd2696f4a41c3a3a2f0c
sebastientromp <sebastien.tromp@gmail.com>
2019-09-03 15:03:08 +0200

    achievements: update the displayName of the amazing plays achievements

--- Files/src/js/models/achievement/raw-achievement.ts
+++ Files/src/js/models/achievement/raw-achievement.ts
@@ -3,12 +3,12 @@ import { RawRequirement } from './raw-requirement';
 export interface RawAchievement {
 	readonly id: string;
 	readonly type: string;
-	readonly name: string;
+	readonly name: string; // The name of the achievement in the achievements view
 	readonly root?: boolean;
 	readonly priority?: number;
-	readonly displayName: string;
+	readonly displayName: string; // The header to display on the achievement notification
+	/** @deprecated */ readonly text?: string;
 	readonly emptyText?: string;
-	readonly text: string;
 	readonly completedText?: string;
 	readonly displayCardId: string;
 	readonly displayCardType: string;
