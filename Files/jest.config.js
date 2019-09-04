47840878cbee9f4160663a3ccb0acc469a9870fc
sebastientromp <sebastien.tromp@gmail.com>
2019-09-04 14:39:26 +0200

    tweak jest config

--- Files/jest.config.js
+++ Files/jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	roots: ['<rootDir>/src', '<rootDir>/test'],
 	setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+	testEnvironment: 'node',
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 		'^.+\\.txt$': 'jest-text-transformer',
 		'^.+\\.log$': 'jest-text-transformer',
 	},
-	testMatch: ['<rootDir>/**/*.test.ts'],
+	testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/test/**/*.test.ts'],
 	transformIgnorePatterns: ['<rootDir>/node_modules/'],
 	verbose: true,
 };
