5946128ab8286051c9f2bc3b1c2b699931cc7919
sebastientromp <sebastien.tromp@gmail.com>
2019-09-13 17:47:01 +0200

    achievements: add The Void and So Quiet achievements in the UI

--- Files/src/js/services/achievement/achievement-sets/deckbuilding/deckbuilding.ts
+++ Files/src/js/services/achievement/achievement-sets/deckbuilding/deckbuilding.ts
@@ -5,7 +5,13 @@ export class DeckbuildingSetProvider extends GenericSetProvider {
 		super(
 			'deckbuilding',
 			'Deckbuilding',
-			['deckbuilding_win_classic', 'deckbuilding_win_epic', 'deckbuilding_win_lifesteal'],
+			[
+				'deckbuilding_win_classic',
+				'deckbuilding_win_epic',
+				'deckbuilding_win_lifesteal',
+				'deckbuilding_win_so_quiet',
+				'deckbuilding_win_the_void',
+			],
 			'deckbuilding',
 		);
 	}
