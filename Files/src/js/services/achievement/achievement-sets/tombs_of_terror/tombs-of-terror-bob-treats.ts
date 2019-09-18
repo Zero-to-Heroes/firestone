0b1b7c503b87cf739d53b8ae44302529410c3d27
sebastientromp <sebastien.tromp@gmail.com>
2019-09-18 14:18:05 +0200

    achievements: update ToT achievements

--- Files/src/js/services/achievement/achievement-sets/tombs_of_terror/tombs-of-terror-bob-treats.ts
+++ Files/src/js/services/achievement/achievement-sets/tombs_of_terror/tombs-of-terror-bob-treats.ts
@@ -8,8 +8,11 @@ export class TombsOfTerrorBobTreatsSetProvider extends GenericSetProvider {
 			[
 				'tombs_of_terror_bob_treat_DALA_904',
 				'tombs_of_terror_bob_treat_DALA_906',
+				'tombs_of_terror_bob_treat_DALA_907',
 				'tombs_of_terror_bob_treat_DALA_908',
 				'tombs_of_terror_bob_treat_DALA_909',
+				'tombs_of_terror_bob_treat_DALA_910',
+				'tombs_of_terror_bob_treat_DALA_912',
 				'tombs_of_terror_bob_treat_DALA_913',
 				'tombs_of_terror_bob_treat_ULDA_601',
 				'tombs_of_terror_bob_treat_ULDA_602',
