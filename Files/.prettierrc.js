41881f971da256c945889bc4a6b43bb3d904393a
sebastientromp <sebastien.tromp@gmail.com>
2019-09-21 09:25:54 +0200

    update tsconfig and prettier config

--- Files/.prettierrc.js
+++ Files/.prettierrc.js
@@ -2,9 +2,8 @@ module.exports = {
 	semi: true,
 	trailingComma: 'all',
 	singleQuote: true,
-	printWidth: 120,
 	tabWidth: 4,
 	useTabs: true,
-	printWidth: 140,
+	printWidth: 120,
 	quoteProps: 'preserve', // Needed for angular templates, like ngClass
 };
