04adb4f425f75411ed50003496767dc69cf4111e
sebastientromp <sebastien.tromp@gmail.com>
2019-10-10 07:09:24 +0200

    achievements: add Free Speech achievement

--- Files/src/js/services/achievement/achievement-sets/deckbuilding/deckbuilding.ts
+++ Files/src/js/services/achievement/achievement-sets/deckbuilding/deckbuilding.ts
@@ -11,6 +11,7 @@ export class DeckbuildingSetProvider extends GenericSetProvider {
 				'deckbuilding_win_complex',
 				'deckbuilding_win_epic',
 				'deckbuilding_win_fragile_nature',
+				'deckbuilding_win_free_speech',
 				'deckbuilding_win_lifesteal',
 				'deckbuilding_win_lone_survivor',
 				'deckbuilding_win_minion',
