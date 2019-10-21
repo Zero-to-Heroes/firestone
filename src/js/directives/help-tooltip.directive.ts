22fb9501af0f5d548956c60dd5d9fb4b228871dd
sebastientromp <sebastien.tromp@gmail.com>
2019-10-21 16:59:19 +0200

    UI: add formatting options to help tooltip component

--- src/js/directives/help-tooltip.directive.ts
+++ src/js/directives/help-tooltip.directive.ts
@@ -1,6 +1,6 @@
-import { Directive, ElementRef, HostListener, Input, OnInit, ComponentRef } from '@angular/core';
-import { OverlayRef, Overlay, OverlayPositionBuilder } from '@angular/cdk/overlay';
+import { Overlay, OverlayPositionBuilder, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
+import { ComponentRef, Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 import { HelpTooltipComponent } from '../components/tooltip/help-tooltip.component';
 
 @Directive({
@@ -9,6 +9,7 @@ import { HelpTooltipComponent } from '../components/tooltip/help-tooltip.compone
 // See https://blog.angularindepth.com/building-tooltips-for-angular-3cdaac16d138
 export class HelpTooltipDirective implements OnInit {
 	@Input('helpTooltip') text = '';
+	@Input('helpTooltipPosition') position: 'bottom' | 'right' = 'bottom';
 
 	private tooltipPortal;
 	private overlayRef: OverlayRef;
@@ -20,29 +21,45 @@ export class HelpTooltipDirective implements OnInit {
 	) {}
 
 	ngOnInit() {
-		const positionStrategy = this.overlayPositionBuilder
-			// Create position attached to the elementRef
-			.flexibleConnectedTo(this.elementRef)
-			// Describe how to connect overlay to the elementRef
-			// Means, attach overlay's center bottom point to the
-			// top center point of the elementRef.
-			.withPositions([
-				{
-					originX: 'center',
-					originY: 'top',
-					overlayX: 'center',
-					overlayY: 'bottom',
-				},
-			])
-			// Fallback if element is close to the top
-			.withPositions([
-				{
-					originX: 'center',
-					originY: 'bottom',
-					overlayX: 'center',
-					overlayY: 'top',
-				},
-			]);
+		console.log('targeting tooltip help element', this.position);
+		const target = this.elementRef.nativeElement.querySelector('[helpTooltipTarget') || this.elementRef;
+
+		const positionStrategy =
+			this.position === 'bottom'
+				? this.overlayPositionBuilder
+						// Create position attached to the elementRef
+						.flexibleConnectedTo(target)
+						// Describe how to connect overlay to the elementRef
+						// Means, attach overlay's center bottom point to the
+						// top center point of the elementRef.
+						.withPositions([
+							{
+								originX: 'center',
+								originY: 'top',
+								overlayX: 'center',
+								overlayY: 'bottom',
+							},
+						])
+						// Fallback if element is close to the top
+						.withPositions([
+							{
+								originX: 'center',
+								originY: 'bottom',
+								overlayX: 'center',
+								overlayY: 'top',
+							},
+						])
+				: this.overlayPositionBuilder
+						// Create position attached to the elementRef
+						.flexibleConnectedTo(target)
+						.withPositions([
+							{
+								originX: 'end',
+								originY: 'center',
+								overlayX: 'start',
+								overlayY: 'center',
+							},
+						]);
 		// Connect position strategy
 		this.overlayRef = this.overlay.create({ positionStrategy });
 	}
