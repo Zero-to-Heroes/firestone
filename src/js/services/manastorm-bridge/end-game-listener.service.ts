4ee611e9952450edd6309c6bfab152fa12497fb5
sebastientromp <sebastien.tromp@gmail.com>
2019-10-31 14:15:57 +0100

    DEV: build empty review on MATCH_START event
    Instead of relying on GEP event

--- src/js/services/manastorm-bridge/end-game-listener.service.ts
+++ src/js/services/manastorm-bridge/end-game-listener.service.ts
@@ -31,25 +31,27 @@ export class EndGameListenerService {
 		this.events.on(Events.NEW_GAME_ID).subscribe(async event => {
 			this.logger.debug('[manastorm-bridge] Received new game id event', event);
 			this.currentGameId = event.data[0];
-			this.currentReviewId = await this.replayUpload.createEmptyReview();
-
-			const info = {
-				type: 'new-empty-review',
-				reviewId: this.currentReviewId,
-			};
-			this.events.broadcast(Events.REVIEW_INITIALIZED, info);
 		});
 		this.gameEvents.allEvents.subscribe(async (gameEvent: GameEvent) => {
 			switch (gameEvent.type) {
-				case 'LOCAL_PLAYER':
+				case GameEvent.GAME_START:
+					this.logger.debug('[manastorm-bridge] Creating empty review');
+					this.currentReviewId = await this.replayUpload.createEmptyReview();
+					const info = {
+						type: 'new-empty-review',
+						reviewId: this.currentReviewId,
+					};
+					this.events.broadcast(Events.REVIEW_INITIALIZED, info);
+					break;
+				case GameEvent.LOCAL_PLAYER:
 					this.currentDeckstring = this.deckService.currentDeck.deckstring;
 					this.currentDeckname = this.deckService.currentDeck.name;
 					break;
-				case 'MATCH_METADATA':
+				case GameEvent.MATCH_METADATA:
 					this.currentBuildNumber = gameEvent.additionalData.metaData.BuildNumber;
 					this.currentScenarioId = gameEvent.additionalData.metaData.ScenarioID;
 					break;
-				case 'GAME_END':
+				case GameEvent.GAME_END:
 					this.logger.debug('[manastorm-bridge] end game, uploading?');
 					await this.endGameUploader.upload(
 						gameEvent,
