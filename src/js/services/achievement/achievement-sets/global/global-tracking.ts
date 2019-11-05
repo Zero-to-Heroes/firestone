3912f027108f66f5c8014eeddb3c85e78a649415
sebastientromp <sebastien.tromp@gmail.com>
2019-11-05 14:23:44 +0100

    CONTENT: add more Glocal achievements

--- src/js/services/achievement/achievement-sets/global/global-tracking.ts
+++ src/js/services/achievement/achievement-sets/global/global-tracking.ts
@@ -5,7 +5,12 @@ export class GlobalTrackingSetProvider extends GenericSetProvider {
 		super(
 			'global_tracking',
 			'Tracking',
-			['global_damage_to_enemy_heroes', 'global_enemy_minions_dead', 'global_mana_spent'],
+			[
+				'global_damage_to_enemy_heroes',
+				'global_enemy_minions_dead',
+				'global_mana_spent',
+				'global_total_duration',
+			],
 			'tracking',
 		);
 	}
