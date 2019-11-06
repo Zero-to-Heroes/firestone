b02966ed78840d2a9e79c8d76038cab0f6d59d9b
sebastientromp <sebastien.tromp@gmail.com>
2019-11-06 16:28:33 +0100

    CONTENT: add Minions achievements for Battlegrounds

--- src/js/services/achievement/achievement-sets/battlegrounds/battlegrounds-hero-powers.ts
+++ src/js/services/achievement/achievement-sets/battlegrounds/battlegrounds-hero-powers.ts
@@ -2,6 +2,6 @@ import { GenericSetProvider } from '../generic-set-provider';
 
 export class BattlegroundsHeroPowersSetProvider extends GenericSetProvider {
 	constructor() {
-		super('battlegrounds_hero_powers', 'Hero Powers', [], 'hero_powers');
+		super('battlegrounds_hero_powers', 'Hero Powers', [], 'achievements_teammate');
 	}
 }
