30bb86b51395f32ec8f2e8e5c28ca9b292c527f0
sebastientromp <sebastien.tromp@gmail.com>
2019-11-10 23:44:43 +0100

    CONTENT: add some more Battlegrounds Tracking achievements and update name and image

--- src/js/services/achievement/achievement-sets/battlegrounds/battlegrounds-tracking.ts
+++ src/js/services/achievement/achievement-sets/battlegrounds/battlegrounds-tracking.ts
@@ -8,7 +8,11 @@ export class BattlegroundsTrackingSetProvider extends GenericSetProvider {
 			[
 				'battlegrounds_coin_spent',
 				'battlegrounds_damage_to_enemy_heroes',
+				'battlegrounds_eliminate_other_players',
 				'battlegrounds_enemy_minions_dead',
+				'battlegrounds_lock_all',
+				'battlegrounds_reroll_tavern',
+				'battlegrounds_tavern_upgrade',
 				'battlegrounds_total_duration',
 				'battlegrounds_total_match',
 			],
