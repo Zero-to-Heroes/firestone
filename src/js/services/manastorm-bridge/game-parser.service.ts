e8e4cf2243a517e2a95fedf18cc80cc61c5f64d5
sebastientromp <sebastien.tromp@gmail.com>
2019-11-13 14:15:33 +0100

    DEV: fix exception when extracting Battlegrounds match before upload

--- src/js/services/manastorm-bridge/game-parser.service.ts
+++ src/js/services/manastorm-bridge/game-parser.service.ts
@@ -110,7 +110,10 @@ export class GameParserService {
 		game.player = gamePlayers[0];
 		game.opponent = gamePlayers[1];
 
-		game.title = game.player.name.replace('"', '') + ' vs ' + game.opponent.name.replace('"', '');
+		game.title =
+			game.player && game.opponent
+				? game.player.name.replace('"', '') + ' vs ' + game.opponent.name.replace('"', '')
+				: 'Unknown matchup';
 
 		game.result = this.extractResult(replayXml, mainPlayerEntityId);
 		// console.log('parsed game');
