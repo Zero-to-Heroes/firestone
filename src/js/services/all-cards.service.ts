53c6d38b0425dd5b226e079a385a3805804cf340
sebastientromp <sebastien.tromp@gmail.com>
2019-12-02 10:22:13 +0100

    BUG: fix issue when trying to load cards db from remote

--- src/js/services/all-cards.service.ts
+++ src/js/services/all-cards.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
+import { of } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
 import { ReferenceCard } from '../models/reference-cards/reference-card';
 import { Set, SetCard } from '../models/set';
 
@@ -58,31 +60,42 @@ export class AllCardsService {
 		this.logger.debug('[all-cards] initializing card db');
 		return new Promise<void>((resolve, reject) => {
 			if (this.allCards) {
-				this.logger.debug('[all-cards] already loaded all cards', this.allCards);
+				this.logger.debug('[all-cards] already loaded all cards');
 				resolve();
 				return;
 			}
-			this.http.get('./cards.json').subscribe(
-				(result: any[]) => {
-					this.logger.debug('[all-cards] retrieved all cards locally');
-					this.allCards = result;
-					resolve();
-				},
-				error => {
-					this.logger.debug('[all-cards] Could not retrieve cards locally, getting them from CDN', error);
-					this.http.get(CARDS_CDN_URL).subscribe(
-						(result: any[]) => {
-							this.logger.debug('[all-cards] retrieved all cards from CDN');
+			this.logger.debug('[all-cards] retrieving local cards');
+			this.http
+				.get('./cards.json')
+				.pipe(
+					timeout(200),
+					catchError((error, caught) => {
+						this.logger.debug('[all-cards] Could not retrieve cards locally, getting them from CDN', error);
+						this.http.get(CARDS_CDN_URL).subscribe(
+							(result: any[]) => {
+								this.logger.debug('[all-cards] retrieved all cards from CDN');
+								this.allCards = result;
+								resolve();
+								return of(null);
+							},
+							error => {
+								this.logger.debug('[all-cards] Could not retrieve cards from CDN', error);
+								return of(null);
+							},
+						);
+						return of(null);
+					}),
+				)
+				.subscribe(
+					(result: any[]) => {
+						if (result) {
+							this.logger.debug('[all-cards] retrieved all cards locally');
 							this.allCards = result;
 							resolve();
-						},
-						error => {
-							this.logger.debug('[all-cards] Could not retrieve cards from CDN', error);
-							reject();
-						},
-					);
-				},
-			);
+						}
+					},
+					error => {},
+				);
 		});
 	}
 
