e8da084730b432cc00233639bb5ec61b91dba34d
sebastientromp <sebastien.tromp@gmail.com>
2019-12-04 22:35:15 +0100

    UI: fix some display issues in the decktracker panel

--- src/js/components/decktracker/main/decktracker-decks.component.ts
+++ src/js/components/decktracker/main/decktracker-decks.component.ts
@@ -20,7 +20,7 @@ import { OverwolfService } from '../../../services/overwolf.service';
 			<section class="empty-state" *ngIf="!_decks || _decks.length === 0">
 				<div class="state-container">
 					<i class="i-236X165">
-						<svg class="svg-icon-fill">
+						<svg>
 							<use xlink:href="/Files/assets/svg/sprite.svg#empty_state_tracker" />
 						</svg>
 					</i>
