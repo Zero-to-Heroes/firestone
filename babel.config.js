7c89903c825fc0d51be72d1bf0e01818ff2efa88
sebastientromp <sebastien.tromp@gmail.com>
2020-01-14 14:40:57 +0100

    DEV: add babel transpilation to support lodash-es in tests

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+// Needed for lodash-es support in tests
+module.exports = {
+	// https://stackoverflow.com/questions/58096872/react-jest-test-fails-to-run-with-ts-jest-unexpected-token-on-imported-file
+	presets: [['@babel/preset-env']],
+	plugins: [],
+};
--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,18 @@ module.exports = {
 	setupFilesAfterEnv: ['<rootDir>/test/setup.js', 'jest-extended'],
 	testEnvironment: 'node',
 	transform: {
+		// https://stackoverflow.com/questions/58096872/react-jest-test-fails-to-run-with-ts-jest-unexpected-token-on-imported-file
+		'^.+\\.jsx?$': 'babel-jest',
 		'^.+\\.tsx?$': 'ts-jest',
 		'^.+\\.txt$': 'jest-text-transformer',
 		'^.+\\.log$': 'jest-text-transformer',
 	},
-	testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/test/**/*.test.ts'],
-	transformIgnorePatterns: ['<rootDir>/node_modules/'],
+	globals: {
+		'ts-jest': {
+			'tsConfig': '<rootDir>/tsconfig.json',
+		},
+	},
+	testMatch: ['**/*.test.ts'],
+	transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\](?!lodash-es/).+\\.js$'],
 	verbose: true,
 };
