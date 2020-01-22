1a33cd72e52463e3f22a0f45c571b656db4b6e38
sebastientromp <sebastien.tromp@gmail.com>
2020-01-22 10:23:32 +0100

    CONTENT: add more achievements for Galakrond

--- src/js/services/achievement/achievement-sets/galadrond-awakening/galakrond-category.ts
+++ src/js/services/achievement/achievement-sets/galadrond-awakening/galakrond-category.ts
@@ -1,10 +1,14 @@
 import { AchievementCategoryProvider } from '../achievement-category-provider';
 import { GalakrondTrackingSetProvider } from './galakrond-tracking';
+import { GalakrondDominationSetProvider } from './galakrond-domination';
+import { GalakrondAmazingPlaysSetProvider } from './galakrond-amazing-plays';
 
 export class GalakrondCategoryProvider extends AchievementCategoryProvider {
 	constructor() {
 		super('galakrond', 'Galakrond', 'galakrond', [
 			new GalakrondTrackingSetProvider(),
+			new GalakrondDominationSetProvider(),
+			new GalakrondAmazingPlaysSetProvider()
 		]);
 	}
 }
