7a6fc545d83bc23dcce11da27f23ecd6eb5c0eb5
sebastientromp <sebastien.tromp@gmail.com>
2020-02-03 16:18:10 +0100

    DEV: remove integration tests for now, as they slow the build by a lot

---
+++ integration-tests/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	tabWidth: 4,
+	useTabs: true,
+	printWidth: 120,
+	quoteProps: 'preserve', // Needed for angular templates, like ngClass
+};
