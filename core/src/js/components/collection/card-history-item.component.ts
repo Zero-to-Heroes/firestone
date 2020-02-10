7df3bc5cedea15e71953bd1cce5a865bcf25e4aa
sebastientromp <sebastien.tromp@gmail.com>
2020-02-10 11:09:37 +0100

    DEV: fix NPE when forcing reload of collection window

--- core/src/js/components/collection/card-history-item.component.ts
+++ core/src/js/components/collection/card-history-item.component.ts
@@ -8,12 +8,12 @@ import {
 	Input,
 	ViewRef,
 } from '@angular/core';
+import { AllCardsService } from '@firestone-hs/replay-parser';
 import { CardHistory } from '../../models/card-history';
 import { Events } from '../../services/events.service';
 import { ShowCardDetailsEvent } from '../../services/mainwindow/store/events/collection/show-card-details-event';
 import { MainWindowStoreEvent } from '../../services/mainwindow/store/events/main-window-store-event';
 import { OverwolfService } from '../../services/overwolf.service';
-import { AllCardsService } from '@firestone-hs/replay-parser';
 
 declare var amplitude;
 
@@ -79,6 +79,18 @@ export class CardHistoryItemComponent implements AfterViewInit {
 		this.cardId = history.cardId;
 		this.newCard = history.isNewCard;
 		this.relevantCount = history.relevantCount;
+		this.updateCard(history);
+	}
+
+	private updateCard(history: CardHistory, retriesLeft = 5) {
+		if (retriesLeft < 0) {
+			return;
+		}
+		if (!this.cards.getCards() || this.cards.getCards().length === 0) {
+			// Cards service not initialized yet
+			setTimeout(() => this.updateCard(history, retriesLeft - 1), 500);
+			return;
+		}
 		const dbCard = this.cards.getCard(history.cardId);
 		// console.log('history for card', history.cardId, dbCard.name, dbCard, history);
 		this.rarityImg = `/Files/assets/images/rarity/rarity-${dbCard.rarity || 'free'}.png`;
