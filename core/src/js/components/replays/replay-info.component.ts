4387fdccc16fcd12aaa48050b851b66a9de3ff0f
sebastientromp <sebastien.tromp@gmail.com>
2020-02-12 15:51:03 +0100

    UI: add tooltip on icons in the replays view

--- core/src/js/components/replays/replay-info.component.ts
+++ core/src/js/components/replays/replay-info.component.ts
@@ -1,13 +1,13 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { GALAKROND_EVIL, GALAKROND_EXPLORER } from '@firestone-hs/reference-data';
+import { AllCardsService } from '@firestone-hs/replay-parser';
 import { NGXLogger } from 'ngx-logger';
-import { StatGameFormatType } from '../../models/mainwindow/stats/stat-game-format.type';
+import { GameStat } from '../../models/mainwindow/stats/game-stat';
 import { StatGameModeType } from '../../models/mainwindow/stats/stat-game-mode.type';
 import { MainWindowStoreEvent } from '../../services/mainwindow/store/events/main-window-store-event';
 import { ShowReplayEvent } from '../../services/mainwindow/store/events/replays/show-replay-event';
 import { OverwolfService } from '../../services/overwolf.service';
-import { GameStat } from '../../models/mainwindow/stats/game-stat';
-import { ScenarioId, GALAKROND_ALL, GALAKROND_EXPLORER, GALAKROND_EVIL } from '@firestone-hs/reference-data';
 
 @Component({
 	selector: 'replay-info',
@@ -15,14 +15,19 @@ import { ScenarioId, GALAKROND_ALL, GALAKROND_EXPLORER, GALAKROND_EVIL } from '@
 	template: `
 		<div class="replay-info {{ gameMode }}">
 			<div class="group mode">
-				<img class="player-rank" [src]="playerRankImage" />
+				<img class="player-rank" [src]="playerRankImage" [helpTooltip]="playerRankImageTooltip" />
 				<div class="rank-text" *ngIf="rankText">{{ rankText }}</div>
 			</div>
 
 			<div class="group player-images">
-				<img class="player-class player" [src]="playerClassImage" />
+				<img class="player-class player" [src]="playerClassImage" [helpTooltip]="playerClassTooltip" />
 				<div class="vs" *ngIf="opponentClassImage">VS</div>
-				<img class="player-class opponent" [src]="opponentClassImage" *ngIf="opponentClassImage" />
+				<img
+					class="player-class opponent"
+					[src]="opponentClassImage"
+					[helpTooltip]="opponentClassTooltip"
+					*ngIf="opponentClassImage"
+				/>
 				<div class="player-name opponent" *ngIf="opponentName">{{ opponentName }}</div>
 			</div>
 
@@ -32,7 +37,7 @@ import { ScenarioId, GALAKROND_ALL, GALAKROND_EXPLORER, GALAKROND_EVIL } from '@
 			</div>
 
 			<div class="group coin" *ngIf="playCoinIconSvg">
-				<div class="play-coin-icon icon" [innerHTML]="playCoinIconSvg"></div>
+				<div class="play-coin-icon icon" [innerHTML]="playCoinIconSvg" [helpTooltip]="playCoinTooltip"></div>
 			</div>
 
 			<div class="replay" *ngIf="reviewId" (click)="showReplay()">
@@ -51,14 +56,18 @@ export class ReplayInfoComponent implements AfterViewInit {
 	replayInfo: GameStat;
 	gameMode: StatGameModeType;
 	playerRankImage: string;
+	playerRankImageTooltip: string;
 	rankText: string;
 	// deckName: string;
 	playerClassImage: string;
+	playerClassTooltip: string;
 	opponentClassImage: string;
+	opponentClassTooltip: string;
 	opponentName: string;
 	matchResultIconSvg: SafeHtml;
 	result: string;
 	playCoinIconSvg: SafeHtml;
+	playCoinTooltip: SafeHtml;
 	reviewId: string;
 
 	private stateUpdater: EventEmitter<MainWindowStoreEvent>;
@@ -67,14 +76,14 @@ export class ReplayInfoComponent implements AfterViewInit {
 		// this.logger.debug('[deck-replay-info] setting value', value);
 		this.replayInfo = this.replayInfo;
 		this.gameMode = value.gameMode;
-		this.playerRankImage = this.buildPlayerRankImage(value);
+		[this.playerRankImage, this.playerRankImageTooltip] = this.buildPlayerRankImage(value);
 		this.rankText = this.buildRankText(value);
 		// this.deckName = value.playerDeckName || value.playerName;
-		this.playerClassImage = this.buildPlayerClassImage(value, true);
-		this.opponentClassImage = this.buildPlayerClassImage(value, false);
+		[this.playerClassImage, this.playerClassTooltip] = this.buildPlayerClassImage(value, true);
+		[this.opponentClassImage, this.opponentClassTooltip] = this.buildPlayerClassImage(value, false);
 		this.matchResultIconSvg = this.buildMatchResultIconSvg(value);
 		this.result = this.buildMatchResultText(value);
-		this.playCoinIconSvg = this.buildPlayCoinIconSvg(value);
+		[this.playCoinIconSvg, this.playCoinTooltip] = this.buildPlayCoinIconSvg(value);
 		this.reviewId = value.reviewId;
 		this.opponentName = value.gameMode !== 'battlegrounds' ? this.sanitizeName(value.opponentName) : null;
 	}
@@ -83,6 +92,7 @@ export class ReplayInfoComponent implements AfterViewInit {
 		private readonly logger: NGXLogger,
 		private readonly ow: OverwolfService,
 		private readonly sanitizer: DomSanitizer,
+		private readonly allCards: AllCardsService,
 	) {}
 
 	ngAfterViewInit() {
@@ -93,57 +103,74 @@ export class ReplayInfoComponent implements AfterViewInit {
 		this.stateUpdater.next(new ShowReplayEvent(this.reviewId));
 	}
 
-	private buildPlayerRankImage(info: GameStat): string {
+	private buildPlayerRankImage(info: GameStat): [string, string] {
 		const gameMode = info.gameMode;
 		const playerRank = info.playerRank;
 		let rankIcon;
+		let rankIconTooltip;
 		if (gameMode === 'ranked') {
 			const standard = 'standard_ranked';
 			if (playerRank === 'legend') {
 				rankIcon = `${standard}/legend`;
+				rankIconTooltip = 'Legend';
 			} else if (!playerRank || parseInt(playerRank) >= 25) {
 				rankIcon = `${standard}/rank25_small`;
+				rankIconTooltip = 'Rank 25';
 			} else {
-				rankIcon = `${standard}/rank${parseInt(playerRank)}_small`;
+				rankIcon = `${standard}/rank${playerRank}_small`;
+				rankIconTooltip = 'Rank ' + playerRank;
 			}
 		} else if (gameMode === 'battlegrounds') {
 			rankIcon = 'battlegrounds';
+			rankIconTooltip = 'Battlegrounds';
 		} else if (gameMode === 'practice') {
 			if (GALAKROND_EXPLORER.indexOf(info.scenarioId) !== -1) {
 				rankIcon = 'galakrond_explorers';
+				rankIconTooltip = "Galakrond's Awakening - Explorers";
 			} else if (GALAKROND_EVIL.indexOf(info.scenarioId) !== -1) {
 				rankIcon = 'galakrond_evil';
+				rankIconTooltip = "Galakrond's Awakening - E.V.I.L.";
 			} else {
 				rankIcon = 'casual';
+				rankIconTooltip = 'Practice';
 			}
 		} else if (gameMode === 'casual') {
 			rankIcon = 'casual';
+			rankIconTooltip = 'Casual';
 		} else if (gameMode === 'friendly') {
 			rankIcon = 'friendly';
+			rankIconTooltip = 'Friendly';
 		} else if (gameMode === 'arena') {
 			rankIcon = 'arena/arena12wins';
+			rankIconTooltip = 'Arena';
 		} else if (gameMode === 'tavern-brawl') {
 			rankIcon = 'tavernbrawl';
+			rankIconTooltip = 'Tavern Brawl';
 		} else {
 			rankIcon = 'arenadraft';
 		}
-		return `/Files/assets/images/deck/ranks/${rankIcon}.png`;
+		return [`/Files/assets/images/deck/ranks/${rankIcon}.png`, rankIconTooltip];
 	}
 
-	private buildPlayerClassImage(info: GameStat, isPlayer: boolean): string {
+	private buildPlayerClassImage(info: GameStat, isPlayer: boolean): [string, string] {
 		if (info.gameMode === 'battlegrounds') {
 			if (!isPlayer) {
-				return null;
+				return [null, null];
 			} else if (info.playerCardId) {
-				return `https://static.zerotoheroes.com/hearthstone/cardart/256x/${info.playerCardId}.jpg`;
+				const card = this.allCards.getCard(info.playerCardId);
+				return [`https://static.zerotoheroes.com/hearthstone/cardart/256x/${info.playerCardId}.jpg`, card.name];
 			} else {
 				// Return Bob to not have an empty image
-				return `https://static.zerotoheroes.com/hearthstone/cardart/256x/TB_BaconShop_HERO_PH.jpg`;
+				return [`https://static.zerotoheroes.com/hearthstone/cardart/256x/TB_BaconShop_HERO_PH.jpg`, null];
 			}
 		}
-		return isPlayer
+		const name = isPlayer
+			? this.allCards.getCard(info.playerCardId).name
+			: this.allCards.getCard(info.opponentCardId).name;
+		const image = isPlayer
 			? `https://static.zerotoheroes.com/hearthstone/cardart/256x/${info.playerCardId}.jpg`
 			: `https://static.zerotoheroes.com/hearthstone/cardart/256x/${info.opponentCardId}.jpg`;
+		return [image, name];
 	}
 
 	private buildMatchResultIconSvg(info: GameStat): SafeHtml {
@@ -181,16 +208,20 @@ export class ReplayInfoComponent implements AfterViewInit {
 		}
 	}
 
-	private buildPlayCoinIconSvg(info: GameStat): SafeHtml {
+	private buildPlayCoinIconSvg(info: GameStat): [SafeHtml, string] {
 		if (info.gameMode === 'battlegrounds') {
-			return null;
+			return [null, null];
 		}
 		const iconName = info.coinPlay === 'coin' ? 'match_coin' : 'match_play';
-		return this.sanitizer.bypassSecurityTrustHtml(`
+		const tooltip = info.coinPlay === 'coin' ? 'Had the Coin' : 'Went first';
+		return [
+			this.sanitizer.bypassSecurityTrustHtml(`
 			<svg class="svg-icon-fill">
 				<use xlink:href="/Files/assets/svg/replays/replays_icons.svg#${iconName}"/>
 			</svg>
-		`);
+		`),
+			tooltip,
+		];
 	}
 
 	private buildRankText(info: GameStat): string {
