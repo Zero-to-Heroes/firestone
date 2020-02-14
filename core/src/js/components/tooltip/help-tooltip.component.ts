261c4b1c1ce9c3561d0d59cd3d9aba450dea4843
sebastientromp <sebastien.tromp@gmail.com>
2020-02-14 14:59:10 +0100

    DEV: allow HTML to be passed to help-tooltip.component.ts

--- core/src/js/components/tooltip/help-tooltip.component.ts
+++ core/src/js/components/tooltip/help-tooltip.component.ts
@@ -1,20 +1,23 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewRef } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
 	selector: 'help-tooltip',
 	styleUrls: [`../../../css/component/tooltip/help-tooltip.component.scss`],
 	template: `
-		<div class="help-tooltip">{{ _text }}</div>
+		<div class="help-tooltip" [innerHTML]="_text"></div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelpTooltipComponent {
-	_text: string;
+	_text: SafeHtml;
 
-	constructor(private cdr: ChangeDetectorRef) {}
+	constructor(private cdr: ChangeDetectorRef, private sanitizer: DomSanitizer) {}
 
 	@Input() set text(value: string) {
-		this._text = value;
+		// console.log('setting tooltip', value);
+		this._text = this.sanitizer.bypassSecurityTrustHtml(`<div>${value}</div>`);
+		// console.log('updated tooltip', this._text);
 		if (!(this.cdr as ViewRef).destroyed) {
 			this.cdr.detectChanges();
 		}
