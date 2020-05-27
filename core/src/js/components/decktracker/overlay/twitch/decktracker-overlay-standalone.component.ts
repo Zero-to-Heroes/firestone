260dcb59c2c6664af82bbb4acf17632784671eae
sebastientromp <sebastien.tromp@gmail.com>
2020-05-27 17:48:32 +0200

    BUG (twitch): fix some mouse over issues

--- core/src/js/components/decktracker/overlay/twitch/decktracker-overlay-standalone.component.ts
+++ core/src/js/components/decktracker/overlay/twitch/decktracker-overlay-standalone.component.ts
@@ -78,7 +78,7 @@ export class DeckTrackerOverlayStandaloneComponent implements AfterViewInit {
 		try {
 			console.log('resize event', event);
 			// Resize the tracker
-			const scale = event.newHeight / 800;
+			const scale = event.newHeight / 950;
 			// console.log('proposed scale', scale);
 			// Now shrink the scale is the tracker is taller than a portion of the container's height
 			const containerHeight = this.el.nativeElement.parentNode.parentNode.getBoundingClientRect().height;
