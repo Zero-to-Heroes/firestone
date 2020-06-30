e6de4e22b4f37a2a2cd7ba4226716bfba6859818
sebastientromp <sebastien.tromp@gmail.com>
2020-06-30 18:31:42 +0200

    DEV: send null ranks if league/rank are invalid

--- core/src/js/services/manastorm-bridge/end-game-uploader.service.ts
+++ core/src/js/services/manastorm-bridge/end-game-uploader.service.ts
@@ -93,14 +93,20 @@ export class EndGameUploaderService {
 			if (playerInfo && game.gameFormat === 'standard') {
 				if (playerInfo.standard?.legendRank > 0) {
 					playerRank = `legend-${playerInfo.standard.legendRank}`;
+				} else if (playerInfo.standard.leagueId >= 0 && playerInfo.standard.rankValue >= 0) {
+					playerRank = `${playerInfo.standard.leagueId}-${playerInfo.standard.rankValue}`;
 				} else {
-					playerRank = `${playerInfo.standard.leagueId}-${playerInfo.standard?.rankValue}`;
+					console.warn('Could not extract player rank', playerInfo.standard);
+					playerRank = null;
 				}
 			} else if (playerInfo && game.gameFormat === 'wild') {
 				if (playerInfo.wild?.legendRank > 0) {
 					playerRank = `legend-${playerInfo.wild.legendRank}`;
+				} else if (playerInfo.wild.leagueId >= 0 && playerInfo.wild.rankValue >= 0) {
+					playerRank = `${playerInfo.wild.leagueId}-${playerInfo.wild.rankValue}`;
 				} else {
-					playerRank = `${playerInfo.wild.leagueId}-${playerInfo.wild?.rankValue}`;
+					console.warn('Could not extract player rank', playerInfo.wild);
+					playerRank = null;
 				}
 			}
 		}
@@ -112,14 +118,20 @@ export class EndGameUploaderService {
 			if (opponentInfo && game.gameFormat === 'standard') {
 				if (opponentInfo.standard?.legendRank > 0) {
 					opponentRank = `legend-${opponentInfo.standard.legendRank}`;
-				} else {
+				} else if (opponentInfo.standard.leagueId >= 0 && opponentInfo.standard.rankValue >= 0) {
 					opponentRank = `${opponentInfo.standard.leagueId}-${opponentInfo.standard?.rankValue}`;
+				} else {
+					console.warn('Could not extract opponent rank', opponentInfo.standard);
+					opponentRank = null;
 				}
 			} else if (opponentInfo && game.gameFormat === 'wild') {
 				if (opponentInfo.wild?.legendRank > 0) {
 					opponentRank = `legend-${opponentInfo.wild.legendRank}`;
-				} else {
+				} else if (opponentInfo.wild.leagueId >= 0 && opponentInfo.wild.rankValue >= 0) {
 					opponentRank = `${opponentInfo.wild.leagueId}-${opponentInfo.wild?.rankValue}`;
+				} else {
+					console.warn('Could not extract opponent rank', opponentInfo.wild);
+					opponentRank = null;
 				}
 			}
 		}
