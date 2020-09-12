e67aa4aad1eb30c511eff3ca17d591a7e73efb78
sebastientromp <sebastien.tromp@gmail.com>
2020-09-12 10:37:06 +0200

    BUG (app): fix parsing for BGS games that involve Lord Barov's hero power

--- core/test/setup.js
+++ core/test/setup.js
@@ -3,15 +3,15 @@ console.error = function(message) {
 	error.apply(console, [...arguments, new Error().stack]); // keep default behaviour, but log the stack trace
 };
 
-global.console = {
-	log: jest.fn(), // console.log are ignored in tests
-	warn: jest.fn(),
-	info: jest.fn(),
+// global.console = {
+// 	log: jest.fn(), // console.log are ignored in tests
+// 	warn: jest.fn(),
+// 	info: jest.fn(),
 
-	// Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
-	error: console.error,
-	debug: console.debug,
-};
+// 	// Keep native behaviour for other methods, use those to print out things in your own tests, not `console.log`
+// 	error: console.error,
+// 	debug: console.debug,
+// };
 
 let isConsoleError;
 
--- integration-tests/bgs-replay-parser/replay-parser.int.test.ts
+++ integration-tests/bgs-replay-parser/replay-parser.int.test.ts
@@ -1,22 +1,15 @@
-import {
-	BgsPostMatchStats,
-	parseBattlegroundsGame,
-	parseHsReplayString,
-	Replay,
-} from '@firestone-hs/hs-replay-xml-parser/dist/public-api';
-import { BgsPlayer } from '../../core/src/js/models/battlegrounds/bgs-player';
+import { parseHsReplayString, Replay } from '@firestone-hs/hs-replay-xml-parser/dist/public-api';
 import { replayXmlTest } from './replay';
 
 describe('BGS Replay-parser - basic test', () => {
-	test('minion damage is correct', async () => {
+	test('sanity', async () => {
 		const replayXml = replayXmlTest;
-		console.time('start parsing');
 		const replay: Replay = parseHsReplayString(replayXml);
-		console.timeEnd('startParsing');
+		console.debug('additional result', replay.additionalResult);
 
-		const stats: BgsPostMatchStats = parseBattlegroundsGame(replayXml, BgsPlayer.create({} as BgsPlayer), []);
+		// const stats: BgsPostMatchStats = parseBattlegroundsGame(replayXml, BgsPlayer.create({} as BgsPlayer), []);
 
-		expect(stats).not.toBe(null);
-		console.debug(stats.damageToEnemyHeroOverTurn);
+		// expect(stats).not.toBe(null);
+		// console.debug(stats.leaderboardPositionOverTurn);
 	});
 });
