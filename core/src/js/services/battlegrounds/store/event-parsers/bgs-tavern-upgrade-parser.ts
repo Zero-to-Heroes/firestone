ef2d071630b68fc5755a4d1f9e388f35aa1afe8d
sebastientromp <sebastien.tromp@gmail.com>
2020-09-20 12:18:51 +0200

    BUG (bgs): bump tier of discovered triple if tavern upgrade occurs afterwards in same turn

--- core/src/js/services/battlegrounds/store/event-parsers/bgs-tavern-upgrade-parser.ts
+++ core/src/js/services/battlegrounds/store/event-parsers/bgs-tavern-upgrade-parser.ts
@@ -2,6 +2,7 @@ import { BattlegroundsState } from '../../../../models/battlegrounds/battlegroun
 import { BgsGame } from '../../../../models/battlegrounds/bgs-game';
 import { BgsPlayer } from '../../../../models/battlegrounds/bgs-player';
 import { BgsTavernUpgrade } from '../../../../models/battlegrounds/in-game/bgs-tavern-upgrade';
+import { BgsTriple } from '../../../../models/battlegrounds/in-game/bgs-triple';
 import { normalizeHeroCardId } from '../../bgs-utils';
 import { BgsTavernUpgradeEvent } from '../events/bgs-tavern-upgrade-event';
 import { BattlegroundsStoreEvent } from '../events/_battlegrounds-store-event';
@@ -20,15 +21,30 @@ export class BgsTavernUpgradeParser implements EventParser {
 			console.error('No player found to update the history', playerToUpdate);
 			return currentState;
 		}
+		const turn = currentState.currentGame.getCurrentTurnAdjustedForAsyncPlay();
 		const newHistory: readonly BgsTavernUpgrade[] = [
 			...playerToUpdate.tavernUpgradeHistory,
 			BgsTavernUpgrade.create({
 				tavernTier: event.tavernTier,
-				turn: currentState.currentGame.getCurrentTurnAdjustedForAsyncPlay(),
+				turn: turn,
 			}),
 		];
+		// If the upgrade happens after a triple has been done on the same turn, it's very likely that they
+		// dioscovered the triple once the upgrade occured, so we adjust the tavern tier
+		const newTripleHistory: readonly BgsTriple[] = playerToUpdate.tripleHistory.map(triple => {
+			if (triple.turn === turn) {
+				return {
+					tierOfTripledMinion: triple.tierOfTripledMinion + 1,
+					cardId: triple.cardId,
+					turn: triple.turn,
+				};
+			} else {
+				return triple;
+			}
+		});
 		const newPlayer = playerToUpdate.update({
 			tavernUpgradeHistory: newHistory,
+			tripleHistory: newTripleHistory,
 		} as BgsPlayer);
 		const newPlayers: readonly BgsPlayer[] = currentState.currentGame.players.map(player =>
 			normalizeHeroCardId(player.cardId) === normalizeHeroCardId(newPlayer.cardId) ? newPlayer : player,
