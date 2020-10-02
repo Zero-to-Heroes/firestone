81088c123fa961d6ff9f25403505746c90e129b1
sebastientromp <sebastien.tromp@gmail.com>
2020-10-02 16:03:33 +0200

    RELEASE: update version to 5.5.18

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -1,4 +1,4 @@
 export class FeatureFlags {
-	public static readonly ENABLE_BG_DESKTOP = true;
-	public static readonly ENABLE_BG_POSTMATCH_SHARE = true;
+	public static readonly ENABLE_BG_DESKTOP = false;
+	public static readonly ENABLE_BG_POSTMATCH_SHARE = false;
 }
