0ffa66300fec632e5709a8caae40a4918bec3adb
sebastientromp <sebastien.tromp@gmail.com>
2020-10-14 16:28:28 +0200

    FEATURE: disable detailed deck stats

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -1,5 +1,5 @@
 export class FeatureFlags {
 	public static readonly ENABLE_BG_DESKTOP = true;
 	public static readonly ENABLE_BG_POSTMATCH_SHARE = true;
-	public static readonly ENABLE_DECK_DETAILS = true;
+	public static readonly ENABLE_DECK_DETAILS = false;
 }
