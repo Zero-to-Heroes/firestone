5054725f7e39e90da984e1104b46344dd811208f
sebastientromp <sebastien.tromp@gmail.com>
2020-11-17 16:36:19 +0100

    deactivate duels

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -2,8 +2,8 @@ export class FeatureFlags {
 	// Deprecated, features are part of prod now
 	public static readonly ENABLE_BG_DESKTOP = true;
 	public static readonly ENABLE_BG_POSTMATCH_SHARE = true;
-
 	public static readonly ENABLE_DECK_DETAILS = true;
-	public static readonly ENABLE_DUELS = true;
+
+	public static readonly ENABLE_DUELS = false;
 	public static readonly ENABLE_CONSTRUCTED_RANKING_GRAPH = false;
 }
