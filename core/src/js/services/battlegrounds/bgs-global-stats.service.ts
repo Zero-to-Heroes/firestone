8566c8157ce8147bfd30a8fa7be058bdbdbb569f
sebastientromp <sebastien.tromp@gmail.com>
2020-11-23 14:08:49 +0100

    DEV. use static json as a source for BGS global stats

--- core/src/js/services/battlegrounds/bgs-global-stats.service.ts
+++ core/src/js/services/battlegrounds/bgs-global-stats.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { BgsStats } from '../../models/battlegrounds/stats/bgs-stats';
 import { ApiRunner } from '../api-runner';
 
-const BGS_STATS_RETRIEVE_URL = 'https://static-api.firestoneapp.com/retrieveBgsGlobalStats/{proxy+}?v=2';
+const BGS_STATS_RETRIEVE_URL = 'https://static.zerotoheroes.com/api/bgs-global-stats.json?v=3';
 
 @Injectable()
 export class BgsGlobalStatsService {
@@ -15,28 +15,8 @@ export class BgsGlobalStatsService {
 		const result: any = await this.api.callGetApiWithRetries(BGS_STATS_RETRIEVE_URL);
 		// console.log('result', result);
 		const globalStats = BgsStats.create({
-			heroStats: result.result.heroStats,
+			heroStats: result.heroStats,
 		} as BgsStats);
 		return globalStats;
 	}
-
-	// private loadGlobalStatsInternal(callback, retriesLeft = 5) {
-	// 	if (retriesLeft <= 0) {
-	// 		console.error('Could not load bgs global stats', `${BGS_STATS_RETRIEVE_URL}`);
-	// 		callback(null);
-	// 		return;
-	// 	}
-	// 	this.http.get(`${BGS_STATS_RETRIEVE_URL}`).subscribe(
-	// 		(result: any) => {
-	// 			const globalStats = BgsStats.create({
-	// 				heroStats: result.result.heroStats,
-	// 			} as BgsStats);
-	// 			console.log('loaded bgs-global-stats');
-	// 			callback(globalStats);
-	// 		},
-	// 		error => {
-	// 			setTimeout(() => this.loadGlobalStatsInternal(callback, retriesLeft - 1), 2000);
-	// 		},
-	// 	);
-	// }
 }
