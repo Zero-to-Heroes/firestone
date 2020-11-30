8ce6bc22d0b130a7a10db046290b4620b774ebbc
sebastientromp <sebastien.tromp@gmail.com>
2020-11-30 10:34:23 +0100

    activate duels

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -3,9 +3,10 @@ export class FeatureFlags {
 	public static readonly ENABLE_BG_DESKTOP = true;
 	public static readonly ENABLE_BG_POSTMATCH_SHARE = true;
 	public static readonly ENABLE_DECK_DETAILS = true;
-
 	public static readonly ENABLE_XP_NOTIFICATION = true;
-	public static readonly ENABLE_DUELS = false;
-	public static readonly ENABLE_NEW_VERSION_NOTIFICATION = false;
+
+	public static readonly ENABLE_DUELS = true;
+	public static readonly ENABLE_NEW_VERSION_NOTIFICATION = true;
+
 	public static readonly ENABLE_CONSTRUCTED_RANKING_GRAPH = false;
 }
