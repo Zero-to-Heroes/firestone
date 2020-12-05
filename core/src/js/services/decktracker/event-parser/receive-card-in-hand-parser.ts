864a9db8b8aecb29fd59e97a8920816974a35611
sebastientromp <sebastien.tromp@gmail.com>
2020-12-05 21:33:32 +0100

    BUG (tracker): fix info leak with Secret Passage + Pieces of C'Thun

--- core/src/js/services/decktracker/event-parser/receive-card-in-hand-parser.ts
+++ core/src/js/services/decktracker/event-parser/receive-card-in-hand-parser.ts
@@ -3,6 +3,7 @@ import { DeckCard } from '../../../models/decktracker/deck-card';
 import { DeckState } from '../../../models/decktracker/deck-state';
 import { GameState } from '../../../models/decktracker/game-state';
 import { GameEvent } from '../../../models/game-event';
+import { publicCardCreators } from '../../hs-utils';
 import { DeckManipulationHelper } from './deck-manipulation-helper';
 import { EventParser } from './event-parser';
 
@@ -20,21 +21,35 @@ export class ReceiveCardInHandParser implements EventParser {
 		const isPlayer = controllerId === localPlayer.PlayerId;
 		const deck = isPlayer ? currentState.playerDeck : currentState.opponentDeck;
 
+		const lastInfluencedByCardId = gameEvent.additionalData?.lastInfluencedByCardId;
+		const isCardInfoPublic = isPlayer || publicCardCreators.indexOf(lastInfluencedByCardId) !== -1;
+
 		// First try and see if this card doesn't come from the board or from the other zone (in case of discovers)
 		const boardCard = this.helper.findCardInZone(deck.board, null, entityId);
 		const otherCard = this.helper.findCardInZone(deck.otherZone, null, entityId);
+		// If a C'Thun piece was set aside, we know its data when getting the card back to hand, so we want to hide it
+		const otherCardWithObfuscation =
+			isCardInfoPublic || !otherCard
+				? otherCard
+				: otherCard.update({
+						creatorCardId: undefined,
+						cardId: undefined,
+						cardName: undefined,
+						lastAffectedByCardId: undefined,
+				  } as DeckCard);
 		// console.log(
 		// 	'[receive-card-in-hand] trying to get card from zone',
 		// 	deck.board,
 		// 	entityId,
 		// 	boardCard,
 		// 	otherCard,
+		// 	otherCardWithObfuscation,
 		// 	cardId,
 		// );
 		const newBoard = boardCard
 			? this.helper.removeSingleCardFromZone(deck.board, null, entityId, deck.deckList.length === 0)[0]
 			: deck.board;
-		const newOther = otherCard
+		const newOther = otherCardWithObfuscation
 			? this.helper.removeSingleCardFromZone(deck.otherZone, null, entityId)[0]
 			: deck.otherZone;
 		// console.log('[receive-card-in-hand] new board', newBoard, newOther);
@@ -42,7 +57,7 @@ export class ReceiveCardInHandParser implements EventParser {
 		const cardData = cardId ? this.allCards.getCard(cardId) : null;
 		const cardWithDefault =
 			boardCard ||
-			otherCard ||
+			otherCardWithObfuscation ||
 			DeckCard.create({
 				cardId: cardId,
 				entityId: entityId,
