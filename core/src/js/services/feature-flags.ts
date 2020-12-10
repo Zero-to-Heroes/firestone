821afa1887a411cea8a8e10d7c28b678561df3e3
sebastientromp <sebastien.tromp@gmail.com>
2020-12-10 09:36:04 +0100

    deactivate duels

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -5,8 +5,8 @@ export class FeatureFlags {
 	public static readonly ENABLE_DECK_DETAILS = true;
 	public static readonly ENABLE_XP_NOTIFICATION = true;
 
-	public static readonly ENABLE_DUELS = true;
-	public static readonly ENABLE_NEW_VERSION_NOTIFICATION = true;
+	public static readonly ENABLE_DUELS = false;
+	public static readonly ENABLE_NEW_VERSION_NOTIFICATION = false;
 
 	public static readonly ENABLE_CONSTRUCTED_RANKING_GRAPH = false;
 }
