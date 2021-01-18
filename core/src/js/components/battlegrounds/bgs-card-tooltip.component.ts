1e5af2bb3f64743a00628c63e7a71d7fb527087f
sebastientromp <sebastien.tromp@gmail.com>
2021-01-18 16:41:38 +0100

    UI (bgs): use real BG image for tooltip
    instead of rebuilding everything

--- core/src/js/components/battlegrounds/bgs-card-tooltip.component.ts
+++ core/src/js/components/battlegrounds/bgs-card-tooltip.component.ts
@@ -1,20 +1,15 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewRef } from '@angular/core';
+import { GameTag } from '@firestone-hs/reference-data';
 import { Entity } from '@firestone-hs/replay-parser';
 
 @Component({
-	selector: 'card-tooltip',
+	selector: 'bgs-card-tooltip',
 	styleUrls: [`../../../css/component/battlegrounds/bgs-card-tooltip.component.scss`],
 	template: `
-		<card
-			[ngClass]="{ 'hidden': !_visible }"
-			class="tooltip"
-			[forbiddenTargetSource]="true"
-			[entity]="_entity"
-			[hasTooltip]="false"
-			[aspectRatio]="0.75"
-			*ngIf="_entity"
-		>
-		</card>
+		<div class="container" [ngClass]="{ 'hidden': !_visible }">
+			<img [src]="image" class="card" />
+			<card-stats [cardId]="cardId" [attack]="attack" [health]="health"> </card-stats>
+		</div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -22,11 +17,24 @@ export class BgsCardTooltipComponent {
 	_entity: Entity;
 	_visible: boolean;
 
+	image: string;
+	cardId: string;
+	attack: number;
+	health: number;
+
 	@Input() set config(value: Entity) {
 		if (value === this._entity) {
 			return;
 		}
 		this._entity = value;
+		this.cardId = value.cardID;
+		this.attack = this._entity.getTag(GameTag.ATK);
+		this.health = this._entity.getTag(GameTag.HEALTH);
+		if (this._entity.getTag(GameTag.PREMIUM) === 1) {
+			this.image = `https://static.zerotoheroes.com/hearthstone/fullcard/en/compressed/battlegrounds/${this._entity.cardID}_bgs_premium.png`;
+		} else {
+			this.image = `https://static.zerotoheroes.com/hearthstone/fullcard/en/compressed/battlegrounds/${this._entity.cardID}_bgs.png`;
+		}
 		// console.log('setting card in tooltip', value);
 		if (!(this.cdr as ViewRef)?.destroyed) {
 			this.cdr.detectChanges();
