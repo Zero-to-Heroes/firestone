d8394d970729ef7c464699dfd7d38e9f19f77890
sebastientromp <sebastien.tromp@gmail.com>
2021-02-09 11:01:20 +0100

    RELEASE: 7.2.7

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -29,22 +29,22 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '7.2.4',
+		version: '7.2.7',
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `
-					Finally some improvements to the in-game overlay in Battlegrounds! Now you don't need multiple monitors to take advantage of many of the BG features :) 
-					<br/>
-					And I'm looking to rework the app's home page (at https://www.firestoneapp.com/) to better showcase everything the app can do. If you or someone you know are available for the job, please let me know (on Discord)!
-					<br/>
-					<br/>
-					Stay safe,
-					<br/>
-					Seb.
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `
+			// 		Finally some improvements to the in-game overlay in Battlegrounds! Now you don't need multiple monitors to take advantage of many of the BG features :)
+			// 		<br/>
+			// 		And I'm looking to rework the app's home page (at https://www.firestoneapp.com/) to better showcase everything the app can do. If you or someone you know are available for the job, please let me know (on Discord)!
+			// 		<br/>
+			// 		<br/>
+			// 		Stay safe,
+			// 		<br/>
+			// 		Seb.
+			// 	`,
+			// },
 			{
 				type: 'main',
 				header: 'Main updates',
@@ -62,20 +62,8 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `You now have a small icon next to the last opponent you fought (useful for Murozond for instance).`,
-							},
-							{
-								type: 'feature',
-								text: `You can now enable the "view replay example" in the battle simulation overlay. It opens the simulation example in a new browser tab, so the option is disabled by default.`,
-							},
-							{
-								type: 'feature',
-								text: `You can now see the last known opponent's board (as well as other pieces of data) when mousing over the opponent's avatar in the leaderboard.`,
-							},
-							{
-								type: 'feature',
-								text: `You can now show the list of all minions, grouped by tavern tier.`,
+								type: 'bug',
+								text: `Fix a bug where the tavern upgrades for opponents would sometimes not be detected.`,
 							},
 							// {
 							// 	type: 'feature',
@@ -83,32 +71,14 @@ export const updates: readonly Update[] = [
 							// },
 						],
 					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'content',
-								text: `Add player and AI decklists for Anduin's Book of Heroes.`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `Add a button to directly copy the current screen to your clipboard (for now only on the Battlegrounds pages). Useful for quickly sharing your end comp on Discord :)`,
-							},
-							{
-								type: 'feature',
-								text: `Add a button to shrae the current screen to Reddit (for now only on the Battlegrounds pages).`,
-							},
-							// {
-							// 	type: 'feature',
-							// 	text: `Added a button to directly share the current screen to Reddit (for now only on the Battlegrounds pages).`,
-							// },
-						],
-					},
+					// {
+					// 	category: 'decktracker',
+					// 	details: [],
+					// },
+					// {
+					// 	category: 'general',
+					// 	details: [],
+					// },
 				],
 			},
 			{
@@ -119,50 +89,49 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'ui',
-								text: `The information for the lobby's other opponents should now be more readable. On bigger screens, it will also display the timing of the last tavern upgrades.`,
+								type: 'feature',
+								text: `Add an option to hide the hero selection screen.`,
+							},
+							{
+								type: 'feature',
+								text: `Add options to hide the battle simulation during the combat phase, and show it in the Tavern.`,
 							},
 							{
 								type: 'ui',
-								text: `The ALL tribe now uses Amalgadon's image, instead of Zapp's.`,
-							},
-							{
-								type: 'ui',
-								text: `Remove the Firestone logo from the battle simulation overlay.`,
+								text: `Increase the fond size for minions stats.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where the winrate chart would cap at 80% in the post-match stats.`,
+								text: `Fix a bug where sometimes the simulation sample link would not open.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where the ghost's board info would override your previous opponent's board.`,
+								text: `Fix a bug where disabling the second-screen app would also disable the battle simulation.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where the "coins wasted" stat was not properly computed anymore.`,
+								text: `Fix a bug where the minions tier list could not be moved around.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a small bug in the simulator with minions that involve their neighbours, when the dead minions is at the far right of the board.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a small bug in the simulator with Greybough's hero power and reborn minions.`,
 							},
 						],
 					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'bug',
-								text: `When opening Darkmoon Races cards, the app doesn't show one notification per card anymore.`,
-							},
-						],
-					},
+					// {
+					// 	category: 'collection',
+					// 	details: [],
+					// },
 					{
 						category: 'decktracker',
 						details: [
 							{
-								type: 'feature',
-								text: `Cards drawn by Tome of Origination are now flagged in the opponent's hand.`,
-							},
-							{
-								type: 'feature',
-								text: `Add more time filter options in the Ladder screens.`,
+								type: 'bug',
+								text: `Fix AI decklists not loading.`,
 							},
 							// {
 							// 	type: 'ui',
@@ -175,7 +144,20 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Add a class filter for hero powers and signature treasures. Please note that dual class treasures are still not correctly handled (they only appear in one of the two classes), and that will be fixed in a later release.`,
+								text: `Add text filter for heroes / hero powers / signature treasures.`,
+							},
+							{
+								type: 'misc',
+								text: `Improve performance when navigating through the various tabs.`,
+							},
+						],
+					},
+					{
+						category: 'achievements',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix a bug where clearing the round 3 in Monster Hunt would grant all achievements for MH. Also adds a similar fix for the other solo adventures.`,
 							},
 						],
 					},
@@ -183,13 +165,13 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'misc',
-								text: `Removed a CPU spike when going into the Duels screens (before entering a match).`,
+								type: 'ui',
+								text: `Stop scaling elements when going full-screen. This rework will be an ongoing effort throughout the next releases.`,
 							},
-							// {
-							// 	type: 'ui',
-							// 	text: `(ALPHA) Add archetype id below the deck name when viewing deck details.`,
-							// },
+							{
+								type: 'bug',
+								text: `Fix a bug where the loading window would not appear anymore.`,
+							},
 						],
 					},
 				],
