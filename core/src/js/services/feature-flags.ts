f87f8d26dec44932f890d67acc14505a3dd3524f
sebastientromp <sebastien.tromp@gmail.com>
2021-02-09 10:46:03 +0100

    activate feature flags for bg sim options

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -7,13 +7,14 @@ export class FeatureFlags {
 	public static readonly ENABLE_BG_OPPONENT_MOUSE_OVER = true;
 	public static readonly ENABLE_BG_MINIONS_LIST = true;
 
-	public static readonly ENABLE_BG_SIMULATION_SHOW_ONLY_ON_RECRUIT = false;
-	public static readonly ENABLE_BG_SIMULATION_HIDE_ON_RECRUIT = false;
+	public static readonly ENABLE_BG_SIMULATION_SHOW_ONLY_ON_RECRUIT = true;
+	public static readonly ENABLE_BG_SIMULATION_HIDE_ON_RECRUIT = true;
+
 	public static readonly ENABLE_DECKTRACKER_RESET_POSITIONS = false;
 	public static readonly ENABLE_BG_TRIBE_HIGHLIGHT = false;
 	public static readonly SHOW_CONSTRUCTED_SECONDARY_WINDOW = false;
 	public static readonly ENABLE_RANKED_ARCHETYPE = false;
-	public static readonly ENABLE_REAL_TIME_STATS = true;
+	public static readonly ENABLE_REAL_TIME_STATS = false;
 
 	// Deprecated, feature is sidelined for now
 	public static readonly ENABLE_CONSTRUCTED_RANKING_GRAPH = false;
