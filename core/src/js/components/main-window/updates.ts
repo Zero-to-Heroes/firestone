c889a4882555be30d47d397130921b92c4ee7a43
sebastientromp <sebastien.tromp@gmail.com>
2021-02-23 11:10:16 +0100

    RELEASE: 7.3.13

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -29,66 +29,49 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '7.3.12',
+		version: '7.3.13',
 		sections: [
-			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `
-			// 		Beta version is back!
-			// 		<br/>
-			// 		<br/>
-			// 		Stay safe,
-			// 		<br/>
-			// 		Seb.
-			// 	`,
-			// },
 			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					// {
-					// 	category: 'achievements',
-					// 	details: [
-					// 		{
-					// 			type: 'feature',
-					// 			text: `(ALPHA) A second-screen window can be activated for non-BG matches that shows you the current progress on each achievements, and highlighting the achievements that have progressed during the current match.`,
-					// 		},
-					// 	],
-					// },
-					// {
-					// 	category: 'battlegrounds',
-					// 	details: [
-					// 		// {
-					// 		// 	type: 'feature',
-					// 		// 	text: `(ALPHA) You can now enable highlighting of minions that match specific tribes in the tavern.`,
-					// 		// },
-					// 	],
-					// },
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'content',
-								text: `The app has been updated for patch 19.6.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix a bug where the option to disable the tracker in specific game modes would not work anymore.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a bug where the tracker would appear in BG games if you have the "don't close tracker after match ends" option enabled.`,
-							},
-						],
-					},
-				],
+				type: 'intro',
+				header: 'Forged in the Barrens Mega Bundle giveaway!',
+				text: `
+					Instead of using giveaways to get more people to like and follow a Twitter account, I would like to use this opportunity to thank all of you who have been using Firestone so far. You don't need to do anything to enter, I will simply pick someone at random who has played at least a match between 2021-02-15 and 2021-02-25 (while using Firestone, otherwise I have no way to know). 
+					<br/>
+					ATTENTION: you need to have created an account and be logged in Overwolf so that I can contact you.
+					<br/>
+					<br/>
+					If you want to spread the love of the app though, please feel free to do so :) The more people use the app, the more relevant the stats and global info become, and the closer I can get to be a real full-time Firestone dev, which would let me keep the flow of updates going :)
+					<br/>
+					<br/>
+					Take care,
+					<br/>
+					Seb.
+				`,
 			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		// {
+			// 		// 	category: 'achievements',
+			// 		// 	details: [
+			// 		// 		{
+			// 		// 			type: 'feature',
+			// 		// 			text: `(ALPHA) A second-screen window can be activated for non-BG matches that shows you the current progress on each achievements, and highlighting the achievements that have progressed during the current match.`,
+			// 		// 		},
+			// 		// 	],
+			// 		// },
+			// 		// {
+			// 		// 	category: 'battlegrounds',
+			// 		// 	details: [
+			// 		// 		// {
+			// 		// 		// 	type: 'feature',
+			// 		// 		// 	text: `(ALPHA) You can now enable highlighting of minions that match specific tribes in the tavern.`,
+			// 		// 		// },
+			// 		// 	],
+			// 		// },
+			// 	],
+			// },
 			// {
 			// 	type: 'minor',
 			// 	header: 'Minor updates',
@@ -104,26 +87,26 @@ export const updates: readonly Update[] = [
 			// 		},
 			// 	],
 			// },
-			{
-				type: 'future',
-				header: "What's next",
-				text: `
-					A few features are on alpha / beta testing phase today:
-					<br/>
-					<ul>
-						<li>(Battlegrounds) A way to highlight specific minions or tribes in the tavern.</li>
-						<li>(Constructed) A way to guess the opponent's archetype from the card they have played, and the ability to override their decklist with a popular list from that archetype.</li>
-						<li>A way to track the current progress you're making towards achievements while in a match.
-					</ul>
-					<br/>
-					If you are interested in helping me test and polish these, feel free to ping me on Discord :)
-					<br/>
-					<br/>
-					Stay safe,
-					<br/>
-					Seb.
-				`,
-			},
+			// {
+			// 	type: 'future',
+			// 	header: "What's next",
+			// 	text: `
+			// 		A few features are on alpha / beta testing phase today:
+			// 		<br/>
+			// 		<ul>
+			// 			<li>(Battlegrounds) A way to highlight specific minions or tribes in the tavern.</li>
+			// 			<li>(Constructed) A way to guess the opponent's archetype from the card they have played, and the ability to override their decklist with a popular list from that archetype.</li>
+			// 			<li>A way to track the current progress you're making towards achievements while in a match.
+			// 		</ul>
+			// 		<br/>
+			// 		If you are interested in helping me test and polish these, feel free to ping me on Discord :)
+			// 		<br/>
+			// 		<br/>
+			// 		Stay safe,
+			// 		<br/>
+			// 		Seb.
+			// 	`,
+			// },
 		],
 	},
 ];
