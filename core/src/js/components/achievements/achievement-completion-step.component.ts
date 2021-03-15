3394503377448fab8f3ebb6034829a748c636db0
sebastientromp <sebastien.tromp@gmail.com>
2021-03-15 09:25:15 +0100

    UI (achievements): fix long tooltips for achievements

--- core/src/js/components/achievements/achievement-completion-step.component.ts
+++ core/src/js/components/achievements/achievement-completion-step.component.ts
@@ -6,7 +6,12 @@ import { CompletionStep } from '../../models/visual-achievement';
 	selector: 'achievement-completion-step',
 	styleUrls: [`../../../css/component/achievements/achievement-completion-step.component.scss`],
 	template: `
-		<div class="completion-step" [ngClass]="{ 'completed': completedTimes > 0 }" [innerHTML]="svgAndTooltip"></div>
+		<div
+			class="completion-step"
+			[ngClass]="{ 'completed': completedTimes > 0 }"
+			[innerHTML]="svgAndTooltip"
+			[helpTooltip]="tooltip"
+		></div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None,
@@ -15,24 +20,20 @@ export class AchievementCompletionStepComponent {
 	completionStep: CompletionStep;
 	completedTimes: number;
 	svgAndTooltip: SafeHtml;
+	tooltip: string;
 
 	constructor(private domSanitizer: DomSanitizer) {}
 
 	@Input() set step(step: CompletionStep) {
 		this.completionStep = step;
 		this.completedTimes = step.numberOfCompletions;
+		this.tooltip = step.text(true);
 		this.svgAndTooltip = this.domSanitizer.bypassSecurityTrustHtml(`
 			<i class="i-30">
                 <svg class="svg-icon-fill">
                     <use xlink:href="assets/svg/sprite.svg#${step.icon}"/>
                 </svg>
             </i>
-            <div class="zth-tooltip bottom">
-                <p>${step.text(true)}</p>
-                <svg class="tooltip-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 9">
-                    <polygon points="0,0 8,-9 16,0"/>
-                </svg>
-            </div>
 		`);
 	}
 }
