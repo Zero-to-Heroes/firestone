7b33cbd0b4075e0039aa6ebc835577a012fe0702
sebastientromp <sebastien.tromp@gmail.com>
2021-03-26 17:21:17 +0100

    DEV: update type of CurrentScene in memory-update.ts

--- core/src/js/models/memory/memory-update.ts
+++ core/src/js/models/memory/memory-update.ts
@@ -1,8 +1,9 @@
+import { SceneMode } from '@firestone-hs/reference-data';
 import { CardPackInfo, PackInfo } from './pack-info';
 
 export interface MemoryUpdate {
 	readonly DisplayingAchievementToast: boolean;
-	readonly CurrentScene: string;
+	readonly CurrentScene: SceneMode;
 
 	// These are not populated by the regular info updates, as they are costly to compute
 	readonly OpenedPack: PackInfo;
