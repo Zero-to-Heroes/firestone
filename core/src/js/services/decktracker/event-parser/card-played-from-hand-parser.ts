1e489baab04d16f2e0de73ef221195860ceb19d1
sebastientromp <sebastien.tromp@gmail.com>
2021-04-12 17:09:09 +0200

    BUG (tracker): fix cost for opponent's global effect not showing

--- core/src/js/services/decktracker/event-parser/card-played-from-hand-parser.ts
+++ core/src/js/services/decktracker/event-parser/card-played-from-hand-parser.ts
@@ -30,7 +30,7 @@ export class CardPlayedFromHandParser implements EventParser {
 		const isPlayer = controllerId === localPlayer.PlayerId;
 		const deck = isPlayer ? currentState.playerDeck : currentState.opponentDeck;
 		const card = this.helper.findCardInZone(deck.hand, cardId, entityId);
-		// console.log('[card-played-from-hand] card in zone', card, deck.hand, cardId, entityId);
+		//console.debug('[card-played-from-hand] card in zone', card, deck.hand, cardId, entityId);
 
 		const [newHand, removedCard] = this.helper.removeSingleCardFromZone(
 			deck.hand,
@@ -41,7 +41,7 @@ export class CardPlayedFromHandParser implements EventParser {
 		// console.log('removed card from hand', removedCard, currentState, gameEvent);
 		let newDeck =
 			removedCard != null ? this.helper.updateDeckForAi(gameEvent, currentState, removedCard) : deck.deck;
-		// console.log('removed card from hand', removedCard, deck.deck, newDeck);
+		//console.debug('removed card from hand', removedCard, deck.deck, newDeck);
 		// This happens when we create a card in the deck, then leave it there when the opponent draws it
 		// (to avoid info leaks). When they play it we won't find it in the "hand" zone, so we try
 		// and see if it is somewhere in the deck
@@ -64,6 +64,8 @@ export class CardPlayedFromHandParser implements EventParser {
 		const cardWithZone =
 			card?.update({
 				zone: isOnBoard ? 'PLAY' : null,
+				manaCost: card.manaCost ?? refCard?.cost,
+				rarity: card.rarity?.toLowerCase() ?? refCard?.rarity?.toLowerCase(),
 			} as DeckCard) ||
 			DeckCard.create({
 				entityId: entityId,
@@ -73,6 +75,7 @@ export class CardPlayedFromHandParser implements EventParser {
 				rarity: refCard?.rarity?.toLowerCase(),
 				zone: isOnBoard ? 'PLAY' : null,
 			} as DeckCard);
+		//console.debug('card with zone', cardWithZone, refCard, cardId);
 		const newBoard: readonly DeckCard[] = isOnBoard
 			? this.helper.addSingleCardToZone(deck.board, cardWithZone)
 			: deck.board;
@@ -86,6 +89,7 @@ export class CardPlayedFromHandParser implements EventParser {
 		let newGlobalEffects: readonly DeckCard[] = deck.globalEffects;
 		if (!isCardCountered && globalEffectCards.includes(card?.cardId)) {
 			newGlobalEffects = this.helper.addSingleCardToZone(deck.globalEffects, cardWithZone);
+			//console.debug('includes global effect', cardWithZone, deck.globalEffects, newGlobalEffects);
 		}
 
 		const isElemental =
