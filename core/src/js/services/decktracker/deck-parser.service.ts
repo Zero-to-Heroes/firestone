07af9620de22365a8300c5ea151fe4f28ed49e01
sebastientromp <sebastien.tromp@gmail.com>
2021-05-06 10:19:01 +0200

    LOG: remove verbose logs

--- core/src/js/services/decktracker/deck-parser.service.ts
+++ core/src/js/services/decktracker/deck-parser.service.ts
@@ -128,17 +128,17 @@ export class DeckParserService {
 	}
 
 	public async queueingIntoMatch(logLine: string) {
-		console.log(
-			'[deck-parser] will detect active deck from queue?',
-			logLine,
-			this.currentGameType,
-			this.selectedDeckId,
-		);
+		// console.log(
+		// 	'[deck-parser] will detect active deck from queue?',
+		// 	logLine,
+		// 	this.currentGameType,
+		// 	this.selectedDeckId,
+		// );
 		if (
 			this.currentGameType === GameType.GT_BATTLEGROUNDS ||
 			this.currentGameType === GameType.GT_BATTLEGROUNDS_FRIENDLY
 		) {
-			console.debug('BG game, returning');
+			// console.debug('BG game, returning');
 			return;
 		}
 
