94a78ce391e5dbef2ab5c78692871866765bafa6
sebastientromp <sebastien.tromp@gmail.com>
2021-06-08 12:37:36 +0200

    DEV: prevent NPE

--- core/src/js/services/battlegrounds/store/battlegrounds-store.service.ts
+++ core/src/js/services/battlegrounds/store/battlegrounds-store.service.ts
@@ -299,8 +299,8 @@ export class BattlegroundsStoreService {
 					prefs.bgsEnableSimulation
 				) {
 					if (
-						this.state.currentGame.battleInfo?.opponentBoard?.player?.cardId &&
-						this.state.currentGame.battleInfo?.opponentBoard?.player?.cardId !==
+						this.state.currentGame?.battleInfo?.opponentBoard?.player?.cardId &&
+						this.state.currentGame?.battleInfo?.opponentBoard?.player?.cardId !==
 							gameEvent.additionalData.opponent &&
 						gameEvent.additionalData.opponent != CardIds.NonCollectible.Neutral.KelthuzadBattlegrounds
 					) {
@@ -312,7 +312,7 @@ export class BattlegroundsStoreService {
 							this.state.currentGame.battleInfo,
 							this.state.currentGame.battleResult,
 						);
-					} else if (!this.state.currentGame.battleResult || !this.state.currentGame.battleInfo) {
+					} else if (!this.state.currentGame?.battleResult || !this.state.currentGame?.battleInfo) {
 						// When no one has a board (or rather, when no player ever attacks during the battle),
 						// the PLAYER_BOARD event is not sent, and so battle result is never set
 						// Ties in battle are the only situation where this can happen, so I'm for now downgrading
