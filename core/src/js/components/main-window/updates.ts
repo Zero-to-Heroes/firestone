46cf29aea492eb0d1fec83872de00703e490ccc7
sebastientromp <sebastien.tromp@gmail.com>
2021-08-09 20:38:53 +0200

    RELEASE: 7.12.13

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -31,50 +31,50 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '7.12.10',
+		version: '7.12.13',
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `
-					This release adds lots of Quality of Life improvements, which are one of my favorite things to do. I hope you'll enjoy it :)
-				`,
-			},
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Mousing over Varian, King of Stormwind in the decklist now highlights all Taunt / Rush / Divine Shield minions from your deck.`,
-							},
-							{
-								type: 'feature',
-								text: `Mousing over Jace Darkweaver now highlights all Fel spells played this game.`,
-							},
-							{
-								type: 'feature',
-								text: `There is now a counter for dead Elwynn Boars that appears when a Boar is played, or if you have a Boar in the deck.`,
-							},
-							{
-								type: 'feature',
-								text: `A widget now appears to remind you of the first Battlecry minion played this turn when you have Bolner Hammerbeak on board or in hand.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where playing a Questline would sometimes cause the secret helper to pop up.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where playing a Sigil would sometimes not update the opponent's hand.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `
+			// 		This release adds lots of Quality of Life improvements, which are one of my favorite things to do. I hope you'll enjoy it :)
+			// 	`,
+			// },
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Mousing over Varian, King of Stormwind in the decklist now highlights all Taunt / Rush / Divine Shield minions from your deck.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Mousing over Jace Darkweaver now highlights all Fel spells played this game.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `There is now a counter for dead Elwynn Boars that appears when a Boar is played, or if you have a Boar in the deck.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `A widget now appears to remind you of the first Battlecry minion played this turn when you have Bolner Hammerbeak on board or in hand.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where playing a Questline would sometimes cause the secret helper to pop up.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where playing a Sigil would sometimes not update the opponent's hand.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -84,47 +84,20 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `(Demon Hunter) Cost reduction buffs from the Questline now show in the opponent's hand.`,
+								text: `Show current bonus damage for Ignite spells shuffled in the deck.`,
 							},
 							{
 								type: 'feature',
-								text: `(Hunter) The exact cards drawn by Devouring Swarm and Rats of Extraordinary Size are now properly flagged in hand. Global effects for the Hunter Questline have been added as well.`,
+								text: `Add support for Lady Prestor.`,
 							},
+						],
+					},
+					{
+						category: 'collection',
+						details: [
 							{
-								type: 'feature',
-								text: `(Mage) Second flame is now properly flagged in hand. Cards drawn / discovered from Sorcerer's Gambit questline steps are also flagged in hand.`,
-							},
-							{
-								type: 'feature',
-								text: `(Priest) Cards drawn by Call of the Grave and the Seek Guidance Questline are now properly flagged in hand.`,
-							},
-							{
-								type: 'feature',
-								text: `(Paladin) Cards drawn by Blessed Goods and Alliance Bannerman are now properly flagged in hand. Lightborn Cariel has been added as a Global Effect when played.`,
-							},
-							{
-								type: 'feature',
-								text: `(Rogue) All cards drawn by the various Spy Gizmos are now properly flagged in hand, as well as the Coins from Loan Shark and the draw from Sketchy Information.`,
-							},
-							{
-								type: 'feature',
-								text: `(Shaman) Stormcaller Bru'kan is now properly flagged in hand, as well as cards drawn by Investment Opportunity. Global effects have been added for Bru'kan and Granite Forgeborn.`,
-							},
-							{
-								type: 'feature',
-								text: `(Warrior) The cards drawn by Raid the docks and Harbor Scamp are now properly flagged in hand.`,
-							},
-							{
-								type: 'feature',
-								text: `(Warlock) Blightborn Tamsin now appears in the Global Effects section after being played, and it is now properly flagged in hand.`,
-							},
-							{
-								type: 'feature',
-								text: `(Neutral) Cards drawn / discovered by Deeprun Engineer, Mailbox Dancer, Pandaren Importer, Entrapped Sorceress are now properly flagged in hand.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where playing the same card multiple times (when bounced back to hand) would only add the Global Effect once.`,
+								type: 'content',
+								text: `Add support for United in Stormwind pack.`,
 							},
 						],
 					},
