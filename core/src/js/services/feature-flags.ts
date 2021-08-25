4d8edc394528f521e11fc0d7aa5fbb8afec75bb9
sebastientromp <sebastien.tromp@gmail.com>
2021-08-25 20:36:55 +0200

    RELEASE: 7.14.2

--- core/src/js/services/feature-flags.ts
+++ core/src/js/services/feature-flags.ts
@@ -3,8 +3,8 @@ export class FeatureFlags {
 	public static readonly ENABLE_CONSTRUCTED_RANKING_GRAPH = true;
 	public static readonly ENABLE_DUELS_LEADERBOARD = true;
 
-	public static readonly ENABLE_BGS_FULL_SIMULATOR = true;
-	public static readonly ENABLE_STATS_TAB = true;
+	public static readonly ENABLE_BGS_FULL_SIMULATOR = false;
+	public static readonly ENABLE_STATS_TAB = false;
 
 	// Shelved for now
 	public static readonly ENABLE_MULTI_GRAPHS = false;
