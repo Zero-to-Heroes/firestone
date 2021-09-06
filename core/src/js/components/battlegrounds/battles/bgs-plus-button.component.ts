fbc56423fbc68c6d9187dd20022c0b7b4e33855a
sebastientromp <sebastien.tromp@gmail.com>
2021-09-06 11:42:16 +0200

    UI (bgs): update icon for when modifying existing minion

--- core/src/js/components/battlegrounds/battles/bgs-plus-button.component.ts
+++ core/src/js/components/battlegrounds/battles/bgs-plus-button.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 	styleUrls: [`../../../../css/component/battlegrounds/battles/bgs-plus-button.component.scss`],
 	template: `
 		<div class="change-icon" inlineSVG="assets/svg/bg_plus.svg" *ngIf="!useUpdateIcon"></div>
-		<div class="change-icon" inlineSVG="assets/svg/bg_rename.svg" *ngIf="useUpdateIcon"></div>
+		<div class="change-icon update" inlineSVG="assets/svg/bg_replace.svg" *ngIf="useUpdateIcon"></div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
