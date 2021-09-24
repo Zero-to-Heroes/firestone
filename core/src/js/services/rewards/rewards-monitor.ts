5eee25b578de52dd68e9fc84717ea63528f155a2
sebastientromp <sebastien.tromp@gmail.com>
2021-09-24 13:33:01 +0200

    BUG (app): fix XP notif not showing
    In a degraded mode, while the memory info is not there

--- core/src/js/services/rewards/rewards-monitor.ts
+++ core/src/js/services/rewards/rewards-monitor.ts
@@ -1,157 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Map } from 'immutable';
 import { MemoryUpdate } from '../../models/memory/memory-update';
+import { RewardsTrackInfo } from '../../models/rewards-track-info';
 import { Events } from '../events.service';
 import { MemoryInspectionService } from '../plugins/memory-inspection.service';
+import { Season3 } from '../stats/xp/xp-tables/season-3';
+import { Season } from '../stats/xp/xp-tables/_season';
 import { sleep } from '../utils';
 
 @Injectable()
 export class RewardMonitorService {
-	private static readonly XP_PER_LEVEL: Map<number, number> = Map([
-		[1, 0],
-		[2, 100],
-		[3, 100],
-		[4, 150],
-		[5, 150],
-		[6, 225],
-		[7, 225],
-		[8, 300],
-		[9, 300],
-		[10, 325],
-		[11, 325],
-		[12, 350],
-		[13, 350],
-		[14, 375],
-		[15, 375],
-		[16, 400],
-		[17, 400],
-		[18, 425],
-		[19, 425],
-		[20, 450],
-		[21, 450],
-		[22, 550],
-		[23, 600],
-		[24, 650],
-		[25, 675],
-		[26, 675],
-		[27, 875],
-		[28, 875],
-		[29, 1000],
-		[30, 1100],
-		[31, 1200],
-		[32, 1200],
-		[33, 1250],
-		[34, 1250],
-		[35, 1300],
-		[36, 1300],
-		[37, 1350],
-		[38, 1350],
-		[39, 1400],
-		[40, 1400],
-		[41, 1450],
-		[42, 1450],
-		[43, 1500],
-		[44, 1500],
-		[45, 1550],
-		[46, 1550],
-		[47, 1600],
-		[48, 1600],
-		[49, 1650],
-		[50, 1650],
-		[51, 1700],
-		[52, 1700],
-		[53, 1750],
-		[54, 1750],
-		[55, 1800],
-		[56, 1800],
-		[57, 1850],
-		[58, 1850],
-		[59, 1900],
-		[60, 1900],
-		[61, 1950],
-		[62, 1950],
-		[63, 2000],
-		[64, 2000],
-		[65, 2050],
-		[66, 2050],
-		[67, 2125],
-		[68, 2125],
-		[69, 2250],
-		[70, 2250],
-		[71, 2375],
-		[72, 2375],
-		[73, 2500],
-		[74, 2500],
-		[75, 2500],
-		[76, 2500],
-		[77, 2500],
-		[78, 2500],
-		[79, 2500],
-		[80, 2500],
-		[81, 2500],
-		[82, 2500],
-		[83, 2500],
-		[84, 2500],
-		[85, 2500],
-		[86, 2500],
-		[87, 2500],
-		[88, 2500],
-		[89, 2500],
-		[90, 2500],
-		[91, 2500],
-		[92, 2500],
-		[93, 2500],
-		[94, 2500],
-		[95, 2500],
-		[96, 2500],
-		[97, 2500],
-		[98, 2500],
-		[99, 2500],
-		[100, 2500],
-		[101, 1325],
-		[102, 1325],
-		[103, 1350],
-		[104, 1350],
-		[105, 1350],
-		[106, 1350],
-		[107, 1350],
-		[108, 1350],
-		[109, 1375],
-		[110, 1375],
-		[111, 1375],
-		[112, 1375],
-		[113, 1400],
-		[114, 1400],
-		[115, 1400],
-		[116, 1400],
-		[117, 1400],
-		[118, 1400],
-		[119, 1425],
-		[120, 1425],
-		[121, 1425],
-		[122, 1450],
-		[123, 1450],
-		[124, 1450],
-		[125, 1450],
-		[126, 1450],
-		[127, 1450],
-		[128, 1475],
-		[129, 1475],
-		[130, 1475],
-	]);
+	private currentSeason: Season = new Season3();
 
-	// private infoAtGameStart: RewardsTrackInfo;
-	// private xpGainedForGame: number;
+	// This is a workaround until the XP memory updates are fixed
+	private lastRewardTrackInfo: RewardsTrackInfo;
 	private xpForGameInfo: XpForGameInfo;
 
 	constructor(private readonly events: Events, private readonly memory: MemoryInspectionService) {
 		this.init();
 	}
-
-	// public async getXpGained(): Promise<number> {
-	// 	const xpForGame = await this.getXpForGameInfo();
-	// 	return xpForGame?.xpGainedWithoutBonus;
-	// }
 
 	public async getXpForGameInfo(): Promise<XpForGameInfo> {
 		return new Promise<XpForGameInfo>(async (resolve) => {
@@ -163,21 +29,59 @@ export class RewardMonitorService {
 			if (!this.xpForGameInfo) {
 				console.warn('[rewards-monitor] could not get xpForGameInfo, getting rewardsTrackInfo');
 				const rewardTrackInfo = await this.memory.getRewardsTrackInfo();
-				console.log('[rewards-monitor] got rewardsTrackInfo', rewardTrackInfo);
-				const partialInfo: XpForGameInfo = rewardTrackInfo
-					? ({
+				if (rewardTrackInfo) {
+					if (this.lastRewardTrackInfo) {
+						const levelsGained = rewardTrackInfo.Level - this.lastRewardTrackInfo.Level;
+						const xpGained =
+							levelsGained === 0
+								? rewardTrackInfo.Xp - this.lastRewardTrackInfo.Xp
+								: rewardTrackInfo.Xp +
+								  // Xp needed to finish the previous level
+								  (this.currentSeason.getXpForLevel(this.lastRewardTrackInfo.Level) -
+										this.lastRewardTrackInfo.Xp) +
+								  this.getXpForIntermediaryLevels(
+										this.lastRewardTrackInfo.Level,
+										rewardTrackInfo.Level,
+								  );
+						const xpModifier = 1 + (rewardTrackInfo?.XpBonusPercent ?? 0) / 100;
+						const rawXpGained = xpGained / xpModifier;
+						this.xpForGameInfo = {
+							previousXp: this.lastRewardTrackInfo.Xp,
+							previousLevel: this.lastRewardTrackInfo.Level,
 							currentXp: rewardTrackInfo.Xp,
 							currentLevel: rewardTrackInfo.Level,
-							bonusXp: rewardTrackInfo.XpBonusPercent,
-					  } as XpForGameInfo)
-					: null;
-				resolve(partialInfo);
+							realXpGained: xpGained,
+							xpGainedWithoutBonus: rawXpGained,
+							levelsGained: levelsGained,
+							bonusXp: rewardTrackInfo?.XpBonusPercent ? Math.round(xpGained - rawXpGained) : 0,
+							xpNeeded: this.currentSeason.getXpForLevel(rewardTrackInfo.Level),
+						};
+						console.log('[rewards-monitor] sent degraded info', this.xpForGameInfo);
+						this.lastRewardTrackInfo = rewardTrackInfo;
+						resolve(this.xpForGameInfo);
+						return;
+					} else {
+						console.log('[rewards-monitor] got rewardsTrackInfo', rewardTrackInfo);
+						const partialInfo: XpForGameInfo = rewardTrackInfo
+							? ({
+									currentXp: rewardTrackInfo.Xp,
+									currentLevel: rewardTrackInfo.Level,
+									bonusXp: rewardTrackInfo.XpBonusPercent,
+							  } as XpForGameInfo)
+							: null;
+						console.log('[rewards-monitor] sent partial info', partialInfo);
+						this.lastRewardTrackInfo = rewardTrackInfo;
+						resolve(partialInfo);
+						return;
+					}
+				}
 			}
 			resolve(this.xpForGameInfo);
+			return;
 		});
 	}
 
-	private init() {
+	private async init() {
 		this.events.on(Events.MEMORY_UPDATE).subscribe(async (data) => {
 			const changes: MemoryUpdate = data.data[0];
 			if (changes?.XpChanges?.length) {
@@ -193,7 +97,7 @@ export class RewardMonitorService {
 						? xpChange.CurrentXp - xpChange.PreviousXp
 						: xpChange.CurrentXp +
 						  // Xp needed to finish the previous level
-						  (RewardMonitorService.XP_PER_LEVEL.get(xpChange.PreviousLevel, 1500) - xpChange.PreviousXp) +
+						  (this.currentSeason.getXpForLevel(xpChange.PreviousLevel) - xpChange.PreviousXp) +
 						  this.getXpForIntermediaryLevels(xpChange.PreviousLevel, xpChange.CurrentLevel);
 				const rewardTrackInfo = await this.memory.getRewardsTrackInfo();
 				const xpModifier = 1 + (rewardTrackInfo?.XpBonusPercent ?? 0) / 100;
@@ -208,12 +112,15 @@ export class RewardMonitorService {
 					realXpGained: xpGained,
 					levelsGained: levelsGained,
 					bonusXp: rewardTrackInfo?.XpBonusPercent ? Math.round(xpGained - rawXpGained) : 0,
-					xpNeeded: RewardMonitorService.XP_PER_LEVEL.get(xpChange.CurrentLevel, 1500),
+					xpNeeded: this.currentSeason.getXpForLevel(xpChange.CurrentLevel),
 				};
 				console.log('[rewards-monitor] built xp for game', levelsGained, xpGained, this.xpForGameInfo);
 				// }
 			}
 		});
+		const rewardTrackInfo = await this.memory.getRewardsTrackInfo();
+		console.log('[rewards-monitor] initialize values from rewardsTrackInfo', rewardTrackInfo);
+		this.lastRewardTrackInfo = rewardTrackInfo;
 	}
 
 	private getXpForIntermediaryLevels(startLevel: number, endLevel: number): number {
@@ -223,7 +130,7 @@ export class RewardMonitorService {
 		}
 		let totalMissingXp = 0;
 		for (let i = startLevel + 1; i <= endLevel - 1; i++) {
-			const xpForFullLevel = RewardMonitorService.XP_PER_LEVEL.get(i, 1500);
+			const xpForFullLevel = this.currentSeason.getXpForLevel(i);
 			totalMissingXp += xpForFullLevel;
 		}
 		return totalMissingXp;
