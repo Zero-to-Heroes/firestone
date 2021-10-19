22af770661e5505d6af9336488e27b7430fabf21
sebastientromp <sebastien.tromp@gmail.com>
2021-10-19 18:07:16 +0200

    RELEASE: 8.0.18

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,15 +32,15 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '8.0.15',
+		version: '8.0.18',
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `
-					There are still some issues here and there with mercenaries (the replays don't show up immediately for instance, the replay viewer should probably be deactivated for Mercenaries games, etc.), and all of these will be fixed over the coming days. Have fun!
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `
+			// 		There are still some issues here and there with mercenaries (the replays don't show up immediately for instance, the replay viewer should probably be deactivated for Mercenaries games, etc.), and all of these will be fixed over the coming days. Have fun!
+			// 	`,
+			// },
 			{
 				type: 'main',
 				header: 'Main updates',
@@ -50,27 +50,11 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Adds a battle team widget for the player.`,
+								text: `Add a tab with a recap of all your mercenaries. It includes their level, XP, coins, current task progression, abilities, etc. and is fully sortable.`,
 							},
 							{
 								type: 'feature',
-								text: `The battle team widget now tracks how many times each ability has been used and the current cooldown left on each.`,
-							},
-							{
-								type: 'feature',
-								text: `A button as been added at the bottom of the team widget to show a recap of the roles chart (what roles does double damage to what other role). Like everything else, there is a setting to disable it if you don't want/need it.`,
-							},
-							{
-								type: 'feature',
-								text: `Adds a team widget on the bounty map (useful to easily get acces to your team when deciding on a path or picking a treasure).`,
-							},
-							{
-								type: 'feature',
-								text: `Mousing over an ability (in the team widget) or a treasure (in the treasure selection screen) now highlights its synergies (for instance, mousing over Banner of the Horde highlights all Horde mercenaries in your team)`,
-							},
-							{
-								type: 'ui',
-								text: `Improve how Mercenaries match look in the Replays tab.`,
+								text: `Add a recap of all active tasks at the bottom of the team widgets.`,
 							},
 						],
 					},
@@ -84,29 +68,12 @@ export const updates: readonly Update[] = [
 						category: 'mercenaries',
 						details: [
 							{
-								type: 'ui',
-								text: `Show max level abilities by default in the battle team widget (and when the opponent actually uses one ability, the real level is used instead).`,
+								type: 'bug',
+								text: `Properly show the remaining ability cooldowns when an enemy hero is summoned from the bench after a teammate dies. Previously, the cooldown was always set to its base value.`,
 							},
 							{
-								type: 'ui',
-								text: `Show each mercenary's current treasures in the team widget.`,
-							},
-							{
-								type: 'ui',
-								text: `Dead mercenaries are now dimmed in the team widget.`,
-							},
-							{
-								type: 'ui',
-								text: `Show 'Unknown mercenary' instead of a blank line when playing PvP in the team widget.`,
-							},
-						],
-					},
-					{
-						category: 'replays',
-						details: [
-							{
-								type: 'feature',
-								text: `Add Mercenary (PvP) and Mercenary (PvE) filters.`,
+								type: 'bug',
+								text: `Fix an issue where the opponent's and player's equipments would sometimes not be shown during a PvP battle.`,
 							},
 						],
 					},
