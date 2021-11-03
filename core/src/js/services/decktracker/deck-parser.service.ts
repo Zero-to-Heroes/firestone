96d1b291859f0d6fac2fcde3bc562d137224f6f6
sebastientromp <sebastien.tromp@gmail.com>
2021-11-03 10:03:56 +0100

    FEATURE (tracker): fix deck list for Diablo in Duels first match

--- core/src/js/services/decktracker/deck-parser.service.ts
+++ core/src/js/services/decktracker/deck-parser.service.ts
@@ -7,7 +7,7 @@ import {
 	PRACTICE_ALL,
 	ScenarioId,
 	SCENARIO_WITHOUT_RESTART,
-	SceneMode,
+	SceneMode
 } from '@firestone-hs/reference-data';
 import { CardsFacadeService } from '@services/cards-facade.service';
 import { DeckDefinition, decode, encode } from 'deckstrings';
@@ -278,6 +278,7 @@ export class DeckParserService {
 			...ARENAS,
 			ScenarioId.STANDARD_1_VS_1_GAME,
 			ScenarioId.WIZARD_DUELS,
+			ScenarioId.WIZARD_DUELS___ALTERAC_VALLEY,
 		];
 		return loggingSCenarios.includes(scenarioId);
 	}
