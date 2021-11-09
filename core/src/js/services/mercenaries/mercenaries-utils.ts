8a428a86fa85382869049ba398a038b262dcbab7
sebastientromp <sebastien.tromp@gmail.com>
2021-11-09 15:24:41 +0100

    BUG (mercs): fix detection of Sneed abilities

--- core/src/js/services/mercenaries/mercenaries-utils.ts
+++ core/src/js/services/mercenaries/mercenaries-utils.ts
@@ -10,9 +10,14 @@ export const normalizeMercenariesCardId = (
 	if (!cardId?.length) {
 		return null;
 	}
-	const skinMatch = cardId.match(/.*_(\d\d)([ab]?)$/);
+	let skinMatch = cardId.match(/.*_(\d\d)([ab]?)$/);
 	if (skinMatch) {
 		return cardId.replace(/(.*)(_\d\d)([ab]?)$/, '$1_01$3');
+	}
+	// Sometimes it is 01, sometimes 001
+	skinMatch = cardId.match(/.*_(\d\d\d)([ab]?)$/);
+	if (skinMatch) {
+		return cardId.replace(/(.*)(_\d\d\d)([ab]?)$/, '$1_001$3');
 	}
 	return cardId;
 };
@@ -23,7 +28,7 @@ export const getMercCardLevel = (cardId: string): number => {
 	}
 
 	// Generic handling of mercenaries skins or levelling
-	const skinMatch = cardId.match(/.*_(\d\d)/);
+	const skinMatch = cardId.match(/.*_(\d+)/);
 	if (skinMatch) {
 		return parseInt(skinMatch[1]);
 	}
