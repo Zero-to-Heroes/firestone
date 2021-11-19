1c20464f1bab190264c31595483124b1a52d8a93
sebastientromp <sebastien.tromp@gmail.com>
2021-11-19 17:31:10 +0100

    DEV: add debug info to subscriptions

--- core/src/js/services/ui-store/app-ui-store-facade.service.ts
+++ core/src/js/services/ui-store/app-ui-store-facade.service.ts
@@ -39,51 +39,69 @@ export class AppUiStoreFacadeService {
 	public listen$<S extends Selector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends Selector<infer T> ? T : never }> {
-		return this.store.listen$(...selectors);
+		return this.debugObservable(this.store.listen$(...selectors));
 	}
 
 	public listenPrefs$<S extends PrefsSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends PrefsSelector<infer T> ? T : never }> {
-		return this.store.listenPrefs$(...selectors);
+		return this.debugObservable(this.store.listenPrefs$(...selectors));
 	}
 
 	public listenDeckState$<S extends GameStateSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends GameStateSelector<infer T> ? T : never }> {
-		return this.store.listenDeckState$(...selectors);
+		return this.debugObservable(this.store.listenDeckState$(...selectors));
 	}
 
 	public listenBattlegrounds$<S extends BattlegroundsStateSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends BattlegroundsStateSelector<infer T> ? T : never }> {
-		return this.store.listenBattlegrounds$(...selectors);
+		return this.debugObservable(this.store.listenBattlegrounds$(...selectors));
 	}
 
 	public listenMercenaries$<S extends MercenariesStateSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends MercenariesStateSelector<infer T> ? T : never }> {
-		return this.store.listenMercenaries$(...selectors);
+		return this.debugObservable(this.store.listenMercenaries$(...selectors));
 	}
 
 	public listenMercenariesOutOfCombat$<S extends MercenariesOutOfCombatStateSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends MercenariesOutOfCombatStateSelector<infer T> ? T : never }> {
-		return this.store.listenMercenariesOutOfCombat$(...selectors);
+		return this.debugObservable(this.store.listenMercenariesOutOfCombat$(...selectors));
 	}
 
 	public listenMercenariesHighlights$<S extends MercenariesHighlightsSelector<any>[]>(
 		...selectors: S
 	): Observable<{ [K in keyof S]: S[K] extends MercenariesHighlightsSelector<infer T> ? T : never }> {
-		return this.store.listenMercenariesHighlights$(...selectors);
+		return this.debugObservable(this.store.listenMercenariesHighlights$(...selectors));
 	}
 
 	public bgHeroStats$(): Observable<readonly BgsHeroStat[]> {
-		return this.store.bgHeroStats$();
+		return this.debugObservable(this.store.bgHeroStats$());
 	}
 
 	public send(event: MainWindowStoreEvent) {
 		return this.store.send(event);
+	}
+
+	private debugObservable<T>(obs: Observable<T>) {
+		if (process.env.NODE_ENV === 'production') {
+			return obs;
+		}
+		obs.subscribe = this.overrideSubscribe(obs, obs.subscribe, new Error(), this);
+		return obs;
+	}
+
+	private overrideSubscribe(obs, oldSubscribe, error, obj) {
+		return function () {
+			const result = oldSubscribe.apply(obs, arguments);
+			result['hop'] = error.stack.split('\n');
+			result['hopContext'] = obj;
+			result['hopArgs'] = arguments;
+			return result;
+		};
 	}
 
 	private async waitForStoreInstance(): Promise<void> {
