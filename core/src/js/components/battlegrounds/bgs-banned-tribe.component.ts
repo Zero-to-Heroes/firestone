214d5b291f0bc848acaeddadcff02d8778a3b303
sebastientromp <sebastien.tromp@gmail.com>
2021-12-13 18:32:11 +0100

    UI (bgs): update design for banned tribes widget

--- core/src/js/components/battlegrounds/bgs-banned-tribe.component.ts
+++ core/src/js/components/battlegrounds/bgs-banned-tribe.component.ts
@@ -7,17 +7,23 @@ import { getTribeIcon } from '../../services/battlegrounds/bgs-utils';
 	styleUrls: [
 		'../../../css/global/components-global.scss',
 		`../../../css/global/cdk-overlay.scss`,
-		'../../../css/component/battlegrounds/bgs-banned-tribe.component.scss',
 		`../../../css/themes/battlegrounds-theme.scss`,
+		'../../../css/component/battlegrounds/bgs-banned-tribe.component.scss',
 	],
 	template: `
 		<div class="bgs-banned-tribe" *ngIf="image">
 			<div class="background"></div>
 			<img class="icon" [src]="image" />
-			<img
-				class="crossed"
-				src="https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/mulligan_discard.png"
-			/>
+			<div class="center-wrapper">
+				<div class="cross-container-outer">
+					<div class="cross-container-inner">
+						<img
+							class="crossed"
+							src="https://static.zerotoheroes.com/hearthstone/asset/coliseum/images/mulligan_discard.png"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
