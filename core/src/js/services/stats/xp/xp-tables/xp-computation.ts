84a76884e99f8f2fe7cf9a09ee0c7d5769fc5c3f
sebastientromp <sebastien.tromp@gmail.com>
2021-12-13 21:15:00 +0100

    BUG (app): fix XP needed for level in XP notif

--- core/src/js/services/stats/xp/xp-tables/xp-computation.ts
+++ core/src/js/services/stats/xp/xp-tables/xp-computation.ts
@@ -1,12 +1,14 @@
 import { Season1 } from './season-1';
 import { Season2 } from './season-2';
 import { Season3 } from './season-3';
+import { Season4 } from './season-4';
 import { Season } from './_season';
 
 export const xpSeason1 = new Season1();
 export const xpSeason2 = new Season2();
 export const xpSeason3 = new Season3();
-const allSeasons: readonly Season[] = [xpSeason1, xpSeason2, xpSeason3];
+export const xpSeason4 = new Season4();
+const allSeasons: readonly Season[] = [xpSeason1, xpSeason2, xpSeason3, xpSeason4];
 
 export const computeXpFromLevel = (fullLevel: string, timestamp: number): number => {
 	if (!fullLevel.includes('-')) {
