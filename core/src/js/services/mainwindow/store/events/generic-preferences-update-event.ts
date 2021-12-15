746e58ebadad267ccb656bf689d73466b9b17901
sebastientromp <sebastien.tromp@gmail.com>
2021-12-15 08:13:16 +0100

    BUG (app): fix incorrect navigation after updating some filters

--- core/src/js/services/mainwindow/store/events/generic-preferences-update-event.ts
+++ core/src/js/services/mainwindow/store/events/generic-preferences-update-event.ts
@@ -13,7 +13,7 @@ export class GenericPreferencesUpdateEvent implements MainWindowStoreEvent {
 	}
 
 	public isNavigationEvent(): boolean {
-		return true;
+		return false;
 	}
 
 	public isResetHistoryEvent(): boolean {
