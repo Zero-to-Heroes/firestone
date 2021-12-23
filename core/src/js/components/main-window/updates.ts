cd2c94f40f4126ad0a6b31ebd0c1aa53beb88ecc
sebastientromp <sebastien.tromp@gmail.com>
2021-12-23 09:35:57 +0100

    RELEASE: 8.4.2

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '8.4.0',
+		version: '8.4.2',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -48,6 +48,10 @@ export const updates: readonly Update[] = [
 					{
 						category: 'general',
 						details: [
+							{
+								type: 'feature',
+								text: `Firestone now sends anonymous match data to hearthstone-decks.net. It's always a pleasure to Work with passionate community members, and I hope that the data you all contribute to will help them build interesting snapshots of the game. In the meantime, if for any reason you don't want your anonymous game data to be shared, you can disable the sync in the settings.`,
+							},
 							{
 								type: 'misc',
 								text: `I have started work on fixing the memory leak. It has some pretty big implications for all the in-game overlays, so please let me know if anything is broken :)`,
@@ -65,11 +69,55 @@ export const updates: readonly Update[] = [
 				header: 'Minor updates',
 				updates: [
 					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'feature',
+								text: `Add the number of total matches you played with a hero when displaying their stats on the hero selection screen.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a bug where Fish of N'Zoth and Snake would appear in the minions list.`,
+							},
+						],
+					},
+					{
 						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
 								text: `Add card highlights for Oracle of Elune and Razormane Battleguard.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the C'Thun counter would not increment.`,
+							},
+						],
+					},
+					{
+						category: 'mercenaries',
+						details: [
+							{
+								type: 'feature',
+								text: `Don't remove completed but unclaimed tasks from the task list.`,
+							},
+						],
+					},
+					{
+						category: 'duels',
+						details: [
+							{
+								type: 'bug',
+								text: `Move Black Soulstone to Pool 2 and Grimmer Patron to Pool 1.`,
+							},
+						],
+					},
+					{
+						category: 'replays',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix a visual bug where Maestra games would not be displayed with the correct class until restarting the app.`,
 							},
 						],
 					},
@@ -79,6 +127,10 @@ export const updates: readonly Update[] = [
 							{
 								type: 'bug',
 								text: `Fix an issue where the navigation was sometimes broken after changing some dropdown filters.`,
+							},
+							{
+								type: 'bug',
+								text: `Windows in integrated mode now have a taskbar entry.`,
 							},
 						],
 					},
