81b99a8896af450ee3da2ac9305de764ebeeb664
sebastientromp <sebastien.tromp@gmail.com>
2022-01-25 09:30:08 +0100

    LOC: externalize strings for settings.decktracker.global

--- core/src/js/components/settings/decktracker/settings-decktracker-global.ts
+++ core/src/js/components/settings/decktracker/settings-decktracker-global.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LocalizationFacadeService } from '../../../services/localization-facade.service';
 import { PreferencesService } from '../../../services/preferences.service';
 import { Knob } from '../preference-slider.component';
 
@@ -18,65 +19,67 @@ import { Knob } from '../preference-slider.component';
 				<div class="subgroup">
 					<preference-toggle
 						field="overlayShowTitleBar"
-						label="Show title bar"
-						tooltip="Show/hide the deck name and cards left in hand and deck"
+						[label]="'settings.decktracker.global.show-title-bar' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-title-bar-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowControlBar"
-						label="Show control bar"
-						tooltip="Show/hide the bar with the logo and the control buttons"
+						[label]="'settings.decktracker.global.show-control-bar' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-control-bar-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						[field]="'overlayShowTooltipsOnHover'"
-						[label]="'Show card tooltips'"
+						[label]="'settings.decktracker.global.show-tooltips-on-hover' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowRarityColors"
-						label="Show rarity colors"
-						tooltip="When active, the mana cost of cards in the tracker will be colored based on the card's rarity"
+						[label]="'settings.decktracker.global.show-rarity-color' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-rarity-color-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowDeckWinrate"
-						label="Show deck winrate"
-						tooltip="When active, it shows the deck overall Ranked winrate above the tracker if we have data (in either Standard or Wild, based on the current game mode)"
+						[label]="'settings.decktracker.global.show-deck-winrate' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-deck-winrate-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowMatchupWinrate"
-						label="Show matchup winrate"
-						tooltip="When active, it shows the deck overall Ranked winrate against the opponent's class above the tracker if we have data (in either Standard or Wild, based on the current game mode)"
+						[label]="'settings.decktracker.global.show-matchup-winrate' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-matchup-winrate-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowGiftedCardsInSeparateLine"
-						label="Show gifts separately"
-						tooltip="When active, cards that have been created are displayed in their own lines (one line for each different creator)"
+						[label]="'settings.decktracker.global.show-gifts-separately' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-gifts-separately-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowStatsChange"
-						label="Show stats change"
-						tooltip="When active, some cards in the decklist will show when their stats have changed (for now only supported for Ignite)"
+						[label]="'settings.decktracker.global.show-stats-change' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-stats-change-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayShowCostReduction"
-						label="Update cost in deck"
-						tooltip="When active, the mana cost of cards in the deck whose cost has been modified reflects the new cost value."
+						[label]="'settings.decktracker.global.show-cost-reduction' | owTranslate"
+						[tooltip]="'settings.decktracker.global.show-cost-reduction' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="overlayHighlightRelatedCards"
-						label="Highlight related cards"
-						tooltip="When active, related cards will be highlighted on your decklist when mousing over specific cards (for instance, mousing over Double Jump in your decklist will highlight all Outcast cards in your deck)"
+						[label]="'settings.decktracker.global.highlight-related-cards' | owTranslate"
+						[tooltip]="'settings.decktracker.global.highlight-related-cards-tooltip' | owTranslate"
 					></preference-toggle>
 					<preference-toggle
 						field="decktrackerCloseOnGameEnd"
-						label="Close tracker on game end"
-						tooltip="Automatically close the tracker when the game ends. If disabled, the tracker needs to be closed manually"
+						[label]="'settings.decktracker.global.close-tracker-on-end' | owTranslate"
+						[tooltip]="'settings.decktracker.global.close-tracker-on-end-tooltip' | owTranslate"
 						advancedSetting
-						messageWhenToggleValue="Got it, Firestone won't close the tracker when the match ends"
+						[messageWhenToggleValue]="
+							'settings.decktracker.global.close-tracker-on-end-advanced' | owTranslate
+						"
 						[valueToDisplayMessageOn]="false"
 					></preference-toggle>
 					<preference-toggle
 						field="decktrackerShowMinionPlayOrderOnBoard"
-						[label]="'settings.decktracker.overlay.minions-play-order' | owTranslate"
-						[tooltip]="'settings.decktracker.overlay.minions-play-order-tooltip' | owTranslate"
+						[label]="'settings.decktracker.global.minions-play-order' | owTranslate"
+						[tooltip]="'settings.decktracker.global.minions-play-order-tooltip' | owTranslate"
 					></preference-toggle>
 				</div>
 			</div>
@@ -84,20 +87,22 @@ import { Knob } from '../preference-slider.component';
 			<div class="reset-container">
 				<button
 					(mousedown)="reset()"
-					helpTooltip="Reset the decktracker positions. This can solve an issue where your tracker doesn't show up anymore on screen."
+					helpTooltip="'settings.decktracker.global.reset-button-tooltip' | owTranslate"
 				>
 					<span>{{ resetText }}</span>
 				</button>
-				<div class="confirmation" *ngIf="showResetConfirmationText">
-					Decktracker positions have been reset, and will be applied in your next match.
-				</div>
+				<div
+					class="confirmation"
+					*ngIf="showResetConfirmationText"
+					[owTranslate]="'settings.decktracker.global.reset-button-confirmation'"
+				></div>
 			</div>
 		</div>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsDecktrackerGlobalComponent {
-	resetText = 'Reset positions';
+	resetText = this.i18n.translateString('settings.decktracker.global.reset-button');
 	confirmationShown = false;
 	showResetConfirmationText = false;
 
@@ -116,16 +121,16 @@ export class SettingsDecktrackerGlobalComponent {
 		},
 	];
 
-	constructor(private readonly prefs: PreferencesService) {}
+	constructor(private readonly prefs: PreferencesService, private readonly i18n: LocalizationFacadeService) {}
 
 	async reset() {
 		if (!this.confirmationShown) {
 			this.confirmationShown = true;
-			this.resetText = 'Are you sure?';
+			this.resetText = this.i18n.translateString('settings.decktracker.global.reset-button-confirm');
 			return;
 		}
 
-		this.resetText = 'Reset positions';
+		this.resetText = this.i18n.translateString('settings.decktracker.global.reset-button');
 		this.confirmationShown = false;
 		this.showResetConfirmationText = true;
 		await this.prefs.resetDecktrackerPositions();
