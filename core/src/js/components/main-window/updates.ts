80e60fd3621914fd84e3f73a02acf4e5bc7023f2
sebastientromp <sebastien.tromp@gmail.com>
2022-03-11 16:01:31 +0100

    RELEASE: 9.1.0

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,67 +32,49 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.0.0',
+		version: '9.1.0',
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `
-					The following weeks will be focused on Duels. This release adds a couple of features and bug fixes, and more is coming. Also, if you have specific wishes for Duels support in Firestone, now is the time to ping me on Discord :)
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `
+			// 		The following weeks will be focused on Duels. This release adds a couple of features and bug fixes, and more is coming. Also, if you have specific wishes for Duels support in Firestone, now is the time to ping me on Discord :)
+			// 	`,
+			// },
 			{
 				type: 'main',
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'general',
-						details: [
-							{
-								type: 'misc',
-								text: `The app is now open for translation! If you want to help, please visit <a href="https://github.com/Zero-to-Heroes/firestone-translations/blob/main/README.md" target="_blank">this link</a>.`,
-							},
-							{
-								type: 'misc',
-								text: `The part of the app that handles reading the game's memory has been improved. It should now be more robust, and you will see notifications appear when the reading process had to restart (which can degrade the app's features until it is fully restarted).`,
-							},
-						],
-					},
-					{
 						category: 'duels',
 						details: [
 							{
 								type: 'feature',
-								text: `You can now see the synergies each of the offered Treasures have with your deck. For some of them (like Orb of Revelation) I wasn't too sure about what exactly to highlight, so feel free to let me know if some of them don't make enough sense.`,
+								text: `Mousing over the hero in the hero selection stage now shows its stats (winrate, win distribution) as well as recent high-win decks. The decks shown match your Dust filter in the app (so if you have it set to 100 dust or less, it will only show you decks that you can craft for that amount).`,
+							},
+							{
+								type: 'feature',
+								text: `Mouseover stats are also available on the Hero Power selection screen, as well as on the Signature treasure screen. Be aware though that there is less data here, so the results might be less relevant.`,
 							},
 						],
 					},
 				],
 			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the selected treasure would not appear in the decklist for the first match.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `I have reversed how highlights for Drekthar works, as it feels more useful to see which minions it can pull from your decks (instead of the ones that potentially prevent it from activating).`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'duels',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the selected treasure would not appear in the decklist for the first match.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
