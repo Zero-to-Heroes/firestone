2d5871e4377d736d64decf04eaae5fead51bece6
sebastientromp <sebastien.tromp@gmail.com>
2022-04-06 15:39:04 +0200

    RELEASE: 9.4.2

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,13 +32,13 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.4.0',
+		version: '9.4.2',
 		sections: [
 			{
 				type: 'intro',
 				header: 'Giveaway time!',
 				text: `
-					See <a href="https://twitter.com/ZerotoHeroes_HS/status/1510961745195675648" target="_blank">this tweet</a> for details.
+					See <a href="https://twitter.com/ZerotoHeroes_HS/status/1510961745195675648" target="_blank">this tweet</a> for details. You have until Friday, 8th to enter.
 				`,
 			},
 			{
@@ -46,108 +46,31 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'duels',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the signature treasure would not disappear even when moving the mouse away from the selection.`,
-							},
-						],
-					},
-					{
 						category: 'general',
 						details: [
 							{
-								type: 'bug',
-								text: `Trying to fix an issue where the app would sometimes lag behind the game state by quite a bit. This has required a rewrite of an important part of the app, so please let me know if you experience some strange behaiors.`,
+								type: 'misc',
+								text: `Update for HS 23.0.`,
 							},
 						],
 					},
 				],
 			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `Now display the current rating on the session widget after a reset.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Properly identify cards in deck created by Dreadlich Tamsin's hero power.`,
-							},
-							{
-								type: 'feature',
-								text: `Add card highlight for Vectus and Shudderwock.`,
-							},
-							{
-								type: 'ui',
-								text: `Stop widgets from flickering when they are first created.`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'feature',
-								text: `Add a secondary icon to neutral heroes on the stats screen so that you can identify which class they are more easily.`,
-							},
-							{
-								type: 'feature',
-								text: `Add an option to deactivate stats on mouseover during a run preparation.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the "archive deck" button would not do anything.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where no stats overlay would appear when no stats are available.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some dual class signature treasures would not have any associated stats.`,
-							},
-						],
-					},
-					{
-						category: 'mercenaries',
-						details: [
-							{
-								type: 'bug',
-								text: `Add a few missing speed buff modifiers.`,
-							},
-							{
-								type: 'ui',
-								text: `Show buff influence as negative values when mousing over a speed modifier in a battle.`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `Improved the performance of screens where huge list of things are displayed (like the cards list screen). Displaying a list of BG matches still remains quite resource intensive though and still feels a bit a bit laggy, and I'm still working on it.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix missing rank images when using a non-English language`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Now display the current rating on the session widget after a reset.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
