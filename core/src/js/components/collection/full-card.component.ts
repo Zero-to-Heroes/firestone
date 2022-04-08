a3ef502ba2ef16aa4b88ca56d20f84a20b404c0b
Boris <43519401+piedpiper358@users.noreply.github.com>
2022-04-09 02:25:16 +0300

    Show class only if it specified
    For mercs portraits

--- core/src/js/components/collection/full-card.component.ts
+++ core/src/js/components/collection/full-card.component.ts
@@ -29,7 +29,7 @@ declare let amplitude;
 			<div class="details" scrollable>
 				<h1>{{ card.name }}</h1>
 				<div class="card-details">
-					<div class="card-info class">
+					<div class="card-info class" *ngIf="class">
 						<span class="sub-title" [owTranslate]="'app.collection.card-details.class'"></span>
 						<span class="value">{{ class }}</span>
 					</div>
