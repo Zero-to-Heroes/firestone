9e0dfc6a41a73d5bcdc7ad95636655042f05cc9e
sebastientromp <sebastien.tromp@gmail.com>
2022-04-24 09:58:03 +0200

    DEV: remove babel dependency
    Was not used anywhere

--- core/babel.config.js
+++
@@ -1,6 +0,0 @@
-// Needed for lodash-es support in tests
-module.exports = {
-	// https://stackoverflow.com/questions/58096872/react-jest-test-fails-to-run-with-ts-jest-unexpected-token-on-imported-file
-	presets: [['@babel/preset-env']],
-	plugins: [],
-};
