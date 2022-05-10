9795497f2f05e2d4ccc7482e2f034bad5b582389
sebastientromp <sebastien.tromp@gmail.com>
2022-05-10 22:28:16 +0200

    RELEASE: 9.5.10

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.5.5',
+		version: '9.5.10',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -46,113 +46,100 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'duels',
+						category: 'general',
 						details: [
 							{
 								type: 'feature',
-								text: `First (very basic) iteration of the deckbuilder. For now it only has the bare minimum, and I'll need your feedback to decide what exactly to add to it :)`,
-							},
-							{
-								type: 'feature',
-								text: `Now displays the opponent's class when mousing over the opponent's name at the top of their tracker.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where Reckoning would be greyed out if the attacking minion died in the exchange.`,
+								text: `App has been updated for patch 23.2. Let me know when you encounter bugs :) Also, the card images will be a bit longer to come this time, because of some internal changes in HS itself which broke part of my pipeline.`,
 							},
 						],
 					},
 				],
 			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Add card oracle for Commander Sivara.`,
-							},
-							{
-								type: 'feature',
-								text: `Add a counter for Grey Sage Parrot.`,
-							},
-							{
-								type: 'feature',
-								text: `Add card highlights for Witching Hour, Tess Greymane and Frizz Kindleroost.`,
-							},
-							{
-								type: 'feature',
-								text: `Card highlight for Abyssal Depths now only impacts your two lowest-cost minions.`,
-							},
-							{
-								type: 'feature',
-								text: `Now tracks the cards in deck revealed by the Joust mechanic (i.e. "reveal a card in each deck", that was introduced in TGT).`,
-							},
-							{
-								type: 'feature',
-								text: `Add "bottom of deck" support for From the Depths, Sir Finley Sea Guide, Bootstrap Sunkeneer, Phasing Portal and Forgotten Depths.`,
-							},
-							{
-								type: 'feature',
-								text: `Parrrley is now properly tracked when added to your opponent's deck.`,
-							},
-							{
-								type: 'feature',
-								text: `Lorekeeper Polkelt now resets the Bottom and Top sections of the tracker.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the player minion summoned by an enemy Dirty Rat would be treated as a played card (and mess up with some features like the Brilliant Macaw counter).`,
-							},
-							{
-								type: 'bug',
-								text: `Card highlight for Contraband Stash now doesn't include neutral cards.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where The Coin would not appear in the Hand section of your opponent's tracker.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the cost of Dragons in your deck would not be updated after playing Frizz Kindleroost.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where stats (including battle simulation, warband stats and winrate) were not updated when facing the ghost.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue about the interaction between Southsea Captain and "attack immediately" minions.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where the alternate skins of minions (like Amalgam / Fish of N'Zoth) would not work properly.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Aranna's health would jump back to 40 after her hero power activates.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where updating a minion in the simulator coming from the Replays tab would freeze the simulator.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add card oracle for Commander Sivara.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add a counter for Grey Sage Parrot.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add card highlights for Witching Hour, Tess Greymane and Frizz Kindleroost.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Card highlight for Abyssal Depths now only impacts your two lowest-cost minions.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Now tracks the cards in deck revealed by the Joust mechanic (i.e. "reveal a card in each deck", that was introduced in TGT).`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add "bottom of deck" support for From the Depths, Sir Finley Sea Guide, Bootstrap Sunkeneer, Phasing Portal and Forgotten Depths.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Parrrley is now properly tracked when added to your opponent's deck.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Lorekeeper Polkelt now resets the Bottom and Top sections of the tracker.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the player minion summoned by an enemy Dirty Rat would be treated as a played card (and mess up with some features like the Brilliant Macaw counter).`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Card highlight for Contraband Stash now doesn't include neutral cards.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where The Coin would not appear in the Hand section of your opponent's tracker.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the cost of Dragons in your deck would not be updated after playing Frizz Kindleroost.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where stats (including battle simulation, warband stats and winrate) were not updated when facing the ghost.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix a sim issue about the interaction between Southsea Captain and "attack immediately" minions.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix a sim issue where the alternate skins of minions (like Amalgam / Fish of N'Zoth) would not work properly.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where Aranna's health would jump back to 40 after her hero power activates.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where updating a minion in the simulator coming from the Replays tab would freeze the simulator.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
