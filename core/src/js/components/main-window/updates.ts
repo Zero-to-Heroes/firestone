7c1697cf0769d8c2098dde3be0dffc793daaa3fe
sebastientromp <sebastien.tromp@gmail.com>
2022-05-16 15:20:27 +0200

    RELEASE: 9.5.16

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.5.10',
+		version: '9.5.15',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -46,100 +46,104 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'general',
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where sometimes the app would stop tracking the game.`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `App has been updated for patch 23.2. Let me know when you encounter bugs :) Also, the card images will be a bit longer to come this time, because of some internal changes in HS itself which broke part of my pipeline.`,
+								text: `Add an option to not reset the "Top of Deck" and "Bottom of Deck" sections after trading a Tradeable card.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a bug where sometimes the Coin was not detected in the opponent's hand.`,
+							},
+						],
+					},
+					{
+						category: 'duels',
+						details: [
+							{
+								type: 'feature',
+								text: `The buckets (in the Deckbuilder) now show the offering rates of each card in it.`,
 							},
 						],
 					},
 				],
 			},
-			// {
-			// 	type: 'minor',
-			// 	header: 'Minor updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'decktracker',
-			// 			details: [
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Add card oracle for Commander Sivara.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Add a counter for Grey Sage Parrot.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Add card highlights for Witching Hour, Tess Greymane and Frizz Kindleroost.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Card highlight for Abyssal Depths now only impacts your two lowest-cost minions.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Now tracks the cards in deck revealed by the Joust mechanic (i.e. "reveal a card in each deck", that was introduced in TGT).`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Add "bottom of deck" support for From the Depths, Sir Finley Sea Guide, Bootstrap Sunkeneer, Phasing Portal and Forgotten Depths.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Parrrley is now properly tracked when added to your opponent's deck.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Lorekeeper Polkelt now resets the Bottom and Top sections of the tracker.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where the player minion summoned by an enemy Dirty Rat would be treated as a played card (and mess up with some features like the Brilliant Macaw counter).`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Card highlight for Contraband Stash now doesn't include neutral cards.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where The Coin would not appear in the Hand section of your opponent's tracker.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where the cost of Dragons in your deck would not be updated after playing Frizz Kindleroost.`,
-			// 				},
-			// 			],
-			// 		},
-			// 		{
-			// 			category: 'battlegrounds',
-			// 			details: [
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where stats (including battle simulation, warband stats and winrate) were not updated when facing the ghost.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix a sim issue about the interaction between Southsea Captain and "attack immediately" minions.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix a sim issue where the alternate skins of minions (like Amalgam / Fish of N'Zoth) would not work properly.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where Aranna's health would jump back to 40 after her hero power activates.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where updating a minion in the simulator coming from the Replays tab would freeze the simulator.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'minor',
+				header: 'Minor updates',
+				updates: [
+					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where Orgozoa was being incorrectly attached to Quilboars.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Perfect Games were not flagged as "Perfect" in the replays tab.`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `Add card highlight for Deepwater Evoker.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where resurrected minions would disappear from the Other zone.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where secrets countered by Improved Ice Trap would still cause the secrets helper to show.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Grey Sage Parrot's widget would show the first spell cast instead of the last one.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Charge minions were not being taken into account in the total attack widget.`,
+							},
+						],
+					},
+					{
+						category: 'duels',
+						details: [
+							{
+								type: 'bug',
+								text: `Improve deck code import / export overall. There are still some limitations caused by HS itself though (neutral Vanndar / Drek'Thar and the League of Explorers heroes).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the hero filter would not apply to your "draft decks" (the ones you created with the deckbuilder that don't have any run yet).`,
+							},
+						],
+					},
+					{
+						category: 'mercenaries',
+						details: [
+							{
+								type: 'feature',
+								text: `Add missing treasures synergies highlights.`,
+							},
+						],
+					},
+				],
+			},
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
