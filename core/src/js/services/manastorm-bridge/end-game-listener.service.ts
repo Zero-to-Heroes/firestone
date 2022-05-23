ae8def9deb8bf8c5d4bef1a223c536c9afd976a4
sebastientromp <sebastien.tromp@gmail.com>
2022-05-23 10:35:00 +0200

    LOG: remove verbose log

--- core/src/js/services/manastorm-bridge/end-game-listener.service.ts
+++ core/src/js/services/manastorm-bridge/end-game-listener.service.ts
@@ -104,7 +104,7 @@ export class EndGameListenerService {
 					console.debug('[manastorm-bridge] bgsHasPrizes', this.bgsHasPrizes);
 					break;
 				case GameEvent.GAME_END:
-					console.log('[manastorm-bridge] end game, uploading?', gameEvent);
+					console.log('[manastorm-bridge] end game, uploading?');
 					if (gameEvent.additionalData.spectating) {
 						console.log('[manastorm-bridge] spectate game, not uploading');
 						return;
