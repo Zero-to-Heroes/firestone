38ca47227a2f4b0c411576179302145742c64553
sebastientromp <sebastien.tromp@gmail.com>
2022-05-29 20:13:26 +0200

    BUG (bgs): fix issue where there are no hero stats retrieved

--- core/src/js/services/battlegrounds/bgs-global-stats.service.ts
+++ core/src/js/services/battlegrounds/bgs-global-stats.service.ts
@@ -12,11 +12,16 @@ export class BgsGlobalStatsService {
 	constructor(private readonly api: ApiRunner) {}
 
 	public async loadGlobalStats(tribes: readonly Race[]): Promise<BgsStats> {
-		const suffix = !tribes?.length || tribes?.length === 9 ? 'all-tribes' : [...tribes].sort().join('-');
+		// We know that we only have stats for groups of 5 tribes, so if we are asking for something else,
+		// we'll just get the full stats instead
+		const suffix = !tribes?.length || tribes?.length !== 5 ? 'all-tribes' : [...tribes].sort().join('-');
 		const url = BGS_STATS_RETRIEVE_URL.replace('%suffix%', suffix);
 		const result: BgsGlobalStats2 = await this.api.callGetApi(url);
 		const globalStats = BgsStats.create(result as BgsStats);
 		console.debug('retrieved global stats', globalStats);
+		if (!globalStats?.heroStats?.length) {
+			console.error('could not load bgs global stats', url, tribes, result);
+		}
 		return globalStats;
 	}
 }
