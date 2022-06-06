9a3e408e3ad72f8abb69c30043775b89a76e70e8
sebastientromp <sebastien.tromp@gmail.com>
2022-06-06 20:44:04 +0200

    RELEASE: 9.7.9

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,51 +32,25 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.7.2',
+		version: '9.7.9',
 		sections: [
-			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `
-			// 		Hello everyone! I'm just back from vacation with my family, so the pace of updates should pick up again. This first release fixes quite a few bugs and adds some missing features for the 23.0 Hearthstone release.
-			// 	`,
-			// },
+			{
+				type: 'intro',
+				header: 'Message from the dev',
+				text: `
+					IMPORTANT: I have started to receive quite a lot of error reports about the app not loading. In many case, this was caused by the Internet Service Provider not allowing access to the https://static.zerotoheroes.com domain. If you're facing issues with the app, please check out <a href="https://github.com/Zero-to-Heroes/firestone/wiki/FAQ---Troubleshooting" target="_blank">the FAQ</a> for some debugging tips.
+				`,
+			},
 			{
 				type: 'main',
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
+						category: 'general',
 						details: [
 							{
-								type: 'feature',
-								text: `Add keyboard controls for the simulator. The goal is the make the simulator feel lighter and snappiere to use.`,
-							},
-							{
-								type: 'feature',
-								text: `After upgrading the tavern, automatically show the minions list for that tavern tier if the list was currently being displayed.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some users would not see any hero stats at the start of the game.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Show the full card learned by Nagaling when mousing over it in the decklist (for people like me with short attention spans that quickly forgot what they chose and can't remember the card by its name).`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix a bug where hero power / signature treasure filters were not reset when changing the hero filter, which could lead to no results being shown without any clear reason why.`,
+								type: 'content',
+								text: `App has been updated for patch 23.4 (this was actually done last week, but I never had the time to write proper release notes). I am still receiving more error reports than usual, so expect a few more bug fixes in the coming days.`,
 							},
 						],
 					},
@@ -90,44 +64,8 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `Add an option to restrict the stats shown at the start of the game based on the current game's MMR / available tribes. WARNING: this usually leads to very low sample sizes, so only use it if you know what you're doing :)`,
-							},
-							{
 								type: 'bug',
-								text: `Fix an issue where Orgozoa could appear in the minions list when Nagas were banned.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Yo-Ho Ogre "attack immediately" timing could not go off between windfury attacks.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Wildfire Elemental excess damage would propagate to neighbours.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where the minion sacrificed by Tamsin's Hero Power could still be attacked on the first attack phase.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue when handling Southsea Captain dying when they had Reborn from The Lich King's hero power.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where the board's composition (for Start of Combat triggers like Red Whelps) would only be computed after Illidan's Hero Power triggered (which would cause dead minions to not be counted).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where minions who died after being attacked by Illidan's hero power could still be attacked in the next normal attack phase.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where the turn order would get messed up after Illidan's hero power activation.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Illidan's second attacker bonus would transfer to another minion if the second attacker died before being able to attack (e.g. when killed by a Ghoul going off).`,
+								text: `Fix a sim issue where the minion summoned by Ozumat's Tentacular hero power would sometimes not be taken into account.`,
 							},
 						],
 					},
@@ -136,27 +74,27 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Mousing over Kazalusan in the decklist now also highlights dragons that were played this game (in the Other zone).`,
-							},
-							{
-								type: 'feature',
-								text: `Add Card Oracle (flagging the card in the opponent's hand) for Felsoul Jailer.`,
-							},
-							{
-								type: 'feature',
-								text: `Add card highlight for Swordfish and Gorloc Ravager.`,
-							},
-							{
-								type: 'feature',
-								text: `Add decklist support for Heroic Brawliseum.`,
+								text: `Add Bottom of Deck support for Disarming Elemental and Drown.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix a bug where the tracker would incorrectly remove a "Bottom of deck" card after drawing another copy from the deck.`,
+								text: `Fix an info leak when drawing a card from a tutor (e.g. a "draw a spell" card) while having one copy in the deck and one copy in the "Bottom of deck" zone. The tracker used to tell you exactly which copy you drew. Now, it assumes that the copy from the "in deck" zone was drawn.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix Card Oracle for Zola the Gorgon and Bronze Herald.`,
+								text: `Fix Card Oracle (flagging the card in the opponent's hand) for Loan Shark.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix info leak where cards burned by Immolate would be revealed in the Other zone.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix info leak where Coilfang Constrictor would reveal too much information. As a result, you won't even see the revealed cards in the "In Hand" section, as showing this info while managing the memory leak would lead to too much spaghetti code.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a bug where Rush minions would be counted in the attack counter the turn they were played.`,
 							},
 						],
 					},
@@ -164,12 +102,16 @@ export const updates: readonly Update[] = [
 						category: 'duels',
 						details: [
 							{
-								type: 'bug',
-								text: `Remove the Uldum Disks of Legends passive from the stats.`,
+								type: 'feature',
+								text: `You can now click on a card in a bucket to add it to your decklist (if it is a valid card for deckbuilding).`,
 							},
 							{
 								type: 'bug',
-								text: `Fix a deckbuilder issue where switching Signature Treasures for one of the neutral heroes would not update the available cards pool.`,
+								text: `Fix a bug where the deckbuilder would sometimes offer non-Core version of some cards. I'm still not sure exactly what the rules are when several versions of the same cards can be added to your deck, so expect a few more bugs / improvements needed on that one.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a bug where deck codes for heroes with dual-class signature treasures would sometimes be incorrect.`,
 							},
 						],
 					},
@@ -178,16 +120,16 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'bug',
-								text: `Fix a bug where rewards would disappera from the Arena Runs list after completing a new run.`,
+								text: `Available secrets for the secrets helper should now be updated.`,
 							},
 						],
 					},
 					{
-						category: 'mercenaries',
+						category: 'collection',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where some Mercs replays would not upload properly.`,
+								text: `Voyage to the Sunken City is now once more part of the Standard sets.`,
 							},
 						],
 					},
