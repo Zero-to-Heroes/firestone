82698b6255400b85980605cc85856f03cda3b42e
sebastientromp <sebastien.tromp@gmail.com>
2022-06-24 11:12:05 +0200

    RELEASE: 9.8.5

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -41,21 +41,25 @@ export const updates: readonly Update[] = [
 			// 		IMPORTANT: I have started to receive quite a lot of error reports about the app not loading. In many case, this was caused by the Internet Service Provider not allowing access to the https://static.zerotoheroes.com domain. If you're facing issues with the app, please check out <a href="https://github.com/Zero-to-Heroes/firestone/wiki/FAQ---Troubleshooting" target="_blank">the FAQ</a> for some debugging tips.
 			// 	`,
 			// },
-			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'duels',
-			// 			details: [
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Add a "Buckets" tab that lets you browse the contents of all available buckets.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'main',
+				header: 'Main updates',
+				updates: [
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `Add a way to see your stats aggregated over any number of decks.`,
+							},
+							{
+								type: 'feature',
+								text: `Add a turn timer.`,
+							},
+						],
+					},
+				],
+			},
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -78,6 +82,50 @@ export const updates: readonly Update[] = [
 							{
 								type: 'bug',
 								text: `Fix a sim issue where Tamsin's Hero Power would only target 4 minions.`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `Add card highlight for Kindle and The Upper Hand, two buffs offered by Bob in Dungeon Runs.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the Secrets Helper would offer rotated out improved secrets when Tavish was played.`,
+							},
+						],
+					},
+					{
+						category: 'replays',
+						details: [
+							{
+								type: 'feature',
+								text: `You can now select multiple decks in the "filter by deck" dropdown.`,
+							},
+						],
+					},
+					{
+						category: 'collection',
+						details: [
+							{
+								type: 'bug',
+								text: `Remove the pity timers from the mini-sets.`,
+							},
+							{
+								type: 'ui',
+								text: `Improve the display of the Pack Stats screen by grouping packs in a more logical way, and sorting the main set packs by the set's release date.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where tooltips were sometimes positioned in a weird way.`,
 							},
 						],
 					},
