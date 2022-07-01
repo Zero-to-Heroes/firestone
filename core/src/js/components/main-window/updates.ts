863d059ef9eadcf48b1317dfcfc7bd8c95cd3d8c
sebastientromp <sebastien.tromp@gmail.com>
2022-07-01 10:27:38 +0200

    RELEASE: 9.8.10

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,29 +32,43 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.8.6',
+		version: '9.8.10',
 		sections: [
-			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `
-			// 		IMPORTANT: I have started to receive quite a lot of error reports about the app not loading. In many case, this was caused by the Internet Service Provider not allowing access to the https://static.zerotoheroes.com domain. If you're facing issues with the app, please check out <a href="https://github.com/Zero-to-Heroes/firestone/wiki/FAQ---Troubleshooting" target="_blank">the FAQ</a> for some debugging tips.
-			// 	`,
-			// },
+			{
+				type: 'intro',
+				header: 'Message from the dev',
+				text: `
+					IMPORTANT: In the future, decks you play with might be shared (anonymously) with the community if they perform well at a high rank. If you don't want others to find out about your secret decklists (I think that can especially be true for high-level Legend players), please turn off the new "Allow game sharing" option under Settings > General.
+				`,
+			},
 			{
 				type: 'main',
 				header: 'Main updates',
 				updates: [
 					{
+						category: 'general',
+						details: [
+							{
+								type: 'feature',
+								text: `Add an option to not let the app shre your decks with the community (see announcement above).`,
+							},
+						],
+					},
+					{
 						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `Add a way to see your stats aggregated over any number of decks.`,
+								text: `Now shows the cards you are missing when building a deck, as well as the dust you need to craft the missing cards (also applies to the Duels deckbuilder).`,
 							},
+						],
+					},
+					{
+						category: 'duels',
+						details: [
 							{
-								type: 'feature',
-								text: `Add a turn timer.`,
+								type: 'bug',
+								text: `Fix an issue where the deckbuilder wouldn't work anymore.`,
 							},
 						],
 					},
@@ -65,87 +79,27 @@ export const updates: readonly Update[] = [
 				header: 'Minor updates',
 				updates: [
 					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Wildfire Element would cause deathrattles and reborn effects to happen first for the main target, then the collateral damage (while in the game all minions die at the same time).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where reborn deathrattle minions would spawn left of their deathrattles, instead of right.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where deathrattle with single targets (Impulsive Trickster, Selfless Hero, etc.) could target dead or dying minions.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Tamsin's Hero Power would only target 4 minions.`,
-							},
-						],
-					},
-					{
 						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `Add card highlight for Kindle and The Upper Hand, two buffs offered by Bob in Dungeon Runs.`,
+								text: `Add global effect for Prince Renathal.`,
 							},
 							{
 								type: 'feature',
-								text: `Add the ability to import a deck code in the deckbuilder.`,
+								text: `Add card highlight for Grave Defiler.`,
 							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the Secrets Helper would offer rotated out improved secrets when Tavish was played.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where cards put at the top of the opponent's deck would never be removed from their deck.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where cards dredged by Disarming Element would not appear in the Bottom of the opponent's deck.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix Card Oracle for Bloodsail Flybooter.`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
 							{
 								type: 'feature',
-								text: `Add the ability to import a deck code in the deckbuilder.`,
+								text: `Flag the exact secrets put in play by Horde Operative.`,
 							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the Bucket cards were not localized.`,
-							},
-						],
-					},
-					{
-						category: 'replays',
-						details: [
 							{
 								type: 'feature',
-								text: `You can now select multiple decks in the "filter by deck" dropdown.`,
+								text: `Update the number of cards that make a valid Renathal deck to 40.`,
 							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
 							{
 								type: 'bug',
-								text: `Remove the pity timers from the mini-sets.`,
-							},
-							{
-								type: 'ui',
-								text: `Improve the display of the Pack Stats screen by grouping packs in a more logical way, and sorting the main set packs by the set's release date.`,
+								text: `Fix an issue where a constructed deck could be used by the tracker instead of your Arena deck.`,
 							},
 						],
 					},
@@ -153,8 +107,8 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where tooltips were sometimes positioned in a weird way.`,
+								type: 'ui',
+								text: `Keep the mana color of missing cards, instead of showing them with a white background mana cost.`,
 							},
 						],
 					},
