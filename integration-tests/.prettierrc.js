8d493337f830421db2c5fc1d5e3cb569b66d5c22
sebastientromp <sebastien.tromp@gmail.com>
2022-07-08 09:13:03 +0200

    DEV: delete integration-tests sub-project

--- integration-tests/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	semi: true,
-	trailingComma: 'all',
-	singleQuote: true,
-	tabWidth: 4,
-	useTabs: true,
-	printWidth: 120,
-	quoteProps: 'preserve', // Needed for angular templates, like ngClass
-};
