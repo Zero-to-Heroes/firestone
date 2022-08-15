042db080ae7ff6079356325924c92ea81a1e9553
Piedpiper <43519401+piedpiper358@users.noreply.github.com>
2022-08-15 14:55:23 +0300

    Default duel deck name
    Thank you, everything worked out, but not for this case. What can I do here?

--- core/src/js/services/ui-store/duels-ui-helper.ts
+++ core/src/js/services/ui-store/duels-ui-helper.ts
@@ -431,7 +431,7 @@ const getTopDeck = (
 		buildNumberAtStart: deckStat.buildNumber,
 	};
 	const deckSummary: DuelsDeckSummary = {
-		deckName: 'tmp',
+		deckName: `[owTranslate]="'app.duels.deck-stat.default-deck-name-blank'"`, 
 		runs: [run],
 		initialDeckList: deckStat.decklist ?? run.initialDeckList,
 		heroCardId: deckStat.heroCardId,
