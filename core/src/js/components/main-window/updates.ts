b6522a9dc3b4c07b2e21ef77370aea7ae2c0a303
sebastientromp <sebastien.tromp@gmail.com>
2022-09-12 15:07:26 +0200

    RELEASE: 9.10.1

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.10.0',
+		version: '9.10.1',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -46,11 +46,24 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'feature',
+								text: `Add a widget to easily see all your current Battlegrounds quests.`,
+							},
+						],
+					},
+					{
 						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `Add a widget to easily see all your current Hearthstone (not BG) quests.`,
+								text: `Add a widget to easily see all your current Hearthstone quests.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where leaving the game while spectating (without closing the app) would prevent the app from recording further games until it is restarted.`,
 							},
 						],
 					},
@@ -69,6 +82,32 @@ export const updates: readonly Update[] = [
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix cards created by Magnetic Mines (Rattlegore's hero power).`,
+							},
+							{
+								type: 'bug',
+								text: `Add the "gift" icon in hand for some more cards.`,
+							},
+						],
+					},
+					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix a sim issue when Tamsin uses her hero power on an empty board.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue when gilding already golden minions.`,
+							},
+						],
+					},
 					{
 						category: 'duels',
 						details: [
