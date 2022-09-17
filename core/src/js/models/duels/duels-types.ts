57b90793883b6270a68201fb43570a613342705e
Piedpiper <43519401+piedpiper358@users.noreply.github.com>
2022-09-18 02:24:15 +0300

    Make duels dust filter options multiples of cards costs

--- core/src/js/models/duels/duels-types.ts
+++ core/src/js/models/duels/duels-types.ts
@@ -1,6 +1,1 @@
-export type DuelsTopDecksDustFilterType = 'all' | '1000' | '500' | '200' | '100' | '40' | '0';
-export type DuelsUnlocksFilterType =
-	| 'all'
-	| 'unlocked'
-	// That third option is not available in the UI, but only for some services
-	| 'reveal-locked-hero-powers';
+export type DuelsTopDecksDustFilterType = 'all' | '3200' | '1600' | '400' | '100' | '40' | '0';
