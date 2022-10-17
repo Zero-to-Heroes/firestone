84c9c02af3df548b6dd080be3439702c5dce7f59
sebastientromp <sebastien.tromp@gmail.com>
2022-10-17 18:26:30 +0200

    RELEASE: 9.13.8

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.13.7',
+		version: '9.13.8',
 		sections: [
 			{
 				type: 'intro',
@@ -66,12 +66,20 @@ export const updates: readonly Update[] = [
 								text: `Add support for multiple activations of Dew Process (via Solar Eclipse for instance) in the "global effects" section to better track how many cards you will draw.`,
 							},
 							{
+								type: 'feature',
+								text: `Multiple Nagalings now each appear on its one line in the tracker, so that it's easier to differentiate the various spells they learned.`,
+							},
+							{
 								type: 'bug',
 								text: `Fix an issue where "Unknown card"s would be removed from the list every time a card was played.`,
 							},
 							{
 								type: 'bug',
 								text: `Fix an info leak that revelead the card's cost and rarity in the "Hand" section of the tracker when drawing a card from the deck that was previously reveleaed (like Renathal).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an info leak with Reno's Crafty Lasso.`,
 							},
 						],
 					},
@@ -82,6 +90,10 @@ export const updates: readonly Update[] = [
 								type: 'bug',
 								text: `Fix an issue where the opponent board overview would be half hidden if using both "display at the bottom" and "increased size" options.`,
 							},
+							{
+								type: 'bug',
+								text: `Fix a sim issues with Leapfroggers.`,
+							},
 						],
 					},
 					{
@@ -91,11 +103,19 @@ export const updates: readonly Update[] = [
 								type: 'feature',
 								text: `The values for the "dust filter" dropdown now match more closely the dust values of the different rarities (Community Contribution!).`,
 							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the deck was not always detected during the first game of a run.`,
+							},
 						],
 					},
 					{
 						category: 'mercenaries',
 						details: [
+							{
+								type: 'ui',
+								text: `Make cooldown information more visible on abilities with cooldowns.`,
+							},
 							{
 								type: 'bug',
 								text: `Fix an issue where some speed buffs (like Alexstrasza's Amulet of Swiftness) were incorrectly applied to all abilities.`,
