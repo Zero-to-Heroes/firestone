66ec5874163433ebc90b4e4ab955d0ae02f855e8
sebastientromp <sebastien.tromp@gmail.com>
2022-10-18 14:18:09 +0200

    BUG (mercs): fix issue where some spawns are flagged as "Unknown mercenary"

--- core/src/js/services/mercenaries/parser/mercenaries-hero-updated-parser.ts
+++ core/src/js/services/mercenaries/parser/mercenaries-hero-updated-parser.ts
@@ -33,24 +33,32 @@ export class MercenariesHeroUpdatedParser implements MercenariesParser {
 			return battleState;
 		}
 		if (!cardId) {
+			console.debug('[merc-hero-revealed-parser] no card Id', event);
 			return battleState;
 		}
 
 		const normalizedCardId = normalizeMercenariesCardId(cardId);
-		const refData = await mainWindowState?.mercenaries?.referenceData;
+		const refData = mainWindowState?.mercenaries?.referenceData;
+
+		// Sometimes the spawns are not referenced in the data (like for Ahune's Ice Shards),
+		// so it's possible that this doesn't return anything
 		const refMerc = refData?.mercenaries?.find(
 			(merc) =>
 				normalizeMercenariesCardId(this.allCards.getCardFromDbfId(merc.cardDbfId).id) === normalizedCardId,
 		);
-		if (!refMerc) {
-			return battleState;
-		}
 
 		const refMercCard = this.allCards.getCard(normalizedCardId);
 		const refMercEquipment = this.allCards.getCardFromDbfId(event.additionalData?.mercenariesEquipmentId);
 		const isPlayer = controllerId === localPlayer.PlayerId;
 		const team = isPlayer ? battleState.playerTeam : battleState.opponentTeam;
 		const turnsElapsed = Math.max(0, battleState.currentTurn - 1);
+
+		const existingMerc = team.getMercenary(entityId);
+		if (!existingMerc) {
+			console.warn('[merc-hero-updated] trying to update non-existing merc', entityId, cardId);
+			return battleState;
+		}
+
 		// console.debug('turnsElapsed', turnsElapsed, battleState.currentTurn, battleState);
 		const newTeam = team.updateMercenary(
 			entityId,
@@ -58,24 +66,25 @@ export class MercenariesHeroUpdatedParser implements MercenariesParser {
 				cardId: refMercCard.id,
 				zone: event.additionalData.zone,
 				zonePosition: event.additionalData.zonePosition,
-				abilities: refMerc.abilities.map((refAbility) => {
-					const refTier = [...refAbility.tiers].sort((a, b) => a.tier - b.tier).pop();
-					if (!refTier) {
-						console.warn('could not find refTier', refAbility?.tiers, refAbility.abilityId, refAbility);
-						return null;
-					}
-					const refCard = this.allCards.getCardFromDbfId(refTier.cardDbfId);
-					return BattleAbility.create({
-						entityId: null,
-						cardId: refCard.id,
-						level: refTier.tier,
-						cooldown: refCard.mercenaryAbilityCooldown ?? 0,
-						cooldownLeft: Math.max(0, (refCard.mercenaryAbilityCooldown ?? 0) - turnsElapsed),
-						speed: refCard.cost ?? 0,
-						totalUsed: null,
-						isTreasure: false,
-					});
-				}),
+				abilities:
+					refMerc?.abilities.map((refAbility) => {
+						const refTier = [...refAbility.tiers].sort((a, b) => a.tier - b.tier).pop();
+						if (!refTier) {
+							console.warn('could not find refTier', refAbility?.tiers, refAbility.abilityId, refAbility);
+							return null;
+						}
+						const refCard = this.allCards.getCardFromDbfId(refTier.cardDbfId);
+						return BattleAbility.create({
+							entityId: null,
+							cardId: refCard.id,
+							level: refTier.tier,
+							cooldown: refCard.mercenaryAbilityCooldown ?? 0,
+							cooldownLeft: Math.max(0, (refCard.mercenaryAbilityCooldown ?? 0) - turnsElapsed),
+							speed: refCard.cost ?? 0,
+							totalUsed: null,
+							isTreasure: false,
+						});
+					}) ?? existingMerc.abilities,
 				level: getMercLevelFromExperience(event.additionalData.mercenariesExperience, refData),
 				role: getHeroRole(refMercCard.mercenaryRole),
 				equipment: BattleEquipment.create({
