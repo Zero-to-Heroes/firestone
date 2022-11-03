ff5a55c718ed07a150aa0f85953b60bf39a374e1
sebastientromp <sebastien.tromp@gmail.com>
2022-11-03 09:11:41 +0100

    RELEASE: 9.14.3

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.14.1',
+		version: '9.14.3',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -47,97 +47,28 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'feature',
-								text: `Add a "live stream" tab where you can see who is streaming Hearthstone (and using Firestone). It is still in its very early stage, and am looking for feedback in how to make this as useful as possible for viewers and streamers alike`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `Add an option to show all Battlecry / Deathrattle minions in the minions list (this adds two more stars next to the tiers 1-6 in the minions list).`,
+								type: 'content',
+								text: `App has been updated for 24.6`,
 							},
 						],
 					},
 				],
 			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `Add a minion list option to group tribeless minions into their base tribe (e.g. show Wrath Weaver inside the "Demons" group).`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Show the cards replayed by Tess / Contraband Stash in the tooltip when mousing over Tess / Stash.`,
-							},
-							{
-								type: 'feature',
-								text: `Add counter for Queensguard.`,
-							},
-							{
-								type: 'feature',
-								text: `Specific card widgets should now appear when you are offered these cards in Discovers to help you make the best decision.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where secrets created by Nagalings would not trigger the Secrets Helper.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an info leak where Chameleos would reveal the position of the copied card in the opponent's hand.`,
-							},
-						],
-					},
-					{
-						category: 'mercenaries',
-						details: [
-							{
-								type: 'feature',
-								text: `Add an option to use the new Tasks widget (similar to what already exists in constructed / battlegrounds).`,
-							},
-							{
-								type: 'bug',
-								text: `Hide the "Map turns" when playing PvP.`,
-							},
-							{
-								type: 'bug',
-								text: `Hide speed value for abilities with Discover and Battlecry effects (Community Contribution!).`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'feature',
-								text: `Add faction and race info to full-card component (faction only for mercs) (Comunity Contribution!).`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the rating graphs would sometimes tell you to select a region.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add a minion list option to group tribeless minions into their base tribe (e.g. show Wrath Weaver inside the "Demons" group).`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
