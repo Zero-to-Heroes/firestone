93203066db250e8d4ba4bdb698485bb24facc2ee
sebastientromp <sebastien.tromp@gmail.com>
2022-11-16 19:47:17 +0100

    RELEASE: 9.16.1

--- core/src/js/components/main-window/updates.ts
+++ core/src/js/components/main-window/updates.ts
@@ -32,7 +32,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '9.16.0',
+		version: '9.16.1',
 		sections: [
 			// {
 			// 	type: 'intro',
@@ -56,12 +56,39 @@ export const updates: readonly Update[] = [
 							},
 						],
 					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `When playing "Guess the Weight", the tracker now shows you the odds for each option. While this particular card is not super popular (being a Wild-only card), it opens the door to showing more contextual information when being offered a discover. Let me know what you'd like to use this for on Discord!`,
+							},
+						],
+					},
+					{
+						category: 'collection',
+						details: [
+							{
+								type: 'feature',
+								text: `The Pity Timers are now on the "Packs" tab. The diversity of bundles and pack types made it confusing to show the pity timers as linked to sets, so they will now be tied to their specific pack type.`,
+							},
+						],
+					},
 				],
 			},
 			{
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
+					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where the Simulator tooltip would get messed up after going once to the Replays tab (Community Contribution!).`,
+							},
+						],
+					},
 					{
 						category: 'mercenaries',
 						details: [
