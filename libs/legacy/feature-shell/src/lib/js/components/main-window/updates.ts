860668f36679dd4e5314673425d6ee58be54c489
sebastientromp <sebastien.tromp@gmail.com>
2022-12-29 10:51:21 +0100

    RELEASE: 10.0.23

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -32,80 +32,50 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '10.0.20',
+		version: '10.0.23',
 		sections: [
+			{
+				type: 'intro',
+				header: 'Message from the dev',
+				text: `I wanted to thank you for using Firestone. It means a lot to me that so many of you use something that I made. I wish you all a very happy new year.`,
+			},
 			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `This release, and probably the next couple ones, will focus on bug fixes and often requested Quality of Life improvements. So nothing super exciting, but I hope you'll enjoy the updates nonetheless.`,
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Loaner decks are now properly supported.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
 			// },
 			{
-				type: 'main',
-				header: 'Main updates',
+				type: 'minor',
+				header: 'Minor updates',
 				updates: [
 					{
 						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `Loaner decks are now properly supported.`,
+								text: `The app now properly tracks multiple global effects triggered when Brann is present.`,
 							},
-						],
-					},
-				],
-			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'battlegrounds',
-						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where the turn-by-turn winrates in the Battles tab would not update until you got a non-0 winrate chance.`,
+								type: 'feature',
+								text: `Playing Lor'Themar now properly doubles the C'Thun size counter.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where some games were incorrectly flagged as "perfect".`,
-							},
-							{
-								type: 'ui',
-								text: `Improve how the "quick opponent recap" (brought up by pressing Tab while in lobby) looks on high resolution screens.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an info leak where "created by" cards don't show an icon in the opponent's hand, when drawn by a tutor. This would give some info about the card being drawn.`,
+								text: `Fix multiple display issues when using the tracker in the "legacy" display mode.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where stolen cards (e.g. by Theotar) are taken into account when building the DK runes widget.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some cards that upgrade when played (like Ignite) have their buff not properly tracked in deck when played by an effect (e.g. Nagaling).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Sivara would show ALL the spells played (instead of the first 3) when mousing over it in the decklist.`,
-							},
-							{
-								type: 'bug',
-								text: `Ignore ties when building deck stats.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'bug',
-								text: `Add missing Deathknight and Golden Wild packs.`,
+								text: `Fix an issue where cards played by effects (e.g. Nagaling) were not always properly handled in the tracker.`,
 							},
 						],
 					},
@@ -114,7 +84,7 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Tutorial quests are now tracked by the quest widget.`,
+								text: `Add an option (under Settings > General) to disable the Mailbox, or hide the Mailbox' unread messages red dot.`,
 							},
 						],
 					},
