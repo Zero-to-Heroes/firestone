ee90989b1702aa7ed311c45b3e980335ea74c02f
sebastientromp <sebastien.tromp@gmail.com>
2023-01-20 08:54:42 +0100

    DEV: create abstract-subscription component
    No reliance on any Firestone-specific stuff

--- libs/legacy/feature-shell/src/lib/js/components/abstract-subscription-store.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/abstract-subscription-store.component.ts
@@ -1,45 +1,15 @@
-import { ChangeDetectorRef, HostListener, Injectable, OnDestroy, ViewRef } from '@angular/core';
-import { Observable, pipe, Subject, UnaryFunction } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, takeUntil, tap } from 'rxjs/operators';
+import { ChangeDetectorRef, Injectable } from '@angular/core';
+import { AbstractSubscriptionComponent } from '@components/abstract-subscription.component';
 import { Preferences } from '../models/preferences';
 import { AppUiStoreFacadeService } from '../services/ui-store/app-ui-store-facade.service';
-import { arraysEqual } from '../services/utils';
 
 @Injectable()
-export abstract class AbstractSubscriptionStoreComponent implements OnDestroy {
-	protected destroyed$ = new Subject<void>();
-
-	constructor(protected readonly store: AppUiStoreFacadeService, protected readonly cdr: ChangeDetectorRef) {}
-
-	@HostListener('window:beforeunload')
-	ngOnDestroy() {
-		this.destroyed$.next();
-		this.destroyed$.complete();
+export abstract class AbstractSubscriptionStoreComponent extends AbstractSubscriptionComponent {
+	constructor(protected readonly store: AppUiStoreFacadeService, protected readonly cdr: ChangeDetectorRef) {
+		super(cdr);
 	}
 
 	protected listenForBasicPref$<T>(selector: (prefs: Preferences) => T, ...logArgs: any[]) {
 		return this.store.listenPrefs$((prefs) => selector(prefs)).pipe(this.mapData(([pref]) => pref));
-	}
-
-	protected mapData<T, R>(
-		extractor: (arg: T) => R,
-		equality = null,
-		debounceTimeMs = 100,
-	): UnaryFunction<Observable<T>, Observable<R>> {
-		return pipe(
-			debounceTime(debounceTimeMs),
-			distinctUntilChanged(equality ?? ((a, b) => arraysEqual(a, b))),
-			map(extractor),
-			distinctUntilChanged(equality ?? ((a, b) => arraysEqual(a, b))),
-			tap((filter) =>
-				setTimeout(() => {
-					if (!(this.cdr as ViewRef)?.destroyed) {
-						this.cdr.detectChanges();
-					}
-				}, 0),
-			),
-			// share(), // TODO: test this when I have time to do it properly
-			takeUntil(this.destroyed$),
-		);
 	}
 }
