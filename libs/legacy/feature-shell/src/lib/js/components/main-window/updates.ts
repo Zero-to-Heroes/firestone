dd5e8489e6bf4fe7a31ce3cba6589d78efe76d3b
sebastientromp <sebastien.tromp@gmail.com>
2023-01-31 16:44:28 +0100

    RELEASE: 10.2.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -32,59 +32,43 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '10.1.9',
+		version: '10.2.0',
 		sections: [
 			// {
 			// 	type: 'intro',
 			// 	header: 'Message from the dev',
 			// 	text: `Thank you all for using Firestone. It means a lot to me that so many of you use something that I made. I wish you a very happy new year.`,
 			// },
-			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'general',
-			// 			details: [
-			// 				{
-			// 					type: 'content',
-			// 					text: `App has been fully updated for patch 25.2. I'm still looking for a way to properly support Professor Putricide creations in the simulator, so stay tuned. Enjoy the new patch :)`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
 			{
-				type: 'minor',
-				header: 'Minor updates',
+				type: 'main',
+				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
+						category: 'general',
 						details: [
 							{
 								type: 'feature',
-								text: `Add counter for Magmaloc.`,
-							},
-							{
-								type: 'content',
-								text: `Remove buddies from the simulator minions list.`,
-							},
-							{
-								type: 'bug',
-								text: `Try to fix an issue where your new MMR can be empty when you finish 1st or 2nd.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue with aura minions (like Cyborg Drake, Southsea Captain, etc.).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue when Putricide's creation had Reborn.`,
+								text: `Mods support is almost ready! I have added more visual feedback, and a way to automatically update the mods from the Mods screen in the settings. If you'd like to give it a try before it hits the live build (and help me make sure everything works properly ^^), please let me know on Discord.`,
 							},
 						],
 					},
 				],
 			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add counter for Magmaloc.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
