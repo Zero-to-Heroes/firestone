89caeae08bde36436f0bb65d3b9ef045b30836e9
sebastientromp <sebastien.tromp@gmail.com>
2023-03-06 09:06:00 +0100

    DEV: remove deprecated tip

--- libs/legacy/feature-shell/src/lib/js/services/tip.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/tip.service.ts
@@ -5,12 +5,13 @@ import { LocalizationFacadeService } from '@services/localization-facade.service
 export class TipService {
 	private numberOfTips = 11;
 	private previousIndex = -1;
+	private excludedTips = [7];
 
 	constructor(private readonly i18n: LocalizationFacadeService) {}
 
 	public getRandomTip(): string {
 		let randomIndex = this.previousIndex;
-		while (randomIndex === this.previousIndex) {
+		while (randomIndex === this.previousIndex || this.excludedTips.includes(randomIndex)) {
 			randomIndex = Math.floor(Math.random() * this.numberOfTips);
 		}
 		this.previousIndex = randomIndex;
