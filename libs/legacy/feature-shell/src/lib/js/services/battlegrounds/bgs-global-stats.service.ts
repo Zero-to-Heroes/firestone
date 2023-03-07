dfba3ba414d55c8233896f719c1ce7106193138d
sebastientromp <sebastien.tromp@gmail.com>
2023-03-07 16:13:25 +0100

    BUG (bgs): fix issue preventing the app to load when bgs global stats API call fails

--- libs/legacy/feature-shell/src/lib/js/services/battlegrounds/bgs-global-stats.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/battlegrounds/bgs-global-stats.service.ts
@@ -26,10 +26,10 @@ export class BgsGlobalStatsService {
 		);
 		const result: BgsGlobalStats2 = await this.api.callGetApi(url);
 		const globalStats = BgsStats.create({
-			allTribes: result.allTribes,
-			heroStats: result.heroStats,
-			lastUpdateDate: result.lastUpdateDate,
-			mmrPercentiles: result.mmrPercentiles,
+			allTribes: result?.allTribes ?? [],
+			heroStats: result?.heroStats ?? [],
+			lastUpdateDate: result?.lastUpdateDate,
+			mmrPercentiles: result?.mmrPercentiles ?? [],
 		} as BgsStats);
 		if (!globalStats?.heroStats?.length) {
 			console.error('could not load bgs global stats', url, tribes, result);
