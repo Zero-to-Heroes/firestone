98f98fe9ddd0a5ae90d26fa10bf7f14125c90269
sebastientromp <sebastien.tromp@gmail.com>
2023-03-13 08:40:45 +0100

    RELEASE: 10.3.7

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,17 +33,17 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '10.3.4',
+		version: '10.3.7',
 		force: true,
 		sections: [
 			{
 				type: 'intro',
 				header: 'Message from the dev',
-				text: `I am considering changes to the Premium accounts, and move some features to be premium-only. You can read more about it <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">here</a>, but the TL;DR is:
+				text: `IMPORTANT: Premium accounts are undergoing some changes, and some features are moving to be premium-only. You can read more about it <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">here</a>, but the TL;DR is:
 				<ul>
 					<li>The ads business model is showing its limits, and I need to change how the app generates revenues to be able to continue doing this full-time</li>
-					<li>Premium will move from $3-remove-the-ads to $5-remove-the-ads-and-get-more-features</li>
-					<li>As much as possible, pure / low-processed data will be free. Premium features will be QoL and high-processing stuff</li>
+					<li>Premium will move from $3-remove-the-ads to $5-remove-the-ads-and-get-more-features. The change in price will be in effect starting next week.</li>
+					<li>As much as possible, pure / low-processed data will be free. Premium features will be QoL and stuff that require a lot of processing</li>
 				</ul>
 				This is still experimental, so let me know what you think!<br/>
 				(PS: This message exceptionnaly ignores your "don't show me release notes" setting because I wanted to be sure that you all got the message)
