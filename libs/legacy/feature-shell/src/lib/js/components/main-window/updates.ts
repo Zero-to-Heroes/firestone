728b4bf5649081e49df37fbe6011cb3c3a9912a2
sebastientromp <sebastien.tromp@gmail.com>
2023-03-20 21:19:46 +0100

    RELEASE: 11.0.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,56 +33,55 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '10.3.14',
+		version: '11.0.0',
 		force: false,
 		sections: [
-			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `IMPORTANT: Premium accounts are undergoing some changes, and some features are moving to be premium-only. You can read more about it <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">here</a>, but the TL;DR is:
-			// 	<ul>
-			// 		<li>The ads business model is showing its limits, and I need to change how the app generates revenues to be able to continue doing this full-time</li>
-			// 		<li>Premium will move from $3-remove-the-ads to $5-remove-the-ads-and-get-more-features. The change in price will be in effect starting next week.</li>
-			// 		<li>As much as possible, pure / low-processed data will be free. Premium features will be QoL and stuff that require a lot of processing</li>
-			// 	</ul>
-			// 	This is still experimental, so let me know what you think!<br/>
-			// 	(PS: This message exceptionnaly ignores your "don't show me release notes" setting because I wanted to be sure that you all got the message)
-			// 	`,
-			// },
+			{
+				type: 'intro',
+				header: 'Message from the dev',
+				text: `With this version, the Premium accounts will now be $5 / month, and will offer more ease-of-use and quality of life features. Please see <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">the wiki</a> for more details, as well as an exhaustive list of features that will be available to premium users.  
+				<p>
+				Relatedly, the right column of the app's main window will now be used for ads for free users.
+				<p>
+				I think it is not too bad. Most of the information that used to be there can be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
+				<p>
+				As usual, I'm open to feedback, so let me know what you think on Discord!
+				`,
+			},
 			{
 				type: 'main',
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'general',
+						category: 'duels',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where the app would not start for some users.`,
+								type: 'feature',
+								text: `Duels now has real tier lists for Heroes, Hero Powers, Signature Treasures, and Active and Passive Treasures.`,
 							},
 						],
 					},
 				],
 			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `Add an option to show a notification when a BG game ends to quickly view your game's stats.`,
-							},
-							{
-								type: 'feature',
-								text: `Add a way to show buddies when selecting minions in the simulator.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add an option to show a notification when a BG game ends to quickly view your game's stats.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add a way to show buddies when selecting minions in the simulator.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
--- libs/shared/framework/common/src/lib/libs/translation-file-version.ts
+++ libs/shared/framework/common/src/lib/libs/translation-file-version.ts
@@ -1,1 +1,1 @@
-export const translationFileVersion = '294-main';
+export const translationFileVersion = '295-main';
