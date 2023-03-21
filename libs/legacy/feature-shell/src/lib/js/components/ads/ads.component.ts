117523b9df255627ac05213a330eac49f1bfa0bc
sebastientromp <sebastien.tromp@gmail.com>
2023-03-21 16:32:07 +0100

    FEATURE (ads): add a link to see all premium features

--- libs/legacy/feature-shell/src/lib/js/components/ads/ads.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/ads/ads.component.ts
@@ -24,23 +24,24 @@ declare let amplitude: any;
 				<single-ad [adId]="'top'"></single-ad>
 			</div>
 
-			<div class="unlock-premium-banner" (click)="showSubscription()" *ngIf="showTopAd">
-				<div class="background"></div>
-				<div class="background-gradient"></div>
-				<div class="content">
-					<div class="text">
-						<span class="main-text" [owTranslate]="'app.global.ads.unlock-text'"></span>
-						<span class="sub-text" [owTranslate]="'app.global.ads.unlock-subtext'"></span>
+			<div class="banner-container">
+				<div class="unlock-premium-banner" (click)="showSubscription()" *ngIf="showTopAd">
+					<div class="background"></div>
+					<div class="background-gradient"></div>
+					<div class="content">
+						<div class="text">
+							<span class="main-text" [owTranslate]="'app.global.ads.unlock-text'"></span>
+							<span class="sub-text" [owTranslate]="'app.global.ads.unlock-subtext'"></span>
+						</div>
+						<button class="cta" [owTranslate]="'app.global.ads.cta-text'"></button>
 					</div>
-					<button class="cta" [owTranslate]="'app.global.ads.cta-text'"></button>
 				</div>
+				<div
+					class="features-link"
+					(click)="showFeatures()"
+					[owTranslate]="'app.global.ads.features-link'"
+				></div>
 			</div>
-			<div
-				class="subscription-link"
-				*ngIf="!showTopAd"
-				(click)="showSubscription()"
-				[owTranslate]="'app.global.ads.support-the-dev'"
-			></div>
 
 			<div class="ad-container bottom-ads">
 				<single-ad [adId]="'bottom'" [tip]="tip$ | async"></single-ad>
@@ -85,5 +86,9 @@ export class AdsComponent extends AbstractSubscriptionStoreComponent implements 
 	showSubscription() {
 		amplitude.getInstance().logEvent('subscription-click', { page: 'banner' });
 		this.ow.openStore();
+	}
+
+	showFeatures() {
+		this.ow.openUrlInDefaultBrowser('https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads');
 	}
 }
--- libs/shared/framework/common/src/lib/libs/translation-file-version.ts
+++ libs/shared/framework/common/src/lib/libs/translation-file-version.ts
@@ -1,1 +1,1 @@
-export const translationFileVersion = '297-main';
+export const translationFileVersion = '299-main';
