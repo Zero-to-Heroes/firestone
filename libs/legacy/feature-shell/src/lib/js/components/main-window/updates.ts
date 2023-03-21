a50eb896e94f44e21044638f597a3865ddad9223
sebastientromp <sebastien.tromp@gmail.com>
2023-03-21 17:31:03 +0100

    RELEASE: 11.0.1

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,17 +33,19 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.0.0',
+		version: '11.0.1',
 		force: false,
 		sections: [
 			{
 				type: 'intro',
 				header: 'Message from the dev',
-				text: `With this version, the Premium accounts will now be $5 / month, and will offer more ease-of-use and quality of life features. Please see <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">the wiki</a> for more details, as well as an exhaustive list of features that will be available to premium users.  
-				<p>
-				Relatedly, the right column of the app's main window will now be used for ads for free users.
-				<p>
-				I think it is not too bad. Most of the information that used to be there can be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
+				text: `This version introduces the Firestone Premium Users rework. You can read all the details (including the list of features that are Premium-exclusive) <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">on the wiki</a>, but the TL;DR is:
+				<ul>
+				<li>The Premium subscription is now $5 / month</li>
+				<li>There is a new ad space for free users</li>
+				<li>A few of the app's features are now premium-exclusive</li>
+				</ul>
+				While I would have loved to keep everything free and continue with the previous business model, I think that the current compromise is not too bad. Most of the information that used to be available for free can still be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
 				<p>
 				As usual, I'm open to feedback, so let me know what you think on Discord!
 				`,
