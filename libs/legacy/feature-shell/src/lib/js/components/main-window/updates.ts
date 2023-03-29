4cb876070d3e89d444fd2f5b706166ce41d51c79
sebastientromp <sebastien.tromp@gmail.com>
2023-03-29 12:20:08 +0200

    RELEASE: 11.0.12

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,23 +33,23 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.0.10',
-		force: true,
+		version: '11.0.12',
+		force: false,
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `This version introduces the Firestone Premium Users rework. You can read all the details (including the list of features that are Premium-exclusive) <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">on the wiki</a>, but the TL;DR is:
-				<ul>
-				<li>The Premium subscription is now $5 / month</li>
-				<li>There is a new ad space for free users</li>
-				<li>A few of the app's features are now premium-exclusive</li>
-				</ul>
-				While I would have loved to keep everything free and continue with the previous business model, I think that the current compromise is not too bad. Most of the information that used to be available for free can still be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
-				<p>
-				As usual, I'm open to feedback, so let me know what you think on Discord!
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `This version introduces the Firestone Premium Users rework. You can read all the details (including the list of features that are Premium-exclusive) <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">on the wiki</a>, but the TL;DR is:
+			// 	<ul>
+			// 	<li>The Premium subscription is now $5 / month</li>
+			// 	<li>There is a new ad space for free users</li>
+			// 	<li>A few of the app's features are now premium-exclusive</li>
+			// 	</ul>
+			// 	While I would have loved to keep everything free and continue with the previous business model, I think that the current compromise is not too bad. Most of the information that used to be available for free can still be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
+			// 	<p>
+			// 	As usual, I'm open to feedback, so let me know what you think on Discord!
+			// 	`,
+			// },
 			{
 				type: 'main',
 				header: 'Main updates',
@@ -58,37 +58,8 @@ export const updates: readonly Update[] = [
 						category: 'duels',
 						details: [
 							{
-								type: 'feature',
-								text: `Duels now has real tier lists for Heroes, Hero Powers, Signature Treasures, and Active and Passive Treasures.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'content',
-								text: `The strategy tips by Minder & Jkirek (BG curvesheet) have been updated for the buddies!`,
-							},
-							{
-								type: 'feature',
-								text: `You can now see the strategy tips for each hero by mousing over the "click to view the tips" text in the BG window`,
-							},
-							{
-								type: 'feature',
-								text: `(PREMIUM) Premium users can also see this information directly in the overlay`,
-							},
-							{
-								type: 'feature',
-								text: `Your own buddy now appears in the minions list. This lets you check out its golden version easily`,
-							},
-							{
-								type: 'feature',
-								text: `If you're playing ETC Band Manager, a new "Buds" tier will appear in the bottom row of the minions list with all the Buddies that they can discover from.`,
-							},
-							{
-								type: 'feature',
-								text: `If you're playing Tess or Scabbs, a new "Buds" tier will appear in the bottom row of the minions list with the buddies from all the players in the lobby.`,
+								type: 'bug',
+								text: `Fix an issue where the "copy deck" button had gone missing when viewing a Duels deck.`,
 							},
 						],
 					},
@@ -99,33 +70,11 @@ export const updates: readonly Update[] = [
 				header: 'Minor updates',
 				updates: [
 					{
-						category: 'battlegrounds',
+						category: 'decktracker',
 						details: [
 							{
 								type: 'feature',
-								text: `The "Simulate" button now changes text while the simulation is ongoing.`,
-							},
-							{
-								type: 'feature',
-								text: `Fix a simulator issue with Frostwolf Lieutenant.`,
-							},
-						],
-					},
-					{
-						category: 'achievements',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue that would prevent the app from updating the in-game achievements progress.`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix the missing scrollbar in the Streams tab.`,
+								text: `Add a counter for Overdraft.`,
 							},
 						],
 					},
