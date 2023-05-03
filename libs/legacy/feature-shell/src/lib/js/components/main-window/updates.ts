0625a7168b561822b01b8c0758d01c4d55176e64
sebastientromp <sebastien.tromp@gmail.com>
2023-05-03 10:13:58 +0200

    RELEASE: 11.1.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.0.22',
+		version: '11.1.0',
 		force: false,
 		sections: [
 			// {
@@ -50,21 +50,21 @@ export const updates: readonly Update[] = [
 			// 	As usual, I'm open to feedback, so let me know what you think on Discord!
 			// 	`,
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `(PREMIUM) The firestoneapp.gg website has been updated with momre Duels stats and filters. The Battlegrounds section now shows which tribes have the best impact on each hero's average position.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `(PREMIUM) The firestoneapp.gg website has been updated with momre Duels stats and filters. The Battlegrounds section now shows which tribes have the best impact on each hero's average position.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -73,24 +73,16 @@ export const updates: readonly Update[] = [
 						category: 'decktracker',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an info leak that would reveal when your opponent drew a Symphony of Sins' movement.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where cards would sometimes not be highlighted properly.`,
-							},
-							{
-								type: 'bug',
-								text: `Tried to fix an issue where cards drawn by Finley would sometimes get icons (like Finley's icon, or the Coin).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Start of Game global effects (e.g. Renathal) would not appear in the tracker anymore.`,
-							},
-							{
 								type: 'content',
-								text: `The opponent's hero power damage counter (aka Mordresh Counter) does not show up automatically when facing a Mage opponent in Standard anymore.`,
+								text: `Add new oracles and card highlights.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Grand Totem Eys'Or's effect was not being added to the Global Effects list.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix oracles for Melomania and Fight Over Me.`,
 							},
 						],
 					},
@@ -98,60 +90,17 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `The heroes tier list now shows which tribes are best for each hero.`,
-							},
-							{
-								type: 'feature',
-								text: `Add the "Warning" icon in the Simulator tab of the BG window when some boards are not supported (it was already showing on the overlay widget).`,
+								type: 'bug',
+								text: `Fix a sim issue where Leapfrogger could not buff itself when triggered by Macaw.`,
 							},
 						],
 					},
 					{
-						category: 'duels',
-						details: [
-							{
-								type: 'feature',
-								text: `Add support for ETC Band Manager's sideboard.`,
-							},
-							{
-								type: 'feature',
-								text: `Hide cards from other classes when building a deck / browsing the buckets and a class is selected.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the rank filter had disappeared from the High-Wins deck tab.`,
-							},
-						],
-					},
-					{
-						category: 'mercenaries',
+						category: 'general',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where the Battle Turn Counter would show an incorrect value.`,
-							},
-						],
-					},
-					{
-						category: 'replays',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the replay controls would not be visible anymore.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'feature',
-								text: `Also add the card back description when mousing over card backs in the Collection tab (not localized yet).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the Search field had become a premium-exclusive feature.`,
+								text: `Fix an issue where the decklists shown in the app would not have the cards ordered alphabetically for a given mana cost.`,
 							},
 						],
 					},
