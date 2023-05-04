600a9c595b21047ee29e270ad84de94e42d6e648
sebastientromp <sebastien.tromp@gmail.com>
2023-05-04 09:33:15 +0200

    DEV: restrict the number of files sent when uploading logs

--- libs/legacy/feature-shell/src/lib/js/services/plugins/simple-io.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/plugins/simple-io.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
+import { groupByFunction } from '@firestone/shared/framework/common';
 import { OverwolfService } from '@firestone/shared/framework/core';
 import * as JSZip from 'jszip';
 
+// All the old files that we don't want to include in the logs
+const EXCLUDED_FILES = [
+	'BattlegroundsMinionsTiersWindow',
+	'BattlegroundsOverlayButtonWindow',
+	'BgsBannedTribes',
+	'BgsBattleSimulationOverlay',
+	'BgsHeroSelectionOverlay',
+	'DeckTrackerOpponentWindow',
+	'DeckTrackerWindow',
+	'MatchOverlayBattlegroundsMouseOverWindow',
+	'MatchOverlayOpponentHandWindow',
+	'MercenariesActionQueueWindow',
+	'MercenariesOpponentTeamWindow',
+	'MercenariesOutOfCombatPlayerTeamWindow',
+	'MercenariesOutOfCombatTreasureSelectionWindow',
+	'MercenariesPlayerTeamWindow',
+	'SecretsHelperWindow',
+];
 @Injectable()
 export class SimpleIOService {
 	constructor(private readonly ow: OverwolfService) {}
@@ -11,13 +30,22 @@ export class SimpleIOService {
 		const path = dirListingResult.path;
 		// It should be a flat structure for us
 		const files = dirListingResult.data.filter((d) => d.type === 'file').map((file) => file.name);
+		const groupedFiles = groupByFunction((fileName: string) => fileName.split('.')[0])(files);
+
 		const fileContents: { fileName: string; contents: string }[] = [];
-		for (const file of files) {
-			const contents = await this.ow.readTextFile(path + '/' + file);
-			fileContents.push({
-				fileName: file,
-				contents: contents,
-			});
+		for (const fileRoot of Object.keys(groupedFiles)) {
+			if (EXCLUDED_FILES.includes(fileRoot) || fileRoot.startsWith('Counter')) {
+				continue;
+			}
+			const allFiles = Object.values(groupedFiles[fileRoot]);
+			const filesToUpload = allFiles.sort().reverse().slice(0, 5);
+			for (const file of filesToUpload) {
+				const contents = await this.ow.readTextFile(path + '/' + file);
+				fileContents.push({
+					fileName: file,
+					contents: contents,
+				});
+			}
 		}
 		const jszip = new JSZip();
 		fileContents.forEach((file) => jszip.file(file.fileName, file.contents));
