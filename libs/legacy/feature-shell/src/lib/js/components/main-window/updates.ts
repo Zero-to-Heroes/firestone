4e3fc131cf23a29cb4500bed21a035db27ef125d
sebastientromp <sebastien.tromp@gmail.com>
2023-05-11 02:34:20 +0200

    RELEASE: 11.0.25

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,130 +33,121 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.0.22',
-		force: false,
+		version: '11.0.25',
+		force: true,
 		sections: [
+			{
+				type: 'intro',
+				header: 'Message from the dev',
+				text: `Firestone is now operational once again! There's still some work needed to support all the new content from 26.2, and that will arrive in the coming days. But the app is now usable again, and you can track your games, see your stats, and use the deck tracker in all game modes. Thanks for your patience!`,
+			},
 			// {
-			// 	type: 'intro',
-			// 	header: 'Message from the dev',
-			// 	text: `This version introduces the Firestone Premium Users rework. You can read all the details (including the list of features that are Premium-exclusive) <a href="https://github.com/Zero-to-Heroes/firestone/wiki/Premium-vs-ads" target="_blank">on the wiki</a>, but the TL;DR is:
-			// 	<ul>
-			// 	<li>The Premium subscription is now $5 / month</li>
-			// 	<li>There is a new ad space for free users</li>
-			// 	<li>A few of the app's features are now premium-exclusive</li>
-			// 	</ul>
-			// 	While I would have loved to keep everything free and continue with the previous business model, I think that the current compromise is not too bad. Most of the information that used to be available for free can still be seen it via other (though less convenient) means directly from the app as well. As for the information that is now exclusive to premium users, I feel that it's usually not vital information, but rather nice-to-have bonuses (like the cards history).
-			// 	<p>
-			// 	As usual, I'm open to feedback, so let me know what you think on Discord!
-			// 	`,
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `(PREMIUM) The firestoneapp.gg website has been updated with momre Duels stats and filters. The Battlegrounds section now shows which tribes have the best impact on each hero's average position.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `(PREMIUM) The firestoneapp.gg website has been updated with momre Duels stats and filters. The Battlegrounds section now shows which tribes have the best impact on each hero's average position.`,
-							},
-						],
-					},
-				],
-			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an info leak that would reveal when your opponent drew a Symphony of Sins' movement.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where cards would sometimes not be highlighted properly.`,
-							},
-							{
-								type: 'bug',
-								text: `Tried to fix an issue where cards drawn by Finley would sometimes get icons (like Finley's icon, or the Coin).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Start of Game global effects (e.g. Renathal) would not appear in the tracker anymore.`,
-							},
-							{
-								type: 'content',
-								text: `The opponent's hero power damage counter (aka Mordresh Counter) does not show up automatically when facing a Mage opponent in Standard anymore.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `The heroes tier list now shows which tribes are best for each hero.`,
-							},
-							{
-								type: 'feature',
-								text: `Add the "Warning" icon in the Simulator tab of the BG window when some boards are not supported (it was already showing on the overlay widget).`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'feature',
-								text: `Add support for ETC Band Manager's sideboard.`,
-							},
-							{
-								type: 'feature',
-								text: `Hide cards from other classes when building a deck / browsing the buckets and a class is selected.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the rank filter had disappeared from the High-Wins deck tab.`,
-							},
-						],
-					},
-					{
-						category: 'mercenaries',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the Battle Turn Counter would show an incorrect value.`,
-							},
-						],
-					},
-					{
-						category: 'replays',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the replay controls would not be visible anymore.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'feature',
-								text: `Also add the card back description when mousing over card backs in the Collection tab (not localized yet).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the Search field had become a premium-exclusive feature.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an info leak that would reveal when your opponent drew a Symphony of Sins' movement.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where cards would sometimes not be highlighted properly.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Tried to fix an issue where cards drawn by Finley would sometimes get icons (like Finley's icon, or the Coin).`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where Start of Game global effects (e.g. Renathal) would not appear in the tracker anymore.`,
+			// 				},
+			// 				{
+			// 					type: 'content',
+			// 					text: `The opponent's hero power damage counter (aka Mordresh Counter) does not show up automatically when facing a Mage opponent in Standard anymore.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'battlegrounds',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `The heroes tier list now shows which tribes are best for each hero.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add the "Warning" icon in the Simulator tab of the BG window when some boards are not supported (it was already showing on the overlay widget).`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'duels',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Add support for ETC Band Manager's sideboard.`,
+			// 				},
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Hide cards from other classes when building a deck / browsing the buckets and a class is selected.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the rank filter had disappeared from the High-Wins deck tab.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'mercenaries',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the Battle Turn Counter would show an incorrect value.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'replays',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the replay controls would not be visible anymore.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'collection',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Also add the card back description when mousing over card backs in the Collection tab (not localized yet).`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the Search field had become a premium-exclusive feature.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
