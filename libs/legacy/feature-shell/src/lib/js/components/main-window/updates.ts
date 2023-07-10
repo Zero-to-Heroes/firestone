a18e31dc2fc11d08c9af366282f2670583a3120a
sebastientromp <sebastien.tromp@gmail.com>
2023-07-10 11:57:25 +0200

    RELEASE: 11.5.9

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.5.5',
+		version: '11.5.9',
 		force: false,
 		sections: [
 			// {
@@ -46,24 +46,24 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'achievements',
-						details: [
-							{
-								type: 'feature',
-								text: `You can now track live progress of achievements during the game! Just find the achievements you want to track from Firestone's Achievements tab, ping them, and you're set! You can also let the app pick 3 achievements for you to try and complete.`,
-							},
-						],
-					},
-					{
 						category: 'general',
 						details: [
 							{
 								type: 'misc',
-								text: `A new Lottery season has started! Quilboars are now worth 10 points each. Results for the previous season have been posted on <a href="https://twitter.com/ZerotoHeroes_HS/status/1675769176424693772?s=20" target="_blank">Twitter</a> and <a href="https://discord.com/channels/187101197767933952/489517715376308244/1125327168076001341" target="_blank">Discord</a>. Good luck!`,
+								text: `I have updated the visual design for the lottery widget. It looks much better now, and the information is easier to see`,
 							},
 							{
+								type: 'content',
+								text: `The app is now updated for 26.6.2`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
 								type: 'bug',
-								text: `Fixed a bug where sometimes the lottery widget wouldn't close properly.`,
+								text: `Fix an issue where many cards were missing the "gift" icon in the opponent's hand.`,
 							},
 						],
 					},
@@ -78,46 +78,32 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'bug',
-								text: `Commander Sivara now won't remember their previous spells when bounced back to hand.`,
+								text: `Fix an issue where the current equipped weapon's durability was not taken into account when computing total damage on board (this is only relevant when the hero has Windfury).`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where the Menagerie counter would sometimes be slightly off.`,
+								text: `Fix an issue where Symphony of Sins token were not tracked properly (the logs do some pretty unusual stuff for these).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Commander Sivara would not remember the quests that you played.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the tracker would get lost when the opponent dredges from our deck (with Disarming Elemental or Spy-o-Matic).`,
+							},
+							{
+								type: 'content',
+								text: `Add several oracles and card highlights, and fix a few existing ones (Hope of Quel'Delar, Rewind, Doomerang, Flesh Behemoth).`,
 							},
 						],
 					},
 					{
-						category: 'battlegrounds',
+						category: 'duels',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix a sim issue with Bristleback Knight.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'feature',
-								text: `You can now filter to show all Twist sets.`,
-							},
-							{
-								type: 'feature',
-								text: `(Premium) The cards history is now linked to the sets that are currently visible on the main screen. This means that you can go into a specific set and see all the history for that set. `,
-							},
-							{
-								type: 'feature',
-								text: `(Premium) You can now view aggregated stats for standard/twist/wild/all sets.`,
-							},
-						],
-					},
-					{
-						category: 'achievements',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where achievements would sometimes not be properly sorted by completion date.`,
+								text: `Fix an issue where the total dust cost for high-wins decks was incorrect.`,
 							},
 						],
 					},
@@ -125,8 +111,8 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where the "don't show me again" button on the lottery window notification wouldn't actually stop showing you notifications.`,
+								type: 'feature',
+								text: `(Premium) Premium users can now rescale the lottery / achievements tracker widget to make it smaller.`,
 							},
 						],
 					},
