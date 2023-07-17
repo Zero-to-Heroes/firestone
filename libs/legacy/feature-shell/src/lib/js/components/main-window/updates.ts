6dc9242f4d7aef64e6b1511cada83783b883e760
sebastientromp <sebastien.tromp@gmail.com>
2023-07-17 11:14:21 +0200

    RELEASE: 11.5.16

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.5.12',
+		version: '11.5.16',
 		force: false,
 		sections: [
 			// {
@@ -46,20 +46,11 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'content',
-								text: `The app has been updated for Quests and patch 26.6.3. Enjoy!`,
-							},
-						],
-					},
-					{
 						category: 'general',
 						details: [
 							{
-								type: 'misc',
-								text: `This lottery season will be a short one! Seasons are 2 week long from now on (with 2 winners for each), and should have a bit more varied tasks. I'm still working on adding new tasks, so if you have any interesting ideas, please hit me up :)`,
+								type: 'bug',
+								text: `Fix an issue where the app would not properly start a new Lottery season.`,
 							},
 						],
 					},
@@ -70,32 +61,24 @@ export const updates: readonly Update[] = [
 				header: 'Minor updates',
 				updates: [
 					{
-						category: 'general',
+						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `You can now change the opacity of the Lottery widget (apart from the ad space, which is always at 100%). Go to Settings > General > Lottery and change the opacity slider.`,
+								type: 'bug',
+								text: `Fix a sim issue with Bassgill.`,
 							},
 							{
-								type: 'feature',
-								text: `You can now see the time left in the current season.`,
+								type: 'bug',
+								text: `Fix an issue when trying to navigate to the Hero Strategies tab from the BG window when using a non-vanilla hero skin.`,
 							},
 						],
 					},
 					{
-						category: 'achievements',
+						category: 'general',
 						details: [
 							{
-								type: 'feature',
-								text: `Live tracking of achievements now contains the current Reward Track XP (if any), and tells you where in the achievements hierarchy you can find that achievement (which is useful for context when the name is not enough).`,
-							},
-							{
-								type: 'feature',
-								text: `The "Pick for Me" feature now excludes Collection and Progression achievements, and prioritizes achievements that grand Rewards Track XP.`,
-							},
-							{
-								type: 'feature',
-								text: `The current progress of achievements is now displayed when browsing achievements in Firestone, and is updated after each game.`,
+								type: 'bug',
+								text: `Try to fix an issue where sometimes some windows would become invisible, and could not be alt-tabbed to.`,
 							},
 						],
 					},
