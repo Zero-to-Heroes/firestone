fd40bd291e92bee32ae13bea60250b88005970c2
sebastientromp <sebastien.tromp@gmail.com>
2023-07-29 13:48:53 +0200

    RELEASE: 11.6.2

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.6.1',
+		version: '11.6.2',
 		force: false,
 		sections: [
 			// {
@@ -46,35 +46,57 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'general',
+						category: 'battlegrounds',
 						details: [
 							{
-								type: 'content',
-								text: `Update app for Hearthstone patch 27.0.`,
+								type: 'feature',
+								text: `(PREMIUM) You can now see stats for quests and rewards in the overlay. This features shows you the average turn to complete a given quest, based on the quest difficulty, your hero and your MMR range, and shows the average final rank for players who picked this quest for your MMR range.<br/> A proper "Quests" tab will be availble soon to all users in the app, where you will be able to get a detailed view on all these stats.`,
 							},
 						],
 					},
 				],
 			},
-			// {
-			// 	type: 'minor',
-			// 	header: 'Minor updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'battlegrounds',
-			// 			details: [
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix a sim issue with Bassgill.`,
-			// 				},
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue when trying to navigate to the Hero Strategies tab from the BG window when using a non-vanilla hero skin.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'minor',
+				header: 'Minor updates',
+				updates: [
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `Add a counter for Astral Automaton. More counters and oracles will be coming in the following weeks, but I will wait for the extension to be available in friendly matches to make testing easier.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Plagues would not be removed from the opponent's deck when drawn.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where ALL minions were not counted in the Elemental counter.`,
+							},
+						],
+					},
+					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix the simulator for the updated behavior of golden Bristleback Knight.`,
+							},
+						],
+					},
+					{
+						category: 'mercenaries',
+						details: [
+							{
+								type: 'misc',
+								text: `Due to the mode not being actively supported anymore, the Mercs icon has now been moved down, below Duels and Arena.`,
+							},
+						],
+					},
+				],
+			},
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
