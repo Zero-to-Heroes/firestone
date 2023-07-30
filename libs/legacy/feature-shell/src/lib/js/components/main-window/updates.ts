e483facd3796ac041589d35caa9c8152700ad495
sebastientromp <sebastien.tromp@gmail.com>
2023-07-30 14:20:49 +0200

    RELEASE: 11.6.3

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.6.2',
+		version: '11.6.3',
 		force: false,
 		sections: [
 			// {
@@ -46,14 +46,23 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
+						category: 'general',
 						details: [
 							{
-								type: 'feature',
-								text: `(PREMIUM) You can now see stats for quests and rewards in the overlay. This features shows you the average turn to complete a given quest, based on the quest difficulty, your hero and your MMR range, and shows the average final rank for players who picked this quest for your MMR range.<br/> A proper "Quests" tab will be availble soon to all users in the app, where you will be able to get a detailed view on all these stats.`,
+								type: 'bug',
+								text: `Fix a display issue where damage done with spells would not be properly shown, although the data was correctly computed.`,
 							},
 						],
 					},
+					// {
+					// 	category: 'battlegrounds',
+					// 	details: [
+					// 		{
+					// 			type: 'feature',
+					// 			text: `(PREMIUM) You can now see stats for quests and rewards in the overlay. This features shows you the average turn to complete a given quest, based on the quest difficulty, your hero and your MMR range, and shows the average final rank for players who picked this quest for your MMR range.<br/> A proper "Quests" tab will be availble soon to all users in the app, where you will be able to get a detailed view on all these stats.`,
+					// 		},
+					// 	],
+					// },
 				],
 			},
 			{
@@ -66,6 +75,10 @@ export const updates: readonly Update[] = [
 							{
 								type: 'feature',
 								text: `Add a counter for Astral Automaton. More counters and oracles will be coming in the following weeks, but I will wait for the extension to be available in friendly matches to make testing easier.`,
+							},
+							{
+								type: 'feature',
+								text: `Add a spell school counter when facing Mage opponents.`,
 							},
 							{
 								type: 'bug',
@@ -83,6 +96,19 @@ export const updates: readonly Update[] = [
 							{
 								type: 'bug',
 								text: `Fix the simulator for the updated behavior of golden Bristleback Knight.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue with Illidan's hero power.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'feature',
+								text: `If there are no top Tavern Brawls decks that you can build, you will now be offered to copy a deck for which you have missing copies, with a warning.`,
 							},
 						],
 					},
