4e9e74581a8424f1a3d49e23908762407cb0584b
sebastientromp <sebastien.tromp@gmail.com>
2023-08-21 13:07:17 +0200

    RELEASE: 11.7.9

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.7.4',
+		version: '11.7.9',
 		force: false,
 		sections: [
 			// {
@@ -50,29 +50,7 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `You can now see the average Turns to Complete for each Quest in Battlegrounds from the main app's window. You can see stats grouped by quest, or split based on the quest's difficulty.`,
-							},
-							{
-								type: 'feature',
-								text: `You can now see the average final Placement for each Quest Reward in Battlegrounds from the main app's window.`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `You can now see your all-time match stats for Ranked, Arena, Duels and Battlegrounds in the new Profile tab.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where cards put in hand by Finley, Sea Guide would sometimes be revealed in the opponent's hand.`,
+								text: `Mousing over the "average position" stat, either in the overlay or in the app's UI, now shows the details on how the stats are computed.  I feel that with anomalies coming and adding another layer of stats, this may help give more weight to what the stats say (and/or help me find some bugs :D)`,
 							},
 						],
 					},
@@ -86,17 +64,29 @@ export const updates: readonly Update[] = [
 						category: 'decktracker',
 						details: [
 							{
+								type: 'feature',
+								text: `Mousing over a card that uses different spell schools (like Inquisitive Creation) now highlights the spells in your deck and hand that have spell schools you haven't played yet.`,
+							},
+							{
 								type: 'content',
-								text: `Add multiple oracle and card highlights.`,
+								text: `Demon Hunter and Death Knight are momentarily removed from the Twist deckbuilder, in preparation of the first Twist season coming with patch 27.2.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Lady Darkvein's counter widget would incorrectly consider spells that got countered.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the Secret Helper would sometimes not show the correct list for secrets created by Tear Reality.`,
 							},
 						],
 					},
 					{
-						category: 'battlegrounds',
+						category: 'general',
 						details: [
 							{
-								type: 'feature',
-								text: `(PREMIUM) Add a setting to turn off the Quests Stats overlay.`,
+								type: 'bug',
+								text: `Fix an issue where you could not log in to Out of Games anymore.`,
 							},
 						],
 					},
