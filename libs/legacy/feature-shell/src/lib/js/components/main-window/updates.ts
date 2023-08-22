68957db6d183e56e12c6023e475dfb6b47fa39ce
sebastientromp <sebastien.tromp@gmail.com>
2023-08-22 22:47:14 +0200

    RELEASE: 11.8.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '11.7.9',
+		version: '11.8.0',
 		force: false,
 		sections: [
 			// {
@@ -46,52 +46,52 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'feature',
-								text: `Mousing over the "average position" stat, either in the overlay or in the app's UI, now shows the details on how the stats are computed.  I feel that with anomalies coming and adding another layer of stats, this may help give more weight to what the stats say (and/or help me find some bugs :D)`,
-							},
-						],
-					},
-				],
-			},
-			{
-				type: 'minor',
-				header: 'Minor updates',
-				updates: [
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Mousing over a card that uses different spell schools (like Inquisitive Creation) now highlights the spells in your deck and hand that have spell schools you haven't played yet.`,
-							},
-							{
-								type: 'content',
-								text: `Demon Hunter and Death Knight are momentarily removed from the Twist deckbuilder, in preparation of the first Twist season coming with patch 27.2.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Lady Darkvein's counter widget would incorrectly consider spells that got countered.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the Secret Helper would sometimes not show the correct list for secrets created by Tear Reality.`,
-							},
-						],
-					},
-					{
 						category: 'general',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where you could not log in to Out of Games anymore.`,
+								type: 'content',
+								text: `The app has been updated for patch 27.2. There are still quite a lot of things to do, so stay tuned for more updates this week :)`,
 							},
 						],
 					},
 				],
 			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Mousing over a card that uses different spell schools (like Inquisitive Creation) now highlights the spells in your deck and hand that have spell schools you haven't played yet.`,
+			// 				},
+			// 				{
+			// 					type: 'content',
+			// 					text: `Demon Hunter and Death Knight are momentarily removed from the Twist deckbuilder, in preparation of the first Twist season coming with patch 27.2.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where Lady Darkvein's counter widget would incorrectly consider spells that got countered.`,
+			// 				},
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the Secret Helper would sometimes not show the correct list for secrets created by Tear Reality.`,
+			// 				},
+			// 			],
+			// 		},
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where you could not log in to Out of Games anymore.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
