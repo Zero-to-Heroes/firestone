248609a5c7aa20b6371eb0da89a39d3f352efdf8
sebastientromp <sebastien.tromp@gmail.com>
2023-09-04 19:58:21 +0200

    DEV (twitch): reload the translation files every day

--- apps/twitch-extension/src/app/app.module.ts
+++ apps/twitch-extension/src/app/app.module.ts
@@ -13,12 +13,40 @@ import { CardsHighlightFacadeService } from '@legacy-import/src/lib/js/services/
 import { LocalizationFacadeService } from '@legacy-import/src/lib/js/services/localization-facade.service';
 import { AppUiStoreFacadeService } from '@legacy-import/src/lib/js/services/ui-store/app-ui-store-facade.service';
 
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { TwitchStoreService } from './twitch-store.service';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+	// The current date, in a yyyyMMdd format
+	const currentDate = new Date();
+	const translationFileVersion = `${currentDate.getFullYear()}${currentDate.getMonth()}${currentDate.getDate()}`;
+	// const translationFileVersion = `${currentDate.getTime()}`;
+	// Force a refresh every day, as we don't want to load a new version simply to update the translations
+	return new TranslateHttpLoader(
+		http,
+		'https://static.firestoneapp.com/data/i18n/',
+		`.json?v=${translationFileVersion}`,
+	);
+}
+
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, LegacyFeatureShellModule],
+	imports: [
+		BrowserModule,
+		LegacyFeatureShellModule,
+		TranslateModule.forRoot({
+			defaultLanguage: 'enUS',
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient],
+			},
+		}),
+	],
 	providers: [
 		CardsFacadeStandaloneService,
 		LocalizationStandaloneService,
