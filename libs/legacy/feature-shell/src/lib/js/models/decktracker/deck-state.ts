d15badcafdd395ec7809086ee301349428ae4ed8
sebastientromp <sebastien.tromp@gmail.com>
2023-09-05 16:08:56 +0200

    DEV: add the Duels' run starting decklist to game state

--- libs/legacy/feature-shell/src/lib/js/models/decktracker/deck-state.ts
+++ libs/legacy/feature-shell/src/lib/js/models/decktracker/deck-state.ts
@@ -56,6 +56,7 @@ export class DeckState {
 	readonly isActivePlayer: boolean;
 	readonly isOpponent: boolean;
 	readonly deckstring?: string;
+	readonly duelsStartingDeckstring?: string;
 	readonly sideboards?: readonly DeckSideboard[];
 	readonly name?: string;
 	readonly hero?: HeroCard;
--- libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/hero-power-changed-parser.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/hero-power-changed-parser.ts
@@ -1,13 +1,21 @@
+import { sanitizeDeckstring } from '@components/decktracker/copy-deckstring.component';
+import { decode, encode } from '@firestone-hs/deckstrings';
+import { GameType } from '@firestone-hs/reference-data';
 import { CardsFacadeService } from '@firestone/shared/framework/core';
 import { DeckCard } from '../../../models/decktracker/deck-card';
 import { DeckState } from '../../../models/decktracker/deck-state';
 import { GameState } from '../../../models/decktracker/game-state';
 import { GameEvent } from '../../../models/game-event';
+import { DuelsDecksProviderService } from '../../duels/duels-decks-provider.service';
 import { LocalizationFacadeService } from '../../localization-facade.service';
 import { EventParser } from './event-parser';
 
 export class HeroPowerChangedParser implements EventParser {
-	constructor(private readonly cards: CardsFacadeService, private readonly i18n: LocalizationFacadeService) {}
+	constructor(
+		private readonly allCards: CardsFacadeService,
+		private readonly i18n: LocalizationFacadeService,
+		private readonly duelsRunService: DuelsDecksProviderService,
+	) {}
 
 	applies(gameEvent: GameEvent, state: GameState): boolean {
 		return !!state;
@@ -18,7 +26,7 @@ export class HeroPowerChangedParser implements EventParser {
 
 		const isPlayer = controllerId === localPlayer.PlayerId;
 		const deck = isPlayer ? currentState.playerDeck : currentState.opponentDeck;
-		const dbCard = this.cards.getCard(cardId);
+		const dbCard = this.allCards.getCard(cardId);
 		const card = DeckCard.create({
 			cardId: cardId,
 			entityId: entityId,
@@ -29,8 +37,23 @@ export class HeroPowerChangedParser implements EventParser {
 			temporaryCard: false,
 			playTiming: GameState.playTiming++,
 		} as DeckCard);
+
+		// Used for Twitch
+		console.debug('[duels-run-deckstring] will handle duels deck?', currentState);
+		const duelsStartingDeckstring = !isPlayer
+			? null
+			: currentState.metadata.gameType === GameType.GT_PVPDR ||
+			  currentState.metadata.gameType === GameType.GT_PVPDR_PAID
+			? this.getDuelsStartingDeckstring(
+					currentState.playerDeck.deckstring,
+					currentState.playerDeck.hero?.cardId,
+					card?.cardId,
+			  )
+			: null;
+
 		const newPlayerDeck = Object.assign(new DeckState(), deck, {
 			heroPower: card,
+			duelsStartingDeckstring: duelsStartingDeckstring,
 		} as DeckState);
 		return Object.assign(new GameState(), currentState, {
 			[isPlayer ? 'playerDeck' : 'opponentDeck']: newPlayerDeck,
@@ -39,5 +62,38 @@ export class HeroPowerChangedParser implements EventParser {
 
 	event(): string {
 		return GameEvent.HERO_POWER_CHANGED;
+	}
+
+	private getDuelsStartingDeckstring(currentDeckString: string, heroCardId: string, heroPowerCardId: string): string {
+		const currentDeck = decode(currentDeckString);
+		if (!currentDeck?.cards?.length) {
+			console.debug('[duels-run-deckstring] no deck found', currentDeck);
+			return null;
+		}
+
+		// Take care of the treasure being here
+		if (currentDeck.cards.length <= 16) {
+			const result = encode(sanitizeDeckstring(currentDeck, this.allCards));
+			console.debug('[duels-run-deckstring] first game, returning deckstring', result, currentDeck);
+			return result;
+		}
+
+		const allRuns = this.duelsRunService.duelsRuns$.getValue();
+		const currentRun = allRuns[0];
+		console.debug('[duels-run-deckstring] current run', currentRun);
+		if (currentRun?.heroCardId !== heroCardId || currentRun?.heroPowerCardId !== heroPowerCardId) {
+			console.debug(
+				'[duels-run-deckstring] hero or hero power do not match',
+				currentRun,
+				heroCardId,
+				heroPowerCardId,
+			);
+			return null;
+		}
+		const deckDefinition = decode(currentRun.initialDeckList);
+		const updatedDeckDefinition = sanitizeDeckstring(deckDefinition, this.allCards);
+		const result = encode(updatedDeckDefinition);
+		console.debug('[duels-run-deckstring] returning deckstring', result, updatedDeckDefinition, deckDefinition);
+		return result;
 	}
 }
