3c8f3faeb1c9ec66f8e1da8672ed08a60569eb4a
sebastientromp <sebastien.tromp@gmail.com>
2023-10-29 22:19:58 +0100

    RELEASE: 12.4.13

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,31 +33,40 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '12.4.7',
+		version: '12.4.13',
 		force: false,
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `This release for now mostly contains technical updates. I'm trying to make the app load faster, and decrease the memory it uses overall, especially if you're only using one or two modes. It will take a while to get there, so expect the improvements to be very gradual. </br>
-				This first phase focuses on making the app load faster when on desktop (i.e. HS is not running).
-				`,
-			},
 			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'general',
-			// 			details: [
-			// 				{
-			// 					type: 'content',
-			// 					text: `The app has been updated for patch 27.6. There are still some things to work on, and probably some bugs that were introduced, and I will iron this out over the next few days. Enjoy the new patch!`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `This release for now mostly contains technical updates. I'm trying to make the app load faster, and decrease the memory it uses overall, especially if you're only using one or two modes. It will take a while to get there, so expect the improvements to be very gradual. </br>
+			// 	This first phase focuses on making the app load faster when on desktop (i.e. HS is not running).
+			// 	`,
 			// },
+			{
+				type: 'main',
+				header: 'Main updates',
+				updates: [
+					{
+						category: 'duels',
+						details: [
+							{
+								type: 'bug',
+								text: `Try and fix an issue where the run ID in duels was not always consistent across games of the same run, which messed up with the "high-wins decks" detection. IMPORTANT: the first game started after updating the app to 12.4.13 (or later) will be in a new run. Sorry about the inconvenience.`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where the tracker would get confused when starting the app in-game, or when handling some kinds of disconnects.`,
+							},
+						],
+					},
+				],
+			},
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -66,29 +75,8 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'content',
-								text: `Update for 27.6.2.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'content',
-								text: `Miscellaneous small updates to widget texts, oracles and card highlights.`,
-							},
-							{
 								type: 'bug',
-								text: `Fix some issues with stealth reconnects - the game logs would behave as though a reconnect was happening, but without any visible effect in the UI. This would cause the tracker to sometimes stop working.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue the cards in ETC's sideboard would appear in the Other zone when playing ETC.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Plagues created by Helya would sometimes not be properly tracked and could cause the tracker to stop working.`,
+								text: `Fix an issue where you couldn't login to Twitch while HS was not running (Community Contribution).`,
 							},
 						],
 					},
@@ -97,24 +85,16 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'bug',
-								text: `Fix a sim issue with Diremuck Forager.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue with Goldrinn.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue with the Anomalous Evidence anomaly.`,
+								text: `Fix a sim issue with Diremuck Forager (Community Contribution).`,
 							},
 						],
 					},
 					{
-						category: 'duels',
+						category: 'achievements',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where the buttons to quickly build a deck when starting a new run would not appear anymore.`,
+								text: `Fix an issue where achievements live tracking would not always work properly, or would sometimes reset unexpectedly (Community Contribution).`,
 							},
 						],
 					},
