f570a7294156ec1d21484108698c7f181b28ab88
sebastientromp <sebastien.tromp@gmail.com>
2023-11-13 10:14:36 +0100

    RELEASE: 12.5.3

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,30 +33,30 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '12.5.1',
+		version: '12.5.3',
 		force: false,
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `It's giveaway time! I will soon announce a couple of giveaways on Twitter, so make sure to follow me there if you want to participate: https://twitter.com/ZerotoHeroes_HS
-				`,
-			},
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the tracker would get stuck if the app was started while a game was already in progress.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `It's giveaway time! I will soon announce a couple of giveaways on Twitter, so make sure to follow me there if you want to participate: https://twitter.com/ZerotoHeroes_HS
+			// 	`,
+			// },
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Fix an issue where the tracker would get stuck if the app was started while a game was already in progress.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -65,17 +65,8 @@ export const updates: readonly Update[] = [
 						category: 'decktracker',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix an issue where Azerite Vein would be greyed out in the secrets helper even when your hand is full.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Hunter of Gatherers could crash the simulator.`,
+								type: 'feature',
+								text: `Add an Excavate counter.`,
 							},
 						],
 					},
@@ -83,17 +74,8 @@ export const updates: readonly Update[] = [
 						category: 'duels',
 						details: [
 							{
-								type: 'feature',
-								text: `When choosing a hero / hero power / signature treasure, if there is no data for the "last patch" time period, it will automatically extend the time range to be able to show you something in the overlay.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'content',
-								text: `Add missing packs.`,
+								type: 'ui',
+								text: `The look and feel for the quick deck select when starting a Duels has been streamlined.`,
 							},
 						],
 					},
