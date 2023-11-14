d5d54be2e24c916111e2a32f9c5c1dd48623f0cb
sebastientromp <sebastien.tromp@gmail.com>
2023-11-14 15:18:32 +0100

    DEV (tracker): small refactor prior to fixing the cards with the same ID not being grouped

--- libs/legacy/feature-shell/src/lib/js/components/decktracker/overlay/deck-zone.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/decktracker/overlay/deck-zone.component.ts
@@ -293,9 +293,12 @@ export class DeckZoneComponent extends AbstractSubscriptionStoreComponent implem
 		const keyWithBonus = showStatsChange ? card.cardId + '_' + (card.mainAttributeChange || 0) : card.cardId;
 		// We never want cards that are played to be grouped with cards that are not played
 		const keyWithHighlights = keyWithBonus + '-' + card.highlight;
-		const keyWithGift = showGiftsSeparately
-			? keyWithHighlights + 'creators-' + ((card.creatorCardIds || []).length > 0 ? 'yes' : 'no')
-			: keyWithHighlights;
+		const creatorsKeySuffix = !card.creatorCardIds?.length
+			? ''
+			: !!card.cardId
+			? 'creators'
+			: 'creators-' + (card.creatorCardIds || []).join('-');
+		const keyWithGift = showGiftsSeparately ? keyWithHighlights + creatorsKeySuffix : keyWithHighlights;
 		const keyWithBottom = showBottomCardsSeparately
 			? keyWithGift + 'bottom-' + (card.positionFromBottom ?? '')
 			: keyWithGift;
