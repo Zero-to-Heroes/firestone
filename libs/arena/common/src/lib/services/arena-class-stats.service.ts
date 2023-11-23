ecf45b925103e99fa84b790c532e2cc955c752e7
sebastientromp <sebastien.tromp@gmail.com>
2023-11-23 14:28:05 +0100

    BUG (arena): fix time filter not working

--- libs/arena/common/src/lib/services/arena-class-stats.service.ts
+++ libs/arena/common/src/lib/services/arena-class-stats.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ArenaClassStat, ArenaClassStats } from '@firestone-hs/arena-stats';
+import { PreferencesService } from '@firestone/shared/common/service';
 import { SubscriberAwareBehaviorSubject } from '@firestone/shared/framework/common';
 import { AbstractFacadeService, ApiRunner, AppInjector, WindowManagerService } from '@firestone/shared/framework/core';
 // import { PreferencesService } from '@firestone/shared/common/service';
@@ -11,7 +12,7 @@ export class ArenaClassStatsService extends AbstractFacadeService<ArenaClassStat
 	public classStats$$: SubscriberAwareBehaviorSubject<readonly ArenaClassStat[] | null | undefined>;
 
 	private api: ApiRunner;
-	// private prefs: PreferencesService;
+	private prefs: PreferencesService;
 
 	constructor(protected override readonly windowManager: WindowManagerService) {
 		super(windowManager, 'arenaClassStats', () => !!this.classStats$$);
@@ -24,35 +25,28 @@ export class ArenaClassStatsService extends AbstractFacadeService<ArenaClassStat
 	protected async init() {
 		this.classStats$$ = new SubscriberAwareBehaviorSubject<readonly ArenaClassStat[] | null | undefined>(null);
 		this.api = AppInjector.get(ApiRunner);
-		// this.prefs = AppInjector.get(PreferencesService);
+		this.prefs = AppInjector.get(PreferencesService);
 
 		this.classStats$$.onFirstSubscribe(async () => {
 			console.debug('[arena-class-stats] init');
+			await this.prefs.isReady();
 
-			const timePeriod = 'last-patch';
-			const url = ARENA_CLASS_STATS_URL.replace('%timePeriod%', timePeriod);
-			const result: ArenaClassStats | null = await this.api.callGetApi(url);
-			console.log('[arena-class-stats] loaded duels config');
-			this.classStats$$.next(result?.stats);
-
-			// await this.prefs.isReady();
-
-			// this.prefs
-			// 	.preferences$((prefs) => prefs.arenaActiveTimeFilter)
-			// 	.subscribe(async ([timeFilter]) => {
-			// 		const timePeriod =
-			// 			timeFilter === 'all-time'
-			// 				? 'past-20'
-			// 				: timeFilter === 'past-seven'
-			// 				? 'past-7'
-			// 				: timeFilter === 'past-three'
-			// 				? 'past-3'
-			// 				: timeFilter;
-			// 		const url = ARENA_CLASS_STATS_URL.replace('%timePeriod%', timePeriod);
-			// 		const result: ArenaClassStats | null = await this.api.callGetApi(url);
-			// 		console.log('[arena-class-stats] loaded duels config');
-			// 		this.classStats$$.next(result?.stats);
-			// 	});
+			this.prefs
+				.preferences$((prefs) => prefs.arenaActiveTimeFilter)
+				.subscribe(async ([timeFilter]) => {
+					const timePeriod =
+						timeFilter === 'all-time'
+							? 'past-20'
+							: timeFilter === 'past-seven'
+							? 'past-7'
+							: timeFilter === 'past-three'
+							? 'past-3'
+							: timeFilter;
+					const url = ARENA_CLASS_STATS_URL.replace('%timePeriod%', timePeriod);
+					const result: ArenaClassStats | null = await this.api.callGetApi(url);
+					console.log('[arena-class-stats] loaded duels config');
+					this.classStats$$.next(result?.stats);
+				});
 		});
 	}
 }
