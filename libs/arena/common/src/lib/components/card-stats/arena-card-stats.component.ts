9d81a3a61a31ef6c64b9ced20d40a2bc467a583d
sebastientromp <sebastien.tromp@gmail.com>
2023-11-24 12:40:23 +0100

    FEATURE (arena): make card stats columns sortable

--- libs/arena/common/src/lib/components/card-stats/arena-card-stats.component.ts
+++ libs/arena/common/src/lib/components/card-stats/arena-card-stats.component.ts
@@ -1,8 +1,9 @@
 import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewRef } from '@angular/core';
 import { ArenaCardStat } from '@firestone-hs/arena-stats';
-import { AbstractSubscriptionComponent, sortByProperties } from '@firestone/shared/framework/common';
-import { ILocalizationService } from '@firestone/shared/framework/core';
-import { Observable, shareReplay, startWith, tap } from 'rxjs';
+import { SortCriteria, SortDirection, invertDirection } from '@firestone/shared/common/view';
+import { AbstractSubscriptionComponent } from '@firestone/shared/framework/common';
+import { CardsFacadeService, ILocalizationService } from '@firestone/shared/framework/core';
+import { BehaviorSubject, Observable, combineLatest, shareReplay, startWith, tap } from 'rxjs';
 import { ArenaCardStatsService } from '../../services/arena-card-stats.service';
 import { ArenaCardStatInfo } from './model';
 
@@ -16,10 +17,31 @@ import { ArenaCardStatInfo } from './model';
 				[attr.aria-label]="'Arena card stats'"
 				*ngIf="{ cards: cards$ | async } as value"
 			>
-				<div class="header">
-					<div class="cell card-details" [fsTranslate]="'app.arena.card-stats.header-card-name'"></div>
-					<div class="cell drawn-total" [fsTranslate]="'app.arena.card-stats.header-drawn-total'"></div>
-					<div class="cell drawn-winrate" [fsTranslate]="'app.arena.card-stats.header-drawn-winrate'"></div>
+				<div class="header" *ngIf="sortCriteria$ | async as sort">
+					<sortable-table-label
+						class="cell card-details"
+						[name]="'app.arena.card-stats.header-card-name' | fsTranslate"
+						[sort]="sort"
+						[criteria]="'name'"
+						(sortClick)="onSortClick($event)"
+					>
+					</sortable-table-label>
+					<sortable-table-label
+						class="cell drawn-total"
+						[name]="'app.arena.card-stats.header-drawn-total' | fsTranslate"
+						[sort]="sort"
+						[criteria]="'drawn-total'"
+						(sortClick)="onSortClick($event)"
+					>
+					</sortable-table-label>
+					<sortable-table-label
+						class="cell drawn-winrate"
+						[name]="'app.arena.card-stats.header-drawn-winrate' | fsTranslate"
+						[sort]="sort"
+						[criteria]="'drawn-winrate'"
+						(sortClick)="onSortClick($event)"
+					>
+					</sortable-table-label>
 				</div>
 				<virtual-scroller
 					#scroll
@@ -44,11 +66,18 @@ import { ArenaCardStatInfo } from './model';
 export class ArenaCardStatsComponent extends AbstractSubscriptionComponent implements AfterContentInit {
 	loading$: Observable<boolean>;
 	cards$: Observable<ArenaCardStatInfo[]>;
+	sortCriteria$: Observable<SortCriteria<ColumnSortType>>;
+
+	private sortCriteria$$ = new BehaviorSubject<SortCriteria<ColumnSortType>>({
+		criteria: 'drawn-winrate',
+		direction: 'desc',
+	});
 
 	constructor(
 		protected override readonly cdr: ChangeDetectorRef,
 		private readonly arenaCardStats: ArenaCardStatsService,
 		private readonly i18n: ILocalizationService,
+		private readonly allCards: CardsFacadeService,
 	) {
 		super(cdr);
 	}
@@ -57,11 +86,12 @@ export class ArenaCardStatsComponent extends AbstractSubscriptionComponent imple
 		await this.arenaCardStats.isReady();
 
 		console.debug('[arena-card-stats] after content init');
-		this.cards$ = this.arenaCardStats.cardStats$$.pipe(
+		this.sortCriteria$ = this.sortCriteria$$;
+		this.cards$ = combineLatest([this.arenaCardStats.cardStats$$, this.sortCriteria$$]).pipe(
 			tap((info) => console.debug('[arena-card-stats] received info', info)),
-			this.mapData((stats) => this.buildCardStats(stats)),
+			this.mapData(([stats, sortCriteria]) => this.buildCardStats(stats, sortCriteria)),
 			shareReplay(1),
-			this.mapData((tiers) => tiers),
+			this.mapData((stats) => stats),
 		);
 		this.loading$ = this.cards$.pipe(
 			startWith(true),
@@ -78,13 +108,17 @@ export class ArenaCardStatsComponent extends AbstractSubscriptionComponent imple
 		return item.cardId;
 	}
 
-	private buildCardStats(stats: readonly ArenaCardStat[] | null | undefined): ArenaCardStatInfo[] {
+	private buildCardStats(
+		stats: readonly ArenaCardStat[] | null | undefined,
+		sortCriteria: SortCriteria<ColumnSortType>,
+	): ArenaCardStatInfo[] {
 		return (
 			stats
 				?.filter((stat) => stat.stats?.drawn > 100)
 				?.map((stat) => this.buildCardStat(stat))
-				.sort(sortByProperties((a: ArenaCardStatInfo) => [-(a.drawWinrate ?? 0)])) ?? []
+				.sort((a, b) => this.sortCards(a, b, sortCriteria)) ?? []
 		);
+		// .sort(sortByProperties((a: ArenaCardStatInfo) => [-(a.drawWinrate ?? 0)])) ?? []
 	}
 
 	private buildCardStat(stat: ArenaCardStat): ArenaCardStatInfo {
@@ -94,4 +128,48 @@ export class ArenaCardStatsComponent extends AbstractSubscriptionComponent imple
 			drawWinrate: stat.stats.drawn > 0 ? stat.stats.drawnThenWin / stat.stats.drawn : null,
 		};
 	}
+
+	onSortClick(rawCriteria: string) {
+		const criteria: ColumnSortType = rawCriteria as ColumnSortType;
+		this.sortCriteria$$.next({
+			criteria: criteria,
+			direction:
+				criteria === this.sortCriteria$$.value?.criteria
+					? invertDirection(this.sortCriteria$$.value.direction)
+					: 'desc',
+		});
+	}
+
+	private sortCards(a: ArenaCardStatInfo, b: ArenaCardStatInfo, sortCriteria: SortCriteria<ColumnSortType>): number {
+		switch (sortCriteria?.criteria) {
+			case 'name':
+				return this.sortByName(a, b, sortCriteria.direction);
+			case 'drawn-winrate':
+				return this.sortByDrawnWinrate(a, b, sortCriteria.direction);
+			case 'drawn-total':
+				return this.sortByDrawnTotal(a, b, sortCriteria.direction);
+			default:
+				return 0;
+		}
+	}
+
+	private sortByName(a: ArenaCardStatInfo, b: ArenaCardStatInfo, direction: SortDirection): number {
+		const aData = this.allCards.getCard(a.cardId)?.name;
+		const bData = this.allCards.getCard(b.cardId)?.name;
+		return direction === 'asc' ? aData.localeCompare(bData) : bData.localeCompare(aData);
+	}
+
+	private sortByDrawnTotal(a: ArenaCardStatInfo, b: ArenaCardStatInfo, direction: SortDirection): number {
+		const aData = a.drawnTotal ?? 0;
+		const bData = b.drawnTotal ?? 0;
+		return direction === 'asc' ? aData - bData : bData - aData;
+	}
+
+	private sortByDrawnWinrate(a: ArenaCardStatInfo, b: ArenaCardStatInfo, direction: SortDirection): number {
+		const aData = a.drawWinrate ?? 0;
+		const bData = b.drawWinrate ?? 0;
+		return direction === 'asc' ? aData - bData : bData - aData;
+	}
 }
+
+type ColumnSortType = 'name' | 'drawn-total' | 'drawn-winrate';
