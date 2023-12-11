a4c6e4b7f1138dfbf5d33b4a001343d66c4d0ab0
sebastientromp <sebastien.tromp@gmail.com>
2023-12-11 17:31:42 +0100

    BUG (bgs): fix BG selected hero not being properly registered in gamestate

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/bgs-hero-selected-card-parser.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/bgs-hero-selected-card-parser.ts
@@ -13,7 +13,7 @@ export class BgsHeroSelectedCardParser implements EventParser {
 	async parse(currentState: GameState, gameEvent: GameEvent): Promise<GameState> {
 		const [cardId, controllerId, localPlayer, entityId] = gameEvent.parse();
 
-		const isPlayer = controllerId === localPlayer.PlayerId;
+		const isPlayer = true; //controllerId === localPlayer.PlayerId;
 		const deck = isPlayer ? currentState.playerDeck : currentState.opponentDeck;
 		const card = this.helper.findCardInZone(deck.hand, cardId, entityId);
 
