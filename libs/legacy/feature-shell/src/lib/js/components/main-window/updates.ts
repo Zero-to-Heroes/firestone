c5d1249959ac913e4b1d7e12c47cfdef7ce91d19
sebastientromp <sebastien.tromp@gmail.com>
2024-01-11 14:14:59 +0100

    DEV: update changelog for next version

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.2.15',
+		version: '13.2.16',
 		force: false,
 		sections: [
 			// {
@@ -42,82 +42,55 @@ export const updates: readonly Update[] = [
 			// 	text: `If you ever wonder what the total play time for each mode looks like for ALL of Firestone users, I have published an infographics on Twitter: https://x.com/ZerotoHeroes_HS/status/1726691418687832090?s=20
 			// 	`,
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'bug',
-								text: `Reconnects should be handled more gracefully now. It is still a tricky subject, so let me know if you experience bugs after reconnecting to your game.`,
-							},
-						],
-					},
-					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Card stats for meta decks should now (finally) be fixed! Sorry again for the inconvenience.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Reconnects should be handled more gracefully now. It is still a tricky subject, so let me know if you experience bugs after reconnecting to your game.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
 					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where loaner decks would not be detected.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix some issues with the Attack counter (replacing weapons, Charge minions).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix the oracle for Fizzle's Snapshot (the cards were not flagged at their correct place in the opponent's hand).`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Identity Theft would create two copies of each card in the opponent's deck.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where Loaner Decks were not properly detected.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the opponent's Jade Golem counter could not be turned off.`,
-							},
-						],
-					},
-					{
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix a sim issue where some secrets could trigger infinitely.`,
+								type: 'feature',
+								text: `Show cards in hand and secrets in BG simulation replays.`,
+							},
+							{
+								type: 'feature',
+								text: `Show minion overlay icons (deathrattle, trigger, poisonous, etc.) on BG simulation replays.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix a sim issue with minions spawned from hand. I had hoped that I could integrate the in-combat buffs to properly guess the actual stats of the minion in hand, but it turns out that it isn't possible without also requiring a lot of maintenance. So sims where minions are summoned from hand will now be a bit less accurate.`,
+								text: `Fix a sim issue with the location of spawned minions.`,
 							},
-						],
-					},
-					{
-						category: 'arena',
-						details: [
 							{
-								type: 'misc',
-								text: `Add clarifications on the tooltips as what the Wins Profile bars represent.`,
+								type: 'bug',
+								text: `Fix a sim issue with buffing the minions in the opponent's hand.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue with Reckoning.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue with Audacious Anchor start of combat behavior.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where minions from a higher tavern tier could still be summoned during combat.`,
 							},
 						],
 					},
