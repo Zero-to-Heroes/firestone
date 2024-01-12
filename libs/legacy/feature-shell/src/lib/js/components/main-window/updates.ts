834fedc573136b0d92705bf5f4ad18326d322f50
sebastientromp <sebastien.tromp@gmail.com>
2024-01-12 08:13:45 +0100

    RELEASE: 13.2.17

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.2.16',
+		version: '13.2.17',
 		force: false,
 		sections: [
 			// {
@@ -42,28 +42,74 @@ export const updates: readonly Update[] = [
 			// 	text: `If you ever wonder what the total play time for each mode looks like for ALL of Firestone users, I have published an infographics on Twitter: https://x.com/ZerotoHeroes_HS/status/1726691418687832090?s=20
 			// 	`,
 			// },
-			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'general',
-			// 			details: [
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Reconnects should be handled more gracefully now. It is still a tricky subject, so let me know if you experience bugs after reconnecting to your game.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'main',
+				header: 'Main updates',
+				updates: [
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'bug',
+								text: `Reconnects should be handled more gracefully now. It is still a tricky subject, so let me know if you experience bugs after reconnecting to your game.`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'bug',
+								text: `Card stats for meta decks should now (finally) be fixed! Sorry again for the inconvenience.`,
+							},
+						],
+					},
+				],
+			},
 			{
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
 					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where loaner decks would not be detected.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix some issues with the Attack counter (replacing weapons, Charge minions).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix the oracle for Fizzle's Snapshot (the cards were not flagged at their correct place in the opponent's hand).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Identity Theft would create two copies of each card in the opponent's deck.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Loaner Decks were not properly detected.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the opponent's Jade Golem counter could not be turned off.`,
+							},
+						],
+					},
+					{
 						category: 'battlegrounds',
 						details: [
+							{
+								type: 'bug',
+								text: `Fix a sim issue where some secrets could trigger infinitely.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue with minions spawned from hand. I had hoped that I could integrate the in-combat buffs to properly guess the actual stats of the minion in hand, but it turns out that it isn't possible without also requiring a lot of maintenance. So sims where minions are summoned from hand will now be a bit less accurate.`,
+							},
 							{
 								type: 'feature',
 								text: `Show cards in hand and secrets in BG simulation replays.`,
@@ -103,6 +149,15 @@ export const updates: readonly Update[] = [
 							{
 								type: 'bug',
 								text: `Fix a sim issue where the first attacker would not be properly recomputed after Tamsin's hero power.`,
+							},
+						],
+					},
+					{
+						category: 'arena',
+						details: [
+							{
+								type: 'misc',
+								text: `Add clarifications on the tooltips as what the Wins Profile bars represent.`,
 							},
 						],
 					},
