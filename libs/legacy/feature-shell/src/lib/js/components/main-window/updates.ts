b806f7f5d94a45c6a05427182aeb07d652187b30
sebastientromp <sebastien.tromp@gmail.com>
2024-01-25 20:54:24 +0100

    BUG (bgs): fix sim issues

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -42,91 +42,47 @@ export const updates: readonly Update[] = [
 			// 	text: `If you ever wonder what the total play time for each mode looks like for ALL of Firestone users, I have published an infographics on Twitter: https://x.com/ZerotoHeroes_HS/status/1726691418687832090?s=20
 			// 	`,
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'arena',
-						details: [
-							{
-								type: 'feature',
-								text: `Card and Class Stats are now updated every hour and tell you when they were last updated.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'arena',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `Card and Class Stats are now updated every hour and tell you when they were last updated.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
 					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `Add a new setting to show the Plagues at the top of the deck to better keep track of them. The setting is ON by default.`,
-							},
-							{
-								type: 'feature',
-								text: `When the opponent plays a card stolen from your deck by Benevolant Banker, it is now removed from your "Deck" section.`,
-							},
-							{
-								type: 'content',
-								text: `Mousing over highlander cards like Reno, Lone Ranger now highlights all duplicates in your deck.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an info leak where the card created by Shattered Reflections would be revealed when drawn by the opponent.`,
-							},
-						],
-					},
-					{
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `Add a counter to tell you how much more (or less) gold you will have next turn.`,
-							},
-							{
-								type: 'feature',
-								text: `The list of Buddies will now always show when spells are in the tavern.`,
+								type: 'bug',
+								text: `Fix a sim issue where Venomstrike Trap and Snake Trap could trigger on a full board.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue with the in-app simulator where the simulation would sometimes never end.`,
+								text: `Fix a sim issue where minions that were reborn could attack out of order.`,
 							},
-						],
-					},
-					{
-						category: 'arena',
-						details: [
-							{
-								type: 'feature',
-								text: `Add a filter in hte Cards Stats tab to show only Neutral cards, or conversely exclude them and show only class cards.`,
-							},
-							{
-								type: 'feature',
-								text: `Add a filter in hte Cards Stats tab to show only Legendary or Treasure cards.`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'feature',
-								text: `Passive treasures that grant Spell Damage will now only highlight cards in your deck that deal damage.`,
-							},
-						],
-					},
-					{
-						category: 'mercenaries',
-						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where some heroes would be put in an incorrect faction, causing some issues with highlights during the treasure selection phase.`,
+								text: `Fix a sim issue where Broodmother's Onyxian Whelp would mess up the attack order after attacking immediately.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where Embrace Your Rage would not be properly accounted for if another Start of Combat hero power triggered before it.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where Stormpike Lieutenant would buff the board, instead of the tavern.`,
 							},
 						],
 					},
