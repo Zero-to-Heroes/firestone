fe18c0f2a5143cf04103f56db8d3f336159eec56
sebastientromp <sebastien.tromp@gmail.com>
2024-02-06 15:08:28 +0100

    RELEASE: 13.4.2

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.3.8',
+		version: '13.4.2',
 		force: false,
 		sections: [
 			// {
@@ -42,73 +42,45 @@ export const updates: readonly Update[] = [
 			// 	text: `If you ever wonder what the total play time for each mode looks like for ALL of Firestone users, I have published an infographics on Twitter: https://x.com/ZerotoHeroes_HS/status/1726691418687832090?s=20
 			// 	`,
 			// },
-			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'arena',
-			// 			details: [
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Card stats now have more info about pick rate, including pick rate for runs that went to 6+ wins. Please be aware that it might take a few days to gather enough data to have reliable stats.`,
-			// 				},
-			// 				{
-			// 					type: 'feature',
-			// 					text: `Premium users can now see the pickrate of each card directly on the overlay.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
 			{
-				type: 'minor',
-				header: 'Minor updates',
+				type: 'main',
+				header: 'Main updates',
 				updates: [
 					{
-						category: 'battlegrounds',
+						category: 'arena',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix a sim issue where Prickly Piper dying would not increase the attack of Blood Gems.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Necrolyte (triggered by Rylak) would not steal Blood Gems.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue with Hawkstrider + Rylak interact.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Rapid Reanimation's target would not always be properly detected.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where Sr. Tomb Diver could gild dead minions.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue where avenge minions summoned by deathrattles would get the avenge value already incremented.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix a sim issue with the timing of Avenge effects with regards to Reborn.`,
+								type: 'feature',
+								text: `You can now see an overview of your Arena runs, including the decklist used and the picks made during the draft. Let me know if there are more things you'd like to see there!`,
 							},
 						],
 					},
 					{
-						category: 'decktracker',
+						category: 'general',
 						details: [
 							{
-								type: 'bug',
-								text: `Secrets are now grouped by class once more in the Secrets Helper.`,
+								type: 'misc',
+								text: `I did some major rework of how games are uploaded. It should be transparent to you, but if you notice anything weird (replay not found, stats missing, etc.), please let me know!`,
 							},
 						],
 					},
 				],
 			},
+			// {
+			// 	type: 'minor',
+			// 	header: 'Minor updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'decktracker',
+			// 			details: [
+			// 				{
+			// 					type: 'bug',
+			// 					text: `Secrets are now grouped by class once more in the Secrets Helper.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
