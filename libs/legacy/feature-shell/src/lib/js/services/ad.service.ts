a17303eac3e2c7555411cf3956a8809b6900f631
sebastientromp <sebastien.tromp@gmail.com>
2024-02-08 15:22:17 +0100

    DEV: add way to toggle ads in dev

--- libs/legacy/feature-shell/src/lib/js/services/ad.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/ad.service.ts
@@ -9,7 +9,7 @@ import {
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { AppUiStoreFacadeService } from './ui-store/app-ui-store-facade.service';
 
-const SHOW_ADS_IN_DEV = true;
+const SHOW_ADS_IN_DEV = false;
 
 @Injectable()
 export class AdService extends AbstractFacadeService<AdService> implements IAdsService {
@@ -36,6 +36,7 @@ export class AdService extends AbstractFacadeService<AdService> implements IAdsS
 		this.hasPremiumSub$$ = new BehaviorSubject<boolean>(false);
 		this.ow = AppInjector.get(OverwolfService);
 		this.store = AppInjector.get(AppUiStoreFacadeService);
+		this.addDevMode();
 
 		this.ow.onSubscriptionChanged(async (event) => {
 			console.log('[ads] subscription changed', event);
@@ -64,10 +65,10 @@ export class AdService extends AbstractFacadeService<AdService> implements IAdsS
 	}
 
 	public async shouldDisplayAdsInternal(): Promise<boolean> {
-		if (process.env.NODE_ENV !== 'production') {
-			console.warn('[ads] not display in dev');
-			return SHOW_ADS_IN_DEV;
-		}
+		// if (process.env.NODE_ENV !== 'production') {
+		// 	console.warn('[ads] not display in dev');
+		// 	return SHOW_ADS_IN_DEV;
+		// }
 		return new Promise<boolean>(async (resolve) => {
 			// Use OW's subscription mechanism
 			const [showAds, user] = await Promise.all([this.ow.shouldShowAds(), this.ow.getCurrentUser()]);
@@ -96,11 +97,23 @@ export class AdService extends AbstractFacadeService<AdService> implements IAdsS
 	}
 
 	private async hasPremiumSubInternal(): Promise<boolean> {
-		if (process.env.NODE_ENV !== 'production') {
-			console.warn('[ads] not display in dev');
-			return !SHOW_ADS_IN_DEV;
-		}
+		// if (process.env.NODE_ENV !== 'production') {
+		// 	console.warn('[ads] not display in dev');
+		// 	return !SHOW_ADS_IN_DEV;
+		// }
 		const shouldDisplayAds = await this.shouldDisplayAds();
 		return !shouldDisplayAds;
+	}
+
+	private addDevMode() {
+		if (process.env.NODE_ENV === 'production') {
+			return;
+		}
+		window['toggleAds'] = () => {
+			this.showAds$$.next(!this.showAds$$.value);
+			this.enablePremiumFeatures$$.next(!this.enablePremiumFeatures$$.value);
+			this.hasPremiumSub$$.next(!this.hasPremiumSub$$.value);
+			console.debug('[ads] toggled ads', this.showAds$$.value);
+		};
 	}
 }
