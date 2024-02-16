c599927c382edf4b88384dd7224e39f239e2064e
sebastientromp <sebastien.tromp@gmail.com>
2024-02-16 10:58:28 +0100

    LOG (coliseum): add debug log for quest reward

--- libs/replay/coliseum/src/lib/components/game/hero/bgs-quest-reward.component.ts
+++ libs/replay/coliseum/src/lib/components/game/hero/bgs-quest-reward.component.ts
@@ -25,6 +25,6 @@ export class BgsQuestRewardComponent {
 		this._reward = value;
 		this.entityId = value.id;
 		this.cardId = value.cardID;
-		console.debug('set reward', value.cardID, value.tags.toJS());
+		console.log('set reward', value.cardID, value.tags.toJS());
 	}
 }
