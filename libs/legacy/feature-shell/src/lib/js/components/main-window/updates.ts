dca2fe2d86b57845a4d1d51f02079d28bf04d29e
sebastientromp <sebastien.tromp@gmail.com>
2024-02-19 17:08:49 +0100

    RELEASE: 13.5.4

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,15 +33,15 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.5.2',
+		version: '13.5.4',
 		force: false,
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `IMPORTANT: some people have reported app crashes since the latest updates. The reports for now have all been on Battlegrounds, so I'm disabling the new Quests live stats for now. Please let me know if this improves the situation for you (you can reach out on Discord: https://discord.gg/vKeB3gnKTy or by using the "Report a bug" feature at the top right of the app's main window).
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `IMPORTANT: some people have reported app crashes since the latest updates. The reports for now have all been on Battlegrounds, so I'm disabling the new Quests live stats for now. Please let me know if this improves the situation for you (you can reach out on Discord: https://discord.gg/vKeB3gnKTy or by using the "Report a bug" feature at the top right of the app's main window).
+			// 	`,
+			// },
 			{
 				type: 'main',
 				header: 'Main updates',
@@ -51,15 +51,7 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `You can now browse the stats for the Quests and Rewards in the app.`,
-							},
-							{
-								type: 'feature',
-								text: `View the Quest/Reward stats directly in the overlay! This shows the reward's average placement and the quest's average turns to complete. This is a premium feature, but free users can still use it a couple of times a day (the exact quantity can still change in the future)`,
-							},
-							{
-								type: 'bug',
-								text: `I forgot to add the setting to disable the quest overlay in 13.5.0, it should now be available under Settings > Battlegrounds > Overlay. Sorry about that!`,
+								text: `You can now browse the stats for the Quests and Rewards in the app (it's back on again!).`,
 							},
 						],
 					},
@@ -73,8 +65,16 @@ export const updates: readonly Update[] = [
 						category: 'decktracker',
 						details: [
 							{
+								type: 'feature',
+								text: `Update the contents of Harth Stonebrew's hands following the 28.6.1 server-side hotfix. I'm still missing an updated version for Blood DK, Unholy DK and Paladin, so please send a screenshot my way if you have one :)`,
+							},
+							{
 								type: 'bug',
-								text: `Fix an issue where the Earthen Golem counter wouldn't appear anymore.`,
+								text: `Fix an issue where the opponent's hand markers would display incorrect information after playing some of the cards created by Harth Stonebrew.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the opponent's corpse counter would not appear.`,
 							},
 						],
 					},
@@ -82,12 +82,24 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'bug',
-								text: `Fix a sim issue where the Stable Amalgamation reward would be treated as Avenge (1).`,
+								type: 'feature',
+								text: `Add a slider to resize the Quests Overlay (under Settings > Battlegrounds > Overlay, at the bottom).`,
 							},
 							{
 								type: 'bug',
-								text: `Fix a sim issue where the effects that summons a minion "when there space" for them would not work as intended.`,
+								text: `Fix an issue where the toggle for the Quests Overlay would not work properly.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where hero powers with Avenge effects would sometimes not trigger.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where quests could get completed during a battle.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a sim issue where Golden Mad Matador would only trigger once.`,
 							},
 						],
 					},
