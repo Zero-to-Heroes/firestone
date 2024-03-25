37e33ed53f9030486105ae0490f8df9cbdf85165
sebastientromp <sebastien.tromp@gmail.com>
2024-03-25 16:59:12 +0100

    RELEASE: 13.8.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.7.5',
+		version: '13.8.0',
 		force: false,
 		sections: [
 			// {
@@ -47,36 +47,19 @@ export const updates: readonly Update[] = [
 				header: 'Main updates',
 				updates: [
 					{
-						category: 'decktracker',
-						details: [
-							{
-								type: 'feature',
-								text: `(PREMIUM) Add a mulligan guide for constructed. It gives you the impact of keeping each card during the mulligan, as well as the percentage of players who keep each card.`,
-							},
-							{
-								type: 'feature',
-								text: `Non-premium users can use the mulligan guide for free 3 times a day.`,
-							},
-						],
-					},
-					{
 						category: 'arena',
 						details: [
 							{
 								type: 'feature',
-								text: `You can now see your deck scores in the My Runs list. This is not retroactive, and will only be available for drafts started after this update.`,
+								text: `The Cards Stats tab now have more stats than ever! You can browse the Deck Winrate and Mulligan Winrate for each card.`,
 							},
 							{
 								type: 'feature',
-								text: `You can now see a list of recent decks that went to 10+ wins, as well as the choices they made during the draft.`,
+								text: `(Premium) The Draft overlay now also shows the Deck Winrate in addition to the Drawn Winrate and Pick rate stats.`,
 							},
 							{
 								type: 'feature',
-								text: `Add the "Deck Winrate" stat in the Card Stats tab.`,
-							},
-							{
-								type: 'feature',
-								text: `Add the "Mulligan Winrate" stat in the Card Stats tab.`,
+								text: `Synergies are now highlighted when mousing over cards in a deck list or draft picks. This works both for your own decks, and the ones from the High-Wins Decks tab.`,
 							},
 						],
 					},
@@ -87,11 +70,68 @@ export const updates: readonly Update[] = [
 				header: 'Minor updates',
 				updates: [
 					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'feature',
+								text: `The opponent's decklist will now be hidden until the mulligan is over, so as to limit info leaks caused by Whizbang decks.`,
+							},
+							{
+								type: 'feature',
+								text: `Add a "Dragons Summoned" counter for Zarimi.`,
+							},
+							{
+								type: 'feature',
+								text: `Add a "Cards Drawn" counter for Playhouse Giant.`,
+							},
+							{
+								type: 'feature',
+								text: `Add a "Turns left before death" counter for Wheel of DEATH!!!`,
+							},
+							{
+								type: 'feature',
+								text: `Add a counter to keep track of (1)-cost cards played for Thirsty Drifter.`,
+							},
+							{
+								type: 'content',
+								text: `When playing as/against Whizbang, the deck card now appears in the Global Effects section, as a reminder of what your deck is about.`,
+							},
+							{
+								type: 'bug',
+								text: `The tracker should now show the correct cost for Zilliax.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where resizing the mulligan overlay would not work very well.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Fizzle's Snapshots would be grouped in a single line in your hand, thus preventing you from accessing the contents of each snapshot.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Zilliax would not be removed from your tracker when drawn or destroyed.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where Loaner decks would not be properly detected.`,
+							},
+						],
+					},
+					{
 						category: 'battlegrounds',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix a simulation issue when Matador doesn't have any valid target left.`,
+								text: `Fix an issue where the Lord of Gains counter would count normal and golden versions of Spellcrafts as two different spells.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a simulation issue with Matador.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix a simulation issue with Flourishing Frostling.`,
 							},
 						],
 					},
@@ -99,8 +139,30 @@ export const updates: readonly Update[] = [
 						category: 'arena',
 						details: [
 							{
+								type: 'feature',
+								text: `The Deck Score now uses the Deck Winrate of each card, instead of the Drawn Winrate.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where resizing the mulligan overlay would not work very well.`,
+							},
+						],
+					},
+					{
+						category: 'collection',
+						details: [
+							{
+								type: 'bug',
+								text: `Class card packs don't have a guaranteed legendary in the first 10 packs, and the pity timers have been updated to reflect that.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
 								type: 'ui',
-								text: `Update how stats are displayed for the Arena draft stats overlay.`,
+								text: `Show the name of the last patch (e.g. 29.0.1) instead of simply "Last Patch" in the time filters.`,
 							},
 						],
 					},
