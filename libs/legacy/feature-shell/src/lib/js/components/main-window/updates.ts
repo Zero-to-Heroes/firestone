2453dac133d60131efaa77c8dfcab51a66e8cdd7
sebastientromp <sebastien.tromp@gmail.com>
2024-04-02 21:41:59 +0200

    RELEASE: 13.9.0

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.8.2',
+		version: '13.9.0',
 		force: false,
 		sections: [
 			// {
@@ -42,31 +42,66 @@ export const updates: readonly Update[] = [
 			// 	text: `IMPORTANT: some people have reported app crashes since the latest updates. The reports for now have all been on Battlegrounds, so I'm disabling the new Quests live stats for now. Please let me know if this improves the situation for you (you can reach out on Discord: https://discord.gg/vKeB3gnKTy or by using the "Report a bug" feature at the top right of the app's main window).
 			// 	`,
 			// },
-			// {
-			// 	type: 'main',
-			// 	header: 'Main updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'arena',
-			// 			details: [
-			// 				{
-			// 					type: 'feature',
-			// 					text: `The Cards Stats tab now have more stats than ever! You can browse the Deck Winrate and Mulligan Winrate for each card.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'main',
+				header: 'Main updates',
+				updates: [
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'misc',
+								text: `I have identified a few memory leaks in the app. I have started fixing the biggest ones, but it's a work in progress. This should help with the app's performance over time.`,
+							},
+						],
+					},
+				],
+			},
 			{
 				type: 'minor',
 				header: 'Minor updates',
 				updates: [
 					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'content',
+								text: `Update the hands given by Harth Stonebrew.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the dust cost of decks would be incorrect with Ziliax.`,
+							},
+						],
+					},
+					{
 						category: 'battlegrounds',
 						details: [
 							{
 								type: 'feature',
-								text: `You can now enable an option to show your opponent's MMR directly in the overlay. WARNING: this uses a name lookup on the official leaderboards to get the information. As such, it is not 100% reliable, especially at the "lower" MMR (< 8000).`,
+								text: `There is now a setting you can turn on to retrieve your opponents MMR from the official leaderboard. WARNING: this is a name-only lookup, so it's possible that the displayed MMR won't be accurate. It also only includes players with an MMR > 8000, since this is what the leaderboards track.`,
+							},
+							{
+								type: 'ui',
+								text: `Slightly tweak the positioning of the quest stats widgets to avoid hiding the quest text.`,
+							},
+						],
+					},
+					{
+						category: 'arena',
+						details: [
+							{
+								type: 'feature',
+								text: `Run overviews now also showcase the legendaries directly in the summary.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where the link to Twitch streams would sometimes point to a missing page.`,
 							},
 						],
 					},
