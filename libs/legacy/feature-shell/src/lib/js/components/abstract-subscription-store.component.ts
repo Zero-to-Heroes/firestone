63810328729972b2376c58119b18afe55808bcf7
sebastientromp <sebastien.tromp@gmail.com>
2024-04-04 09:11:58 +0200

    DEV: abstract-subscription-store.component.ts now uses PrefServices instead of store

--- libs/legacy/feature-shell/src/lib/js/components/abstract-subscription-store.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/abstract-subscription-store.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Injectable } from '@angular/core';
-import { Preferences } from '@firestone/shared/common/service';
+import { Preferences, PreferencesService } from '@firestone/shared/common/service';
 import { AbstractSubscriptionComponent } from '@firestone/shared/framework/common';
+import { AppInjector } from '@firestone/shared/framework/core';
 import { AppUiStoreFacadeService } from '../services/ui-store/app-ui-store-facade.service';
 
 /**
@@ -8,12 +9,15 @@ import { AppUiStoreFacadeService } from '../services/ui-store/app-ui-store-facad
  */
 @Injectable()
 export abstract class AbstractSubscriptionStoreComponent extends AbstractSubscriptionComponent {
-	constructor(protected readonly store: AppUiStoreFacadeService, protected readonly cdr: ChangeDetectorRef) {
+	private readonly _prefs: PreferencesService;
+
+	constructor(store: AppUiStoreFacadeService, protected readonly cdr: ChangeDetectorRef) {
 		super(cdr);
+		this._prefs = AppInjector.get(PreferencesService);
 	}
 
 	/** @deprecated use PreferencesService.preferences$() instead */
 	protected listenForBasicPref$<T>(selector: (prefs: Preferences) => T, ...logArgs: any[]) {
-		return this.store.listenPrefs$((prefs) => selector(prefs)).pipe(this.mapData(([pref]) => pref));
+		return this._prefs.preferences$$.pipe(this.mapData((prefs) => selector(prefs)));
 	}
 }
