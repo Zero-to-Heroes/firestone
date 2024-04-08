0acdc1ed65cb21ecbda897ccd61079c572ade818
sebastientromp <sebastien.tromp@gmail.com>
2024-04-08 18:09:05 +0200

    DEV: fix memory leak in bgs-hero-tips.component.ts

--- libs/legacy/feature-shell/src/lib/js/components/overlays/tips/bgs-hero-tips.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/overlays/tips/bgs-hero-tips.component.ts
@@ -1,11 +1,11 @@
 import { ComponentType } from '@angular/cdk/portal';
-import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewRef } from '@angular/core';
 import { BgsHeroStrategyTipsTooltipComponent } from '@components/battlegrounds/hero-selection/bgs-hero-strategy-tips-tooltip.component';
 import { normalizeCardId } from '@components/battlegrounds/post-match/card-utils';
-import { CardsFacadeService } from '@firestone/shared/framework/core';
+import { BgsStateFacadeService } from '@firestone/battlegrounds/common';
+import { AbstractSubscriptionComponent } from '@firestone/shared/framework/common';
+import { CardsFacadeService, waitForReady } from '@firestone/shared/framework/core';
 import { Observable } from 'rxjs';
-import { AppUiStoreFacadeService } from '../../../services/ui-store/app-ui-store-facade.service';
-import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-store.component';
 
 @Component({
 	selector: 'bgs-hero-tips',
@@ -28,22 +28,27 @@ import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BgsHeroTipsComponent extends AbstractSubscriptionStoreComponent implements AfterContentInit {
+export class BgsHeroTipsComponent extends AbstractSubscriptionComponent implements AfterContentInit {
 	componentType: ComponentType<BgsHeroStrategyTipsTooltipComponent> = BgsHeroStrategyTipsTooltipComponent;
 
 	currentHeroCardId$: Observable<string>;
 
 	constructor(
-		protected readonly store: AppUiStoreFacadeService,
 		protected readonly cdr: ChangeDetectorRef,
 		private readonly allCards: CardsFacadeService,
+		private readonly bgsState: BgsStateFacadeService,
 	) {
-		super(store, cdr);
+		super(cdr);
 	}
 
-	ngAfterContentInit() {
-		this.currentHeroCardId$ = this.store
-			.listenBattlegrounds$(([state]) => state.currentGame?.getMainPlayer()?.cardId)
-			.pipe(this.mapData(([cardId]) => normalizeCardId(cardId, this.allCards)));
+	async ngAfterContentInit() {
+		await waitForReady(this.bgsState);
+		this.currentHeroCardId$ = this.bgsState.gameState$$.pipe(
+			this.mapData((state) => normalizeCardId(state.currentGame?.getMainPlayer()?.cardId, this.allCards)),
+		);
+
+		if (!(this.cdr as ViewRef).destroyed) {
+			this.cdr.detectChanges();
+		}
 	}
 }
