dff8594e63825712fc55e41b5b7c993dd82efdd5
sebastientromp <sebastien.tromp@gmail.com>
2024-04-23 15:44:24 +0200

    BUG (bgs): fix face-offs recap not reporting when opponent has skins

--- libs/legacy/feature-shell/src/lib/js/components/battlegrounds/in-game/bgs-hero-face-offs.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/battlegrounds/in-game/bgs-hero-face-offs.component.ts
@@ -1,11 +1,11 @@
-import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewRef } from '@angular/core';
 import { BgsFaceOff } from '@firestone-hs/hs-replay-xml-parser/dist/lib/model/bgs-face-off';
-import { BgsNextOpponentOverviewPanel, BgsPlayer } from '@firestone/battlegrounds/common';
+import { normalizeHeroCardId } from '@firestone-hs/reference-data';
+import { BgsNextOpponentOverviewPanel, BgsPlayer, BgsStateFacadeService } from '@firestone/battlegrounds/common';
+import { AbstractSubscriptionComponent, deepEqual, groupByFunction } from '@firestone/shared/framework/common';
+import { CardsFacadeService, waitForReady } from '@firestone/shared/framework/core';
 import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { AppUiStoreFacadeService } from '../../../services/ui-store/app-ui-store-facade.service';
-import { deepEqual, groupByFunction } from '../../../services/utils';
-import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-store.component';
+import { filter, tap } from 'rxjs/operators';
 
 @Component({
 	selector: 'bgs-hero-face-offs',
@@ -36,63 +36,81 @@ import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BgsHeroFaceOffsComponent extends AbstractSubscriptionStoreComponent implements AfterContentInit {
+export class BgsHeroFaceOffsComponent extends AbstractSubscriptionComponent implements AfterContentInit {
 	nextOpponentCardId$: Observable<string>;
 	opponents$: Observable<readonly BgsPlayer[]>;
 	faceOffsByOpponent$: Observable<{ [opponentHeroCardId: string]: readonly BgsFaceOff[] }>;
 
-	constructor(protected readonly store: AppUiStoreFacadeService, protected readonly cdr: ChangeDetectorRef) {
-		super(store, cdr);
+	constructor(
+		protected readonly cdr: ChangeDetectorRef,
+		private readonly state: BgsStateFacadeService,
+		private readonly allCards: CardsFacadeService,
+	) {
+		super(cdr);
 	}
 
-	ngAfterContentInit() {
-		const currentPanel$: Observable<BgsNextOpponentOverviewPanel> = this.store
-			.listenBattlegrounds$(
-				([state]) => state.panels,
-				([state]) => state.currentPanelId,
-			)
-			.pipe(
-				filter(([panels, currentPanelId]) => !!panels?.length && !!currentPanelId),
-				this.mapData(
-					([panels, currentPanelId]) =>
-						panels.find((panel) => panel.id === currentPanelId) as BgsNextOpponentOverviewPanel,
-				),
-			);
+	async ngAfterContentInit() {
+		await waitForReady(this.state);
+
+		const currentPanel$: Observable<BgsNextOpponentOverviewPanel> = this.state.gameState$$.pipe(
+			this.mapData((state) => ({
+				panels: state.panels,
+				currentPanelId: state.currentPanelId,
+			})),
+			filter(({ panels, currentPanelId }) => !!panels?.length && !!currentPanelId),
+			this.mapData(
+				({ panels, currentPanelId }) =>
+					panels.find((panel) => panel.id === currentPanelId) as BgsNextOpponentOverviewPanel,
+			),
+		);
 		this.nextOpponentCardId$ = currentPanel$.pipe(this.mapData((panel) => panel?.opponentOverview?.cardId));
-		this.faceOffsByOpponent$ = this.store
-			.listenBattlegrounds$(([state]) => state.currentGame?.faceOffs)
-			.pipe(
-				this.mapData(
-					([faceOffs]) => groupByFunction((faceOff: BgsFaceOff) => faceOff.opponentCardId)(faceOffs ?? []),
-					(a, b) => deepEqual(a, b),
+		this.faceOffsByOpponent$ = this.state.gameState$$.pipe(
+			this.mapData((state) => state.currentGame?.faceOffs),
+			this.mapData(
+				(faceOffs) => {
+					const result = groupByFunction((faceOff: BgsFaceOff) =>
+						normalizeHeroCardId(faceOff.opponentCardId, this.allCards),
+					)(faceOffs ?? []);
+					return result;
+				},
+				(a, b) => deepEqual(a, b),
+			),
+		);
+		this.opponents$ = this.state.gameState$$.pipe(
+			this.mapData((state) => state.currentGame?.players),
+			filter((players) => !!players?.length),
+			this.mapData(
+				(players) =>
+					players
+						.filter((player) => !player.isMainPlayer)
+						.sort((a, b) => {
+							if (a.leaderboardPlace < b.leaderboardPlace) {
+								return -1;
+							}
+							if (b.leaderboardPlace < a.leaderboardPlace) {
+								return 1;
+							}
+							if (a.damageTaken < b.damageTaken) {
+								return -1;
+							}
+							if (b.damageTaken < a.damageTaken) {
+								return 1;
+							}
+							return 0;
+						}),
+				(a, b) => deepEqual(a, b),
+			),
+			tap((info) =>
+				console.debug(
+					'opponents',
+					info.map((o) => o.cardId),
 				),
-			);
-		this.opponents$ = this.store
-			.listenBattlegrounds$(([state]) => state.currentGame?.players)
-			.pipe(
-				filter(([players]) => !!players?.length),
-				this.mapData(
-					([players]) =>
-						players
-							.filter((player) => !player.isMainPlayer)
-							.sort((a, b) => {
-								if (a.leaderboardPlace < b.leaderboardPlace) {
-									return -1;
-								}
-								if (b.leaderboardPlace < a.leaderboardPlace) {
-									return 1;
-								}
-								if (a.damageTaken < b.damageTaken) {
-									return -1;
-								}
-								if (b.damageTaken < a.damageTaken) {
-									return 1;
-								}
-								return 0;
-							}),
-					(a, b) => deepEqual(a, b),
-				),
-			);
+			),
+		);
+
+		if (!(this.cdr as ViewRef).destroyed) {
+			this.cdr.detectChanges();
+		}
 	}
 
 	trackByFn(index, item: BgsPlayer) {
