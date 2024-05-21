9b619181ff53001dfbc6b3605feeed253403f027
sebastientromp <sebastien.tromp@gmail.com>
2024-05-21 16:58:26 +0200

    DEV: fix prod compilation issue

--- libs/settings/src/index.ts
+++ libs/settings/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/common/services/bootstrap-settings.service';
 export * from './lib/common/services/custom-appearance.service';
+export * from './lib/general/custom-color-picker.component';
 export * from './lib/general/settings-general-appearance.component';
 export * from './lib/settings.module';
