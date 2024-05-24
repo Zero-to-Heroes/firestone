a2203da748e8aa8299072e7c160ffee8114fe39c
sebastientromp <sebastien.tromp@gmail.com>
2024-05-24 11:45:56 +0200

    RELEASE: 13.14.5

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.14.0',
+		version: '13.14.5',
 		force: false,
 		sections: [
 			// {
@@ -53,25 +53,116 @@ export const updates: readonly Update[] = [
 								type: 'feature',
 								text: `You can now customize the color of the background for some of the widgets. This is a very early step towards more customization, so don't hesitate to send me your feedback if you have any!`,
 							},
+							{
+								type: 'feature',
+								text: `(Premium) The Hero Selection Overlay stats has been improved, and should now make it clearer what the impact of the current tribes is on each hero's average position, as well as align the look and feel with how other similar stats are presented.`,
+							},
+							{
+								type: 'feature',
+								text: `Non-premium users can use the Hero Stats Overlay for free once every day.`,
+							},
 						],
 					},
 				],
 			},
-			// {
-			// 	type: 'minor',
-			// 	header: 'Minor updates',
-			// 	updates: [
-			// 		{
-			// 			category: 'decktracker',
-			// 			details: [
-			// 				{
-			// 					type: 'bug',
-			// 					text: `Fix an issue where Zilliax would not be properly tracked when shuffled back into the deck.`,
-			// 				},
-			// 			],
-			// 		},
-			// 	],
-			// },
+			{
+				type: 'minor',
+				header: 'Minor updates',
+				updates: [
+					{
+						category: 'battlegrounds',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where Unpinning a minion in the minions list would unpin all of them.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the MMR / Tribes filter was not taken into account when building hero stats (and the hero tier list) in-game.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the MMR graph tab would mix Solo and Duos. You can now select which game mode you want to Rating graph for.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix multiple simulation issues`,
+							},
+						],
+					},
+					{
+						category: 'decktracker',
+						details: [
+							{
+								type: 'content',
+								text: `Repackaged Box now show you the contents of the Box when mousing over it in the decklist.`,
+							},
+							{
+								type: 'content',
+								text: `Add several card oracles (flag the correct card in the opponent's hand).`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where merging decks would sometimes not work, or sometimes split them apart.`,
+							},
+						],
+					},
+					{
+						category: 'arena',
+						details: [
+							{
+								type: 'feature',
+								text: `You can now filter High-Wins Runs based on legendary cards.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'feature',
+								text: `(Twitch) You can now see the hero portrait when mousing over it.`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Constructed) Add a button to minimize the tracker.`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Constructed) Add an option to color the mana cost of cards in the tracker.`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Constructed) Mousing over a card in hand also highlights related cards in the tracker.`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Battlegrounds) Add a mouse over for the hero's completed quest (yes, that's coming late, but at least it will be there for the next time Quests are live).`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Battlegrounds) When mousing over Sire D's hero power, it will now show the quest reward, instead of the hero power image.`,
+							},
+							{
+								type: 'feature',
+								text: `(Twitch) (Battlegrounds) Add spells in the minions list (this can be turned on/off from the extension's overlay Settings by each user).`,
+							},
+							{
+								type: 'bug',
+								text: `(Twitch) The Settings panel now will keep its size even when changing the size of the overlays.`,
+							},
+							{
+								type: 'bug',
+								text: `(Twitch) Fix an issue where the language set in the Settings was not always taken into account.`,
+							},
+							{
+								type: 'bug',
+								text: `(Twitch) Fix an issue where order of the cards on the board was not always refreshed.`,
+							},
+						],
+					},
+				],
+			},
 			// {
 			// 	type: 'future',
 			// 	header: 'Under the Hood',
