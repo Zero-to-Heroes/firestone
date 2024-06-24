fc20d485e0a9792e873d7971750b7ff1732d8675
sebastientromp <sebastien.tromp@gmail.com>
2024-06-24 10:07:37 +0200

    RELEASE: 13.17.2

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.16.2',
+		version: '13.17.1',
 		force: false,
 		sections: [
 			// {
@@ -42,21 +42,21 @@ export const updates: readonly Update[] = [
 			// 	text: `IMPORTANT: some people have reported app crashes since the latest updates. The reports for now have all been on Battlegrounds, so I'm disabling the new Quests live stats for now. Please let me know if this improves the situation for you (you can reach out on Discord: https://discord.gg/vKeB3gnKTy or by using the "Report a bug" feature at the top right of the app's main window).
 			// 	`,
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'content',
-								text: `Firestone has been updated for patch 29.6.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'content',
+			// 					text: `Firestone has been updated for patch 29.6.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -66,20 +66,57 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Add a scrollbar to the Mulligan Overview widget when there are too many cards.`,
+								text: `Activating the Streamer Mode in Firestone now hides the opponent's name in the decktracker.`,
+							},
+							{
+								type: 'content',
+								text: `Add many oracles and card highlights. These are usually not written in the release notes anymore, but I just wanted to mention it so you know that these are continually being improved on :)`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where some cards created in deck would not be flagged as gifts when drawn by the opponent.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the mana cost of cards transformed in hand (like Shifter Zerus) would not be properly updated.`,
+							},
+							{
+								type: 'ui',
+								text: `Move the format info of the mulligan deck overview (standard/wild/twist) to a second line to reduce the horizontal space taken by the widget.`,
 							},
 						],
 					},
 					{
-						category: 'arena',
+						category: 'battlegrounds',
 						details: [
 							{
+								type: 'feature',
+								text: `There is now an option to always show Tier 7 minions in the minions list.`,
+							},
+							{
 								type: 'ui',
-								text: `Squeeze the columns smaller in the Card Stats page when viewing the stats on smaller screens.`,
+								text: `The minion stats should now appear on top of the overlays (like Divine Shield, Stealth, etc.) when viewing replays so that they are clearly visible at all times.`,
 							},
 							{
 								type: 'bug',
-								text: `(Premium) Fix an issue where the stats recap in the sidebar would not update after changing the time filter.`,
+								text: `Fix many simulation issues. I still expect a few releases over the week to get to pre-patch level of accuracy, but the odds should be decently reliable at the moment. Note: one of difficulties with having full accuracy is that Hearthstone itself has a few bugs; so the simulation can be accurate, but the game itself can still be wrong, which results in incorrect odds.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where some buddies would not appear in the buddies list.`,
+							},
+							{
+								type: 'bug',
+								text: `(Premium) Fix some live stats being incorrectly computed while in game ("coins wasted" and "enemy heroes killed" mainly).`,
+							},
+						],
+					},
+					{
+						category: 'collection',
+						details: [
+							{
+								type: 'bug',
+								text: `Fix an issue where the count of Diamond / Signature cards would not appear when browsing the collection.`,
 							},
 						],
 					},
