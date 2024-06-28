a2df359aabab3686a15213b5e4c6bea99796487a
sebastientromp <sebastien.tromp@gmail.com>
2024-06-28 21:10:20 +0200

    LOG: remove debug logs

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/copied-from-entity-id-parser.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/copied-from-entity-id-parser.ts
@@ -31,16 +31,16 @@ export class CopiedFromEntityIdParser implements EventParser {
 		// The issue when using only the entityId is that we can't find the card in deck, as
 		// the entityId is not stored there
 		let copiedCard: DeckCard = copiedDeck.findCard(copiedCardEntityId)?.card;
-		console.debug(
-			'[copied-from-entity] copiedCard',
-			isPlayer,
-			copiedCard,
-			newCopy,
-			copiedDeck,
-			copiedCardEntityId,
-			gameEvent,
-			deck,
-		);
+		// console.debug(
+		// 	'[copied-from-entity] copiedCard',
+		// 	isPlayer,
+		// 	copiedCard,
+		// 	newCopy,
+		// 	copiedDeck,
+		// 	copiedCardEntityId,
+		// 	gameEvent,
+		// 	deck,
+		// );
 
 		// Typically happens when the opponent copies a card in our deck. Their copy is known (we know entityId + cardId)
 		// but it references an entityId on our side that we don't know of (if it's in the deck)
@@ -51,7 +51,7 @@ export class CopiedFromEntityIdParser implements EventParser {
 					(card) =>
 						card.cardId === copyCardId && card.positionFromBottom == null && card.positionFromTop == null,
 				) ?? copiedDeck.deck.find((card) => card.cardId === copyCardId);
-			console.debug('[copied-from-entity] copiedCard not found', copiedCard, copyCardId, copiedDeck.deck);
+			// console.debug('[copied-from-entity] copiedCard not found', copiedCard, copyCardId, copiedDeck.deck);
 		}
 
 		// Cards like Masked Reveler summon a copy of a card from the deck. Because we don't store the entityId of
