1fe8192428bd116e96c06cd89e8dcf498b3c6845
sebastientromp <sebastien.tromp@gmail.com>
2024-07-01 10:08:49 +0200

    RELEASE: 13.17.13

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.17.1',
+		version: '13.17.13',
 		force: false,
 		sections: [
 			// {
@@ -66,23 +66,23 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `Activating the Streamer Mode in Firestone now hides the opponent's name in the decktracker.`,
+								text: `Flint Firearm now shows all Quickdraw cards you can get on mouse over. This new piece of tech will let me display dynamic card pools for other cards, so let me know which ones you'd like to see!`,
 							},
 							{
-								type: 'content',
-								text: `Add many oracles and card highlights. These are usually not written in the release notes anymore, but I just wanted to mention it so you know that these are continually being improved on :)`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some cards created in deck would not be flagged as gifts when drawn by the opponent.`,
+								type: 'feature',
+								text: `Cards received from Repackaged Box are now flagged in hand.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where the mana cost of cards transformed in hand (like Shifter Zerus) would not be properly updated.`,
+								text: `Fix an issue where secrets replayed by Tess (and probably Yogg) would not be flagged as gifts.`,
 							},
 							{
-								type: 'ui',
-								text: `Move the format info of the mulligan deck overview (standard/wild/twist) to a second line to reduce the horizontal space taken by the widget.`,
+								type: 'bug',
+								text: `Fix an issue where some cards like Tess would show incorrect information after playing a hero card and changing classes.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where some card tooltips would suddenly become empty.`,
 							},
 						],
 					},
@@ -90,33 +90,34 @@ export const updates: readonly Update[] = [
 						category: 'battlegrounds',
 						details: [
 							{
-								type: 'feature',
-								text: `There is now an option to always show Tier 7 minions in the minions list.`,
-							},
-							{
-								type: 'ui',
-								text: `The minion stats should now appear on top of the overlays (like Divine Shield, Stealth, etc.) when viewing replays so that they are clearly visible at all times.`,
+								type: 'bug',
+								text: `Only show Disguised Graverobber in the minions list when Undead are in.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix many simulation issues. I still expect a few releases over the week to get to pre-patch level of accuracy, but the odds should be decently reliable at the moment. Note: one of difficulties with having full accuracy is that Hearthstone itself has a few bugs; so the simulation can be accurate, but the game itself can still be wrong, which results in incorrect odds.`,
+								text: `Fix an issue where the opponnet's boards were not always properly detected when facing the ghosts in Duos.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue where some buddies would not appear in the buddies list.`,
-							},
-							{
-								type: 'bug',
-								text: `(Premium) Fix some live stats being incorrectly computed while in game ("coins wasted" and "enemy heroes killed" mainly).`,
+								text: `Fix multiple simulation issues.`,
 							},
 						],
 					},
 					{
-						category: 'collection',
+						category: 'arena',
 						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where the count of Diamond / Signature cards would not appear when browsing the collection.`,
+								text: `Fix an issue where a lot of match-related data points were missing from the card stats, like mulligan winrate.`,
+							},
+						],
+					},
+					{
+						category: 'general',
+						details: [
+							{
+								type: 'misc',
+								text: `Most images will now be streamed from the cloud instead of being embedded in the app. This reduces the download size of each update.`,
 							},
 						],
 					},
