452a1a08f560a42dbf9480eb7e874d60beb68678
sebastientromp <sebastien.tromp@gmail.com>
2024-07-10 10:08:50 +0200

    RELEASE: 13.18.6

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,7 +33,7 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.18.2',
+		version: '13.18.6',
 		force: false,
 		sections: [
 			// {
@@ -42,21 +42,21 @@ export const updates: readonly Update[] = [
 			// 	text: `IMPORTANT: some people have reported app crashes since the latest updates. The reports for now have all been on Battlegrounds, so I'm disabling the new Quests live stats for now. Please let me know if this improves the situation for you (you can reach out on Discord: https://discord.gg/vKeB3gnKTy or by using the "Report a bug" feature at the top right of the app's main window).
 			// 	`,
 			// },
-			{
-				type: 'main',
-				header: 'Main updates',
-				updates: [
-					{
-						category: 'general',
-						details: [
-							{
-								type: 'feature',
-								text: `A very first version of the Guilds feature is now available! They will allow you to share all kind of things Hearthston-related with your friends or communities. This first version adds a shared leaderboard for all game modes, and more things are planned for the future (let me know what you'd like to see!). Please note that guild creation is not yet publicly available, and is for now reserved to streamers who contact me directly on Discord.  <br/> The goal is to first see what are the features that click with the community, and then open it up to everyone.`,
-							},
-						],
-					},
-				],
-			},
+			// {
+			// 	type: 'main',
+			// 	header: 'Main updates',
+			// 	updates: [
+			// 		{
+			// 			category: 'general',
+			// 			details: [
+			// 				{
+			// 					type: 'feature',
+			// 					text: `A very first version of the Guilds feature is now available! They will allow you to share all kind of things Hearthston-related with your friends or communities. This first version adds a shared leaderboard for all game modes, and more things are planned for the future (let me know what you'd like to see!). Please note that guild creation is not yet publicly available, and is for now reserved to streamers who contact me directly on Discord.  <br/> The goal is to first see what are the features that click with the community, and then open it up to everyone.`,
+			// 				},
+			// 			],
+			// 		},
+			// 	],
+			// },
 			{
 				type: 'minor',
 				header: 'Minor updates',
@@ -65,53 +65,12 @@ export const updates: readonly Update[] = [
 						category: 'decktracker',
 						details: [
 							{
-								type: 'feature',
-								text: `Flint Firearm now shows all Quickdraw cards you can get on mouse over. This new piece of tech will let me display dynamic card pools for other cards, so let me know which ones you'd like to see!`,
-							},
-							{
-								type: 'feature',
-								text: `Cards received from Repackaged Box are now flagged in hand.`,
-							},
-							{
-								type: 'feature',
-								text: `Add the new opponent decks in Twist.`,
+								type: 'bug',
+								text: `Fix an issue where the attack counter would ignore the attack of the hero if a hero card was played this turn.`,
 							},
 							{
 								type: 'bug',
-								text: `Fix an issue that would show incorrect cards as being in your opponent's deck when using the Legacy Tracker mode.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where secrets replayed by Tess (and probably Yogg) would not be flagged as gifts.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some cards like Tess would show incorrect information after playing a hero card and changing classes.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where some card tooltips would suddenly become empty.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where card highlight for Lady Liadrin would also include spells played on non-friendly characters.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'bug',
-								text: `Only show Disguised Graverobber in the minions list when Undead are in.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the opponnet's boards were not always properly detected when facing the ghosts in Duos.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix multiple simulation issues.`,
+								text: `Fix an issue where the mana cost of Zilliax would always be 0 in the Legacy Tracker view.`,
 							},
 						],
 					},
@@ -120,16 +79,7 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'bug',
-								text: `Fix an issue where a lot of match-related data points were missing from the card stats, like mulligan winrate.`,
-							},
-						],
-					},
-					{
-						category: 'duels',
-						details: [
-							{
-								type: 'misc',
-								text: `At long last, it's time to let Duels go. This was a really fun mode, and I'm glad I could help so many of you with your runs. But it's time to move on to other things. The Duels entry in the menu has finally been removed.`,
+								text: `Fix an issue where the data in My Stats would reflect the current Hero Class filter from My Runs.`,
 							},
 						],
 					},
@@ -137,8 +87,12 @@ export const updates: readonly Update[] = [
 						category: 'general',
 						details: [
 							{
-								type: 'misc',
-								text: `Most images will now be streamed from the cloud instead of being embedded in the app. This reduces the download size of each update.`,
+								type: 'feature',
+								text: `Opening the app will now get you to the last tab that was open when you closed the app.`,
+							},
+							{
+								type: 'bug',
+								text: `Fix an issue where the Guilds' Arena leaderboard would incorrectly round down the average.`,
 							},
 						],
 					},
