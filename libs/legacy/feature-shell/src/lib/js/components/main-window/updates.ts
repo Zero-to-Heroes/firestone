3706736b512e03237a072aa1701f218c4d2c70d0
sebastientromp <sebastien.tromp@gmail.com>
2024-07-25 13:28:10 +0200

    RELEASE: 13.19.3

--- libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
+++ libs/legacy/feature-shell/src/lib/js/components/main-window/updates.ts
@@ -33,16 +33,16 @@ export interface UpdateSectionItemDetails {
 
 export const updates: readonly Update[] = [
 	{
-		version: '13.19.1',
+		version: '13.19.3',
 		force: false,
 		sections: [
-			{
-				type: 'intro',
-				header: 'Message from the dev',
-				text: `This release aims at adding as many features for the new Perils in Paradise expansions ahead of its official launch. Since they have been added while the cards themselves are not playable, please keep in mind that some of these might be a little buggy, so please leet me know if you encounter something that doesn't work as it should. <br/>
-				And many more goodies will come once I'm finally able to play with the cards and see how some of these actually work :)
-				`,
-			},
+			// {
+			// 	type: 'intro',
+			// 	header: 'Message from the dev',
+			// 	text: `This release aims at adding as many features for the new Perils in Paradise expansions ahead of its official launch. Since they have been added while the cards themselves are not playable, please keep in mind that some of these might be a little buggy, so please leet me know if you encounter something that doesn't work as it should. <br/>
+			// 	And many more goodies will come once I'm finally able to play with the cards and see how some of these actually work :)
+			// 	`,
+			// },
 			// {
 			// 	type: 'main',
 			// 	header: 'Main updates',
@@ -67,71 +67,35 @@ export const updates: readonly Update[] = [
 						details: [
 							{
 								type: 'feature',
-								text: `When mousing over Tidepool Pupil in your decklist, you can now see the 3 spells you can discover from when active. It will also show you the spells that have been "stored" if you haven't yet played 3 spells.`,
+								text: `Show the minions added in the Minion Sandwich when mousing over it in the decklist.`,
 							},
 							{
 								type: 'feature',
-								text: `Add a "locations used" counter for Seaside Giant.`,
+								text: `Update the "Top of deck" zone after playing Overplanner.`,
 							},
 							{
 								type: 'feature',
-								text: `Add a counter for Snatch and Grab that tracks how many cards from another class you've played this game. `,
+								text: `Track spells that will be replayed by Mistah Vistah's Scenic Vista.`,
 							},
 							{
 								type: 'feature',
-								text: `Add a counter for Sea Shanty that tracks how many spells you've played on characters this game. `,
+								text: `Update Eruption damage info when they are upgraded in deck.`,
 							},
 							{
 								type: 'feature',
-								text: `Add a counter for Damage Taken on your turn, for cards like Sauna Regular or Party Planner Vona. `,
+								text: `Add spell schools already played as related cards to Carress, Cabaret Star.`,
 							},
 							{
 								type: 'feature',
-								text: `The deckbuilder now properly supports Tourist cards.`,
+								text: `Show the Spell Schools widget for Razzle Dazzler.`,
 							},
-							{
-								type: 'content',
-								text: `Add more card highlights and oracles (flagging the card in the opponent's hand) for the new Perils in Paradise expansion.`,
-							},
-							{
-								type: 'bug',
-								text: `Fix an issue where the cost of cards in hand/deck would still be updated even if the "update card cost" option was disabled in the settings.`,
-							},
-						],
-					},
-					{
-						category: 'battlegrounds',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the simulator could sometimes crash because of Fish of N'Zoth.`,
-							},
-						],
-					},
-					{
-						category: 'collection',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the class filter would not work properly.`,
-							},
-						],
-					},
-					{
-						category: 'arena',
-						details: [
-							{
-								type: 'bug',
-								text: `Fix an issue where the high-wins runs could contain some obviously corrupted lists with multiple legendaries.`,
-							},
-						],
-					},
-					{
-						category: 'general',
-						details: [
 							{
 								type: 'feature',
-								text: `Guilds now have a tab for Friendly Battles! If both players from the same Guild play a friendly game, it will appear in the Guild's tab.`,
+								text: `Add a Dead Minions counter for Reska, the Pit Lord.`,
+							},
+							{
+								type: 'feature',
+								text: `Show the card that will be replayed by Sunwing Squawker when mousing over it in the decklist.`,
 							},
 						],
 					},
