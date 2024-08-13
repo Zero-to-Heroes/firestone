1ee88b4e6505b63a77c9c0e8d0df950d876fe5b6
sebastientromp <sebastien.tromp@gmail.com>
2024-08-13 14:25:48 +0200

    DEV: extract BgsBattleSimulationExecutorService to shared lib

--- apps/legacy/src/app/app.module.ts
+++ apps/legacy/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BattlegroundsSimulatorModule, BgsBattleSimulationExecutorService } from '@firestone/battlegrounds/simulator';
 import { LegacyFeatureShellModule } from '@firestone/legacy/feature-shell';
 import { SharedCommonServiceModule } from '@firestone/shared/common/service';
 import { SharedFrameworkCoreModule } from '@firestone/shared/framework/core';
 import { BgsBattlePositioningExecutorService } from '../../../../libs/legacy/feature-shell/src/lib/js/services/battlegrounds/bgs-battle-positioning-executor.service';
-import { BgsBattleSimulationExecutorService } from '../../../../libs/legacy/feature-shell/src/lib/js/services/battlegrounds/bgs-battle-simulation-executor.service';
 import { AppBoostrapperComponent } from './app-bootstrap.component';
 import { BgsBattlePositioningWorkerService } from './impl/bgs-battle-positioning-worker.service';
 import { BgsBattleSimulationWorkerService } from './impl/bgs-battle-simulation-worker.service';
 
 @NgModule({
 	declarations: [AppBoostrapperComponent],
-	imports: [BrowserModule, LegacyFeatureShellModule, SharedFrameworkCoreModule, SharedCommonServiceModule],
+	imports: [
+		BrowserModule,
+		LegacyFeatureShellModule,
+		SharedFrameworkCoreModule,
+		SharedCommonServiceModule,
+		BattlegroundsSimulatorModule,
+	],
 	providers: [
 		{ provide: BgsBattleSimulationExecutorService, useClass: BgsBattleSimulationWorkerService },
 		{ provide: BgsBattlePositioningExecutorService, useClass: BgsBattlePositioningWorkerService },
