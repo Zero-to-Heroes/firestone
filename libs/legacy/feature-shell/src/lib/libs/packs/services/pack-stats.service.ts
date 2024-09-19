dfb2b36b4b342ca06684a35d0294956c2d14db7e
sebastientromp <sebastien.tromp@gmail.com>
2024-09-19 10:13:30 +0200

    DEV: update URL endpoint for pack stats

--- libs/legacy/feature-shell/src/lib/libs/packs/services/pack-stats.service.ts
+++ libs/legacy/feature-shell/src/lib/libs/packs/services/pack-stats.service.ts
@@ -8,8 +8,8 @@ import { Events } from '../../../js/services/events.service';
 import { CollectionPacksUpdatedEvent } from '../../../js/services/mainwindow/store/events/collection/colection-packs-updated-event';
 import { AppUiStoreFacadeService } from '../../../js/services/ui-store/app-ui-store-facade.service';
 
-const PACKS_UPDATE_URL = 'https://uuglyygz3fyixbjbmquk3kcj540acpze.lambda-url.us-west-2.on.aws/';
-const PACKS_RETRIEVE_URL = 'https://reycp2lobj5voigcqandjgyvoy0vsmyf.lambda-url.us-west-2.on.aws/';
+const PACKS_UPDATE_URL = 'https://zbfdquy6qvpmragkcwjzpr3v5a0bcgfn.lambda-url.us-west-2.on.aws/';
+const PACKS_RETRIEVE_URL = 'https://dwfrvwcatnkfohkhcksuxweoii0mtgch.lambda-url.us-west-2.on.aws/';
 
 @Injectable()
 export class PackStatsService {
