f596a0b608308f131fac44c011a761b181194ee7
sebastientromp <sebastien.tromp@gmail.com>
2024-09-30 08:43:38 +0200

    DEV: prep for battles screenshot

--- libs/legacy/feature-shell/src/lib/js/components/battlegrounds/battles/bgs-battles-view.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/battlegrounds/battles/bgs-battles-view.component.ts
@@ -3,6 +3,7 @@ import {
 	ChangeDetectionStrategy,
 	ChangeDetectorRef,
 	Component,
+	ElementRef,
 	EventEmitter,
 	Input,
 } from '@angular/core';
@@ -11,6 +12,8 @@ import { BgsFaceOffWithSimulation } from '@firestone/battlegrounds/core';
 import { AnalyticsService, OverwolfService } from '@firestone/shared/framework/core';
 import { BattlegroundsStoreEvent } from '@services/battlegrounds/store/events/_battlegrounds-store-event';
 import { BgsSelectBattleEvent } from '@services/battlegrounds/store/events/bgs-select-battle-event';
+import domtoimage from 'dom-to-image-more';
+import html2canvas from 'html2canvas';
 import { BattlegroundsMainWindowSelectBattleEvent } from '../../../services/mainwindow/store/events/battlegrounds/battlegrounds-main-window-select-battle-event';
 import { AppUiStoreFacadeService } from '../../../services/ui-store/app-ui-store-facade.service';
 import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-store.component';
@@ -35,6 +38,8 @@ import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-
 			</div>
 			<ng-container>
 				<div class="content" *ngIf="faceOffs?.length">
+					<!-- <div class="screenshot-container" (click)="takeScreenshot()">Screenshot</div>
+					<div class="screenshot-container" (click)="takeScreenshotDomToImage()">ScreenshotDomToImage</div> -->
 					<div class="battles-list" scrollable>
 						<bgs-battle-recap
 							*ngFor="let faceOff of faceOffs; trackBy: trackByFn"
@@ -58,6 +63,7 @@ import { AbstractSubscriptionStoreComponent } from '../../abstract-subscription-
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
+// TODO: remove store and use stateUpdater instead
 export class BgsBattlesViewComponent extends AbstractSubscriptionStoreComponent implements AfterViewInit {
 	@Input() faceOffs: readonly BgsFaceOffWithSimulation[];
 	@Input() selectedFaceOff: BgsFaceOffWithSimulation;
@@ -79,6 +85,7 @@ export class BgsBattlesViewComponent extends AbstractSubscriptionStoreComponent 
 		protected readonly cdr: ChangeDetectorRef,
 		private readonly ow: OverwolfService,
 		private readonly analytics: AnalyticsService,
+		private readonly el: ElementRef,
 	) {
 		super(store, cdr);
 	}
@@ -103,5 +110,56 @@ export class BgsBattlesViewComponent extends AbstractSubscriptionStoreComponent 
 
 	trackByFn(index: number, item: BgsFaceOffWithSimulation) {
 		return item.id;
+	}
+
+	takeScreenshot() {
+		const captureElement: HTMLElement = document.querySelector('.battles-list');
+		html2canvas(captureElement).then((canvas) => {
+			console.debug('finished capturing canvas', canvas);
+			// Copy the resulting image to the clipboard
+			canvas.toBlob((blob) => {
+				const item = new ClipboardItem({ 'image/png': blob });
+				console.debug('item', item);
+				navigator.clipboard.write([item]).then(
+					() => {
+						console.log('Screenshot copied to clipboard');
+					},
+					(err) => {
+						console.error('Could not copy screenshot to clipboard', err);
+					},
+				);
+			});
+
+			// And save it to the disk
+			const imageData = canvas.toDataURL('image/png');
+			console.debug('imageData', imageData);
+			const link = document.createElement('a');
+			link.setAttribute('download', 'screenshot.png');
+			link.setAttribute('href', imageData);
+			link.click();
+		});
+	}
+
+	// TODO: add proper header
+	takeScreenshotDomToImage() {
+		const captureElement: HTMLElement = document.querySelector('.battles-list');
+		// Change the background before taking the screenshot
+		captureElement.style.backgroundSize = 'cover';
+		// Doesn't work, but we should be able to get the value with javascript from the CSS?
+		// captureElement.style.backgroundImage = 'var(--window-background-image)';
+		captureElement.style.backgroundImage =
+			'radial-gradient(44.58% 50% at 50% 50%, rgba(48, 35, 128, 0.7) 0%, rgba(24, 24, 43, 0.7) 100% ), url("https://static.zerotoheroes.com/hearthstone/asset/firestone/images/backgrounds/decktracker_main_window.png")';
+		setTimeout(() => {
+			domtoimage
+				.toJpeg(captureElement, {
+					with: captureElement.scrollWidth,
+					height: captureElement.scrollHeight,
+					quality: 0.95,
+				})
+				.then(function (dataUrl) {
+					captureElement.style.background = 'transparent';
+					console.debug('dataUrl', dataUrl);
+				});
+		}); // Delay in milliseconds
 	}
 }
