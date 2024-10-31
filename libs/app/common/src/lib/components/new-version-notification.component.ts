30c63b9276650b6673e6067168fa2ccf9157de3d
sebastientromp <sebastien.tromp@gmail.com>
2024-10-31 18:13:53 +0100

    RELEASE: 13.32.3

--- libs/app/common/src/lib/components/new-version-notification.component.ts
+++ libs/app/common/src/lib/components/new-version-notification.component.ts
@@ -249,7 +249,7 @@ export class NewVersionNotificationComponent
 
 	private async loadVersions(): Promise<readonly AppVersion[]> {
 		const versions: readonly AppVersion[] = [
-			{ version: '13.32.2', date: '2024-10-31' },
+			{ version: '13.32.3', date: '2024-10-31' },
 			{ version: '13.31.2', date: '2024-10-29' },
 			{ version: '13.30.7', date: '2024-10-18' },
 			{ version: '13.30.3', date: '2024-10-17' },
