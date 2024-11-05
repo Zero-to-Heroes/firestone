c3731e2fcac7eb55d7bf855539a45d6319d29cc6
sebastientromp <sebastien.tromp@gmail.com>
2024-11-05 06:36:02 +0100

    BUG (lottery): prevent resizing when not premium

--- libs/legacy/feature-shell/src/lib/js/components/lottery/lottery.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/lottery/lottery.component.ts
@@ -197,11 +197,13 @@ export class LotteryWidgetComponent
 			}),
 		);
 
-		this.prefs.preferences$$.pipe(this.mapData((prefs) => prefs.lotteryScale)).subscribe((scale) => {
-			const newScale = scale / 100;
-			const element = this.el.nativeElement.querySelector('.scalable');
-			this.renderer.setStyle(element, 'transform', `scale(${newScale})`);
-		});
+		combineLatest([this.ads.hasPremiumSub$$, this.prefs.preferences$$])
+			.pipe(this.mapData(([premium, prefs]) => ({ premium, scale: prefs.lotteryScale })))
+			.subscribe(({ premium, scale }) => {
+				const newScale = premium ? scale / 100 : 1;
+				const element = this.el.nativeElement.querySelector('.scalable');
+				this.renderer.setStyle(element, 'transform', `scale(${newScale})`);
+			});
 
 		if (!(this.cdr as ViewRef).destroyed) {
 			this.cdr.detectChanges();
