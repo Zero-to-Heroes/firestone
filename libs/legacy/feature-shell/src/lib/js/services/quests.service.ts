c0c32228a2f4d8685b3b39a098df17b0b082a97b
sebastientromp <sebastien.tromp@gmail.com>
2024-12-04 10:22:01 +0100

    DEV: reduce unnecessary quest events processing

--- libs/legacy/feature-shell/src/lib/js/services/quests.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/quests.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { QuestsInfo } from '@firestone-hs/reference-data';
 import { MemoryInspectionService, SceneService } from '@firestone/memory';
 import { GameStatusService, PreferencesService } from '@firestone/shared/common/service';
+import { deepEqual } from '@firestone/shared/framework/common';
 import { ApiRunner, LocalStorageService, waitForReady } from '@firestone/shared/framework/core';
 import { BehaviorSubject, combineLatest } from 'rxjs';
-import { distinctUntilChanged, filter, map, take } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, switchMap, take } from 'rxjs/operators';
 import { ActiveQuestsUpdatedEvent } from './mainwindow/store/events/quests/active-quests-updated-event';
 import { ReferenceQuestsLoadedEvent } from './mainwindow/store/events/quests/reference-quests-loaded-event';
 import { AppUiStoreFacadeService } from './ui-store/app-ui-store-facade.service';
@@ -59,13 +60,17 @@ export class QuestsService {
 						map(([locale, load]) => ({ locale })),
 					)
 					.subscribe(({ locale }) => this.loadReferenceQuests(locale));
-				this.scene.currentScene$$.subscribe(async (scene) => {
+				this.scene.currentScene$$
 					// Assumption for now is that quests can only be completed during gameplay
 					// Also, quests are not updated live while playing
 					// TODO: doesn't account for rerolls
-					const activeQuests = await this.memory.getActiveQuests();
-					this.store.send(new ActiveQuestsUpdatedEvent(activeQuests));
-				});
+					.pipe(
+						switchMap((scene) => this.memory.getActiveQuests()),
+						distinctUntilChanged((a, b) => deepEqual(a, b)),
+					)
+					.subscribe(async (activeQuests) => {
+						this.store.send(new ActiveQuestsUpdatedEvent(activeQuests));
+					});
 			});
 	}
 
