2c4b28d850c576263005c76acb0ed0b9a75f2eb6
sebastientromp <sebastien.tromp@gmail.com>
2024-12-15 21:13:56 +0100

    BUG (tracker): fix discover options sometimes being counted as part of starting deck

--- libs/game-state/src/lib/models/deck-state.ts
+++ libs/game-state/src/lib/models/deck-state.ts
@@ -266,7 +266,8 @@ export class DeckState {
 	public getAllCardsFromStarterDeck(): readonly DeckCard[] {
 		return [...this.deckList, ...this.hand, ...this.deck, ...this.board, ...this.otherZone]
 			.filter((c) => !!c)
-			.filter((c) => !c.creatorCardId?.length && !c.stolenFromOpponent);
+			.filter((c) => !c.creatorCardId?.length && !c.stolenFromOpponent)
+			.filter((c) => !c.temporaryCard);
 	}
 
 	// TODO: Probably not the place for these methods
