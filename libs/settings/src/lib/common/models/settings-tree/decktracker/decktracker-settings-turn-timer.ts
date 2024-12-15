1176a3a31c68ef0dab47ffd8a1fde39287b04608
sebastientromp <sebastien.tromp@gmail.com>
2024-12-15 10:23:36 +0100

    BEHAVIOR (tracker): allow turn timer's background to turn fully transparent

--- libs/settings/src/lib/common/models/settings-tree/decktracker/decktracker-settings-turn-timer.ts
+++ libs/settings/src/lib/common/models/settings-tree/decktracker/decktracker-settings-turn-timer.ts
@@ -57,7 +57,7 @@ export const decktrackerTurnTimerSettings = (context: SettingContext): SettingNo
 						tooltip: null,
 						disabledIf: (prefs: Preferences) => !prefs.showTurnTimer,
 						sliderConfig: {
-							min: 10,
+							min: 0,
 							max: 100,
 							snapSensitivity: 5,
 						},
