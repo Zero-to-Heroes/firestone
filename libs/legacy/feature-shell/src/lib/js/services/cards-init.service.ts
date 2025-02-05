36dca8271accf8bf309f02a6c6172ecbdf8002db
sebastientromp <sebastien.tromp@gmail.com>
2025-02-05 09:56:00 +0100

    DEV: use local cache for cards to bootstrap the app faster

--- libs/legacy/feature-shell/src/lib/js/services/cards-init.service.ts
+++ libs/legacy/feature-shell/src/lib/js/services/cards-init.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AllCardsService } from '@firestone-hs/reference-data';
+import { AllCardsService, ReferenceCard } from '@firestone-hs/reference-data';
 import { PreferencesService } from '@firestone/shared/common/service';
+import { DiskCacheService } from '@firestone/shared/framework/core';
 import { distinctUntilChanged, map, skip } from 'rxjs';
 import { CARDS_VERSION } from './hs-utils';
 
@@ -8,7 +9,11 @@ import { CARDS_VERSION } from './hs-utils';
 export class CardsInitService {
 	private inInit = false;
 
-	constructor(private readonly cards: AllCardsService, private readonly prefs: PreferencesService) {}
+	constructor(
+		private readonly cards: AllCardsService,
+		private readonly prefs: PreferencesService,
+		private readonly diskCache: DiskCacheService,
+	) {}
 
 	public async init() {
 		if (this.inInit) {
@@ -39,10 +44,30 @@ export class CardsInitService {
 	private async initCards(locale: string) {
 		const fileName = this.getFileName(locale);
 		console.log('[cards-init] initializing cards db with', fileName);
-		await this.cards.initializeCardsDb(CARDS_VERSION, fileName);
+		const localCards: readonly ReferenceCard[] | null = await this.retrieveLocalCards(fileName);
+		if (!!localCards?.length) {
+			this.cards.initializeCardsDbFromCards(localCards);
+			console.log('[cards-init] initialized cards with local cache', localCards?.length);
+		}
+		// Make this non-blocking, so the app can already start with the cached info, while we get the
+		// updated cards in the background
+		setTimeout(async () => {
+			await this.cards.initializeCardsDb(CARDS_VERSION, fileName);
+			console.log('[cards-init] loaded cards from remote', this.cards.getCards().length);
+			await this.saveCardsLocally(fileName, this.cards.getCards());
+		});
 	}
 
 	private getFileName(locale: string) {
 		return `cards_${locale}.gz.json`;
+	}
+
+	private async saveCardsLocally(fileName: string, cards: readonly ReferenceCard[]) {
+		await this.diskCache.storeItem(fileName, cards);
+	}
+
+	private async retrieveLocalCards(fileName: string): Promise<readonly ReferenceCard[] | null> {
+		const localCards = await this.diskCache.getItem<readonly ReferenceCard[]>(fileName);
+		return localCards;
 	}
 }
