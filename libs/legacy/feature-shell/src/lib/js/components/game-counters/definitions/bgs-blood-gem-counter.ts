5e285dd3afd34383fd100ed3e55b144a96df15c1
sebastientromp <sebastien.tromp@gmail.com>
2025-03-09 14:05:14 +0100

    UI (bgs): add + sign to the blood gem counter

--- libs/legacy/feature-shell/src/lib/js/components/game-counters/definitions/bgs-blood-gem-counter.ts
+++ libs/legacy/feature-shell/src/lib/js/components/game-counters/definitions/bgs-blood-gem-counter.ts
@@ -50,7 +50,7 @@ export class BgsBloodGemCounterDefinition
 	public emit(input: { attack: number; health: number }): NonFunctionProperties<BgsBloodGemCounterDefinition> {
 		return {
 			type: 'bgsBloodGem',
-			value: `${input.attack}/${input.health}`,
+			value: `+${input.attack}/+${input.health}`,
 			image: `https://static.zerotoheroes.com/hearthstone/cardart/256x/${CardIds.BloodGem}.jpg`,
 			cssClass: 'blood-gem-counter',
 			tooltip: this.i18n.translateString(`counters.bgs-blood-gem.${this.side}`, {
