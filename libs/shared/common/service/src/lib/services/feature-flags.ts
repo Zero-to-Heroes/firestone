e355a96a3810e6af8abae243fbb0e8bf2d47fcbe
sebastientromp <sebastien.tromp@gmail.com>
2025-03-10 08:29:28 +0100

    RELEASE: 14.3.6

--- libs/shared/common/service/src/lib/services/feature-flags.ts
+++ libs/shared/common/service/src/lib/services/feature-flags.ts
@@ -3,7 +3,8 @@ import { OverwolfService } from '@firestone/shared/framework/core';
 export const ENABLE_BGS_COMPS = true;
 export const BG_USE_ANOMALIES = true;
 export const ENABLE_TEBEX = async (ow: OverwolfService) => {
-	return ow.isBetaChannel();
+	return false;
+	// return ow.isBetaChannel();
 };
 
 // Shelved for now
