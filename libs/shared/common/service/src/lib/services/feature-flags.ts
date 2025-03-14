a180a90e9966569641d2611ec2bb7e655800af57
sebastientromp <sebastien.tromp@gmail.com>
2025-03-14 08:55:55 +0100

    FEATURE (app): enable Tebex by default

--- libs/shared/common/service/src/lib/services/feature-flags.ts
+++ libs/shared/common/service/src/lib/services/feature-flags.ts
@@ -3,8 +3,8 @@ import { OverwolfService } from '@firestone/shared/framework/core';
 export const ENABLE_BGS_COMPS = true;
 export const BG_USE_ANOMALIES = true;
 export const ENABLE_TEBEX = async (ow: OverwolfService) => {
-	// return false;
-	return ow.isBetaChannel();
+	return true;
+	// return ow.isBetaChannel();
 };
 
 // Shelved for now
