a9de98b1c2647687eda5c2ffa4b512f2d842ed7c
sebastientromp <sebastien.tromp@gmail.com>
2025-04-01 10:21:19 +0200

    BUG (tracker): fix fatigue counter not showing up for player

--- libs/game-state/src/lib/counters/impl/fatigue.ts
+++ libs/game-state/src/lib/counters/impl/fatigue.ts
@@ -6,16 +6,20 @@ import { GameState } from '../../models/game-state';
 import { CounterDefinitionV2 } from '../_counter-definition-v2';
 import { CounterType } from '../_exports';
 
+const cards: readonly CardIds[] = [CardIds.CurseOfAgony_AgonyToken];
+
 export class FatigueCounterDefinitionV2 extends CounterDefinitionV2<number> {
 	public override id: CounterType = 'fatigue';
 	public override image = '';
 	public override imageIcon = `https://static.zerotoheroes.com/hearthstone/cardart/256x/FATIGUE.jpg`;
 	public override type: 'hearthstone' | 'battlegrounds' = 'hearthstone';
-	public override cards: readonly CardIds[] = [CardIds.CurseOfAgony_AgonyToken];
+	public override cards: readonly CardIds[] = [];
 
 	readonly player = {
 		pref: 'playerFatigueCounter' as const,
-		display: (state: GameState): boolean => true,
+		display: (state: GameState): boolean => {
+			return state.playerDeck.hasRelevantCard(cards) || this.player.value(state) > 0;
+		},
 		value: (state: GameState) => {
 			return state.playerDeck.fatigue ?? 0;
 		},
