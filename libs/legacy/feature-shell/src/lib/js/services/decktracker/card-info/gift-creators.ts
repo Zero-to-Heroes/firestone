892c130f7ab8e9cf9342f312989c73fb8c9cf8e5
sebastientromp <sebastien.tromp@gmail.com>
2025-04-14 13:14:53 +0200

    CONTENT (tracker): add oracle for Treacherous Tormentor

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/card-info/gift-creators.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/card-info/gift-creators.ts
@@ -1004,4 +1004,5 @@ export const giftCreators = [
 	CardIds.KragwaTheFrog_TRL_345,
 	CardIds.KragwaTheFrog_CORE_TRL_345,
 	CardIds.DarkGiftToken_EDR_102t,
+	CardIds.TreacherousTormentor_EDR_102,
 ];
