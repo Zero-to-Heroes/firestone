a75cc182839f3d4ded8e834979d5ad4cead9c155
sebastientromp <sebastien.tromp@gmail.com>
2025-04-18 08:57:55 +0200

    DEV: small simplfication of the processing-queue.service

--- libs/shared/framework/core/src/lib/services/processing-queue.service.ts
+++ libs/shared/framework/core/src/lib/services/processing-queue.service.ts
@@ -1,81 +1,97 @@
+/** @deprecated */
 export class ProcessingQueue<T> {
-	private eventQueue: readonly T[] = [];
-	private pendingQueue: readonly T[] = [];
+	private eventQueue: T[] = [];
+	private pendingQueue: T[] = [];
 	private isProcessing = false;
-	private interval: NodeJS.Timer | undefined = undefined;
+	private processingTimer: NodeJS.Timeout | null = null;
 
 	constructor(
 		private readonly processingFunction: (eventQueue: readonly T[]) => Promise<readonly T[]>,
-		private readonly processFrequency: number,
+		private readonly processFrequency: number, // Frequency in milliseconds
 		private readonly queueName?: string,
 	) {}
 
 	public clear() {
-		this.isProcessing = true;
 		this.eventQueue = [];
 		this.pendingQueue = [];
-		if (this.interval) {
-			clearInterval(this.interval);
-			this.interval = undefined;
-		}
 		this.isProcessing = false;
+
+		// Clear the timer if it exists
+		if (this.processingTimer) {
+			clearTimeout(this.processingTimer);
+			this.processingTimer = null;
+		}
 	}
 
 	public eventsPendingCount(): number {
 		return this.eventQueue.length + this.pendingQueue.length;
 	}
 
-	public async enqueue(event: T) {
-		// What happens if this happens while we're waiting for the processingFunction
-		// to complete? As it's async
-		if (this.isProcessing) {
-			// Don't modify the queue while processing is ongoing
-			this.pendingQueue = [...this.pendingQueue, event];
-		} else {
-			this.eventQueue = [...this.eventQueue, event];
-		}
-		this.processQueue();
+	public enqueue(event: T) {
+		this.pendingQueue.push(event);
+		this.scheduleProcessing();
 	}
 
-	public async enqueueAll(events: T[]) {
-		// What happens if this happens while we're waiting for the processingFunction
-		// to complete? As it's async
-		if (this.isProcessing) {
-			// Don't modify the queue while processing is ongoing
-			this.pendingQueue = [...this.pendingQueue, ...events];
-		} else {
-			this.eventQueue = [...this.eventQueue, ...events];
+	public enqueueAll(events: T[]) {
+		this.pendingQueue.push(...events);
+		this.scheduleProcessing();
+	}
+
+	private scheduleProcessing() {
+		// If a timer is already set, do nothing
+		if (this.processingTimer) {
+			return;
 		}
 
-		this.processQueue();
+		// Schedule the processing to occur after the specified frequency
+		this.processingTimer = setTimeout(async () => {
+			await this.processQueue();
+		}, this.processFrequency);
 	}
 
 	private async processQueue() {
+		// Prevent concurrent processing
 		if (this.isProcessing) {
 			return;
 		}
+
 		this.isProcessing = true;
-		if (this.eventQueue.length === 0) {
-			if (this.interval) {
-				clearInterval(this.interval);
-				this.interval = undefined;
-			}
-			this.isProcessing = false;
-			return;
-		}
-		if (!this.interval) {
-			this.interval = setInterval(() => this.processQueue(), this.processFrequency);
-		}
-		let hasEventBeenProcessed = true;
-		// We processed some events, but not all
-		// If no event has been processed, this could mean the processing function is waiting for something
-		// so we just leave the processing loop
-		while (this.eventQueue.length !== 0 && hasEventBeenProcessed) {
-			const queueAfterProcess = await this.processingFunction([...this.eventQueue]);
-			hasEventBeenProcessed = queueAfterProcess.length !== this.eventQueue.length;
-			this.eventQueue = [...queueAfterProcess, ...this.pendingQueue];
+
+		try {
+			// Move pending events to the event queue
+			this.eventQueue.push(...this.pendingQueue);
 			this.pendingQueue = [];
+
+			while (this.eventQueue.length > 0) {
+				console.debug('[ProcessingQueue] Processing queue', this.eventQueue.length);
+
+				// Process the current queue
+				const queueAfterProcess = await this.processingFunction(this.eventQueue);
+
+				// If no events were processed, break the loop to avoid infinite processing
+				if (queueAfterProcess.length === this.eventQueue.length) {
+					console.warn('[ProcessingQueue] No events processed, exiting processing loop');
+					break;
+				}
+
+				// Update the event queue with the remaining events
+				this.eventQueue = queueAfterProcess as T[];
+			}
+		} catch (error) {
+			console.error('[ProcessingQueue] Error during queue processing', error);
+		} finally {
+			this.isProcessing = false;
+
+			// Clear the timer after processing
+			if (this.processingTimer) {
+				clearTimeout(this.processingTimer);
+				this.processingTimer = null;
+			}
+
+			// If there are new pending events, schedule the next batch
+			if (this.pendingQueue.length > 0) {
+				this.scheduleProcessing();
+			}
 		}
-		this.isProcessing = false;
 	}
 }
