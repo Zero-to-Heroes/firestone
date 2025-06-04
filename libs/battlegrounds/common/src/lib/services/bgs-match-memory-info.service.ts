a7a34c969b63d9d03512990a74402e27381703dc
sebastientromp <sebastien.tromp@gmail.com>
2025-06-04 10:13:29 +0200

    DEV: add try/catch to bgs-match-memory-info.service

--- libs/battlegrounds/common/src/lib/services/bgs-match-memory-info.service.ts
+++ libs/battlegrounds/common/src/lib/services/bgs-match-memory-info.service.ts
@@ -42,62 +42,68 @@ export class BgsMatchMemoryInfoService {
 
 		let inProcess = false;
 		this.sub = interval(INTERVAL).subscribe(async () => {
-			if (inProcess) {
-				return;
-			}
+			try {
+				if (inProcess) {
+					// console.debug('[bgs-match-memory-info] already in process, skipping this reading');
+					return;
+				}
 
-			const gameState = this.gameState.gameState$$.getValue();
-			if (
-				!gameState?.bgState?.currentGame ||
-				!gameState.gameStarted ||
-				gameState.gameEnded ||
-				!isBattlegrounds(gameState.metadata?.gameType)
-			) {
-				// console.debug(
-				// 	'[bgs-match-memory-info] no BG game in progress, stopping memory reading',
-				// 	gameState?.bgState?.currentGame,
-				// 	gameState?.gameStarted,
-				// 	gameState?.gameEnded,
-				// 	isBattlegrounds(gameState?.metadata?.gameType),
-				// );
-				return;
-			}
+				const gameState = this.gameState.gameState$$.getValue();
+				if (
+					!gameState?.bgState?.currentGame ||
+					!gameState.gameStarted ||
+					gameState.gameEnded ||
+					!isBattlegrounds(gameState.metadata?.gameType)
+				) {
+					// console.debug(
+					// 	'[bgs-match-memory-info] no BG game in progress, not doing memory reading',
+					// 	!!gameState?.bgState?.currentGame,
+					// 	gameState?.gameStarted,
+					// 	gameState?.gameEnded,
+					// 	isBattlegrounds(gameState?.metadata?.gameType),
+					// );
+					return;
+				}
 
-			inProcess = true;
-			// console.debug('[bgs-match-memory-info] reading memory info');
-			const memoryInfo = await this.memory.getBattlegroundsMatchWithPlayers(1);
-			// So that we send it right away to get the most important info
-			this.battlegroundsMemoryInfo$$.next(memoryInfo);
-			// console.debug('[bgs-match-memory-info] sent memory info basic info', memoryInfo);
+				inProcess = true;
+				// console.debug('[bgs-match-memory-info] reading memory info');
+				const memoryInfo = await this.memory.getBattlegroundsMatchWithPlayers(1);
+				// So that we send it right away to get the most important info
+				this.battlegroundsMemoryInfo$$.next(memoryInfo);
+				// console.debug('[bgs-match-memory-info] sent memory info basic info', memoryInfo?.Game?.AvailableRaces);
 
-			const prefs = await this.prefs.getPreferences();
-			if (!prefs.bgsShowMmrInLeaderboardOverlay && !prefs.bgsShowMmrInOpponentRecap) {
+				const prefs = await this.prefs.getPreferences();
+				if (!prefs.bgsShowMmrInLeaderboardOverlay && !prefs.bgsShowMmrInOpponentRecap) {
+					inProcess = false;
+					return;
+				}
+
+				// console.debug('[bgs-match-memory-info] reading player MMR info');
+				const playersMmr = await this.matchPlayers.playersMatchMmr$$.getValueWithInit();
+				const result: BattlegroundsInfo = {
+					...memoryInfo,
+					Game: {
+						...memoryInfo!.Game,
+						Players: memoryInfo!.Game.Players.map((player) => {
+							const playerMmr = playersMmr?.find((mmr) => mmr.playerId === player.Id);
+							const playerResult = {
+								...player,
+								// We set the damage to null because? Most likely this is to avoid info
+								// leaks
+								Damage: gameState.bgState.currentGame?.phase === 'recruit' ? player.Damage : null,
+								Mmr: playerMmr?.mmr,
+							};
+							return playerResult;
+						}),
+					},
+				} as BattlegroundsInfo;
+				// console.debug('[bgs-match-memory-info] memory info read', result);
+				this.battlegroundsMemoryInfo$$.next(result);
 				inProcess = false;
-				return;
+			} catch (e) {
+				console.error('[bgs-match-memory-info] error while reading memory info', e);
+				inProcess = false;
 			}
-
-			// console.debug('[bgs-match-memory-info] reading player MMR info');
-			const playersMmr = await this.matchPlayers.playersMatchMmr$$.getValueWithInit();
-			const result: BattlegroundsInfo = {
-				...memoryInfo,
-				Game: {
-					...memoryInfo!.Game,
-					Players: memoryInfo!.Game.Players.map((player) => {
-						const playerMmr = playersMmr?.find((mmr) => mmr.playerId === player.Id);
-						const playerResult = {
-							...player,
-							// We set the damage to null because? Most likely this is to avoid info
-							// leaks
-							Damage: gameState.bgState.currentGame?.phase === 'recruit' ? player.Damage : null,
-							Mmr: playerMmr?.mmr,
-						};
-						return playerResult;
-					}),
-				},
-			} as BattlegroundsInfo;
-			// console.debug('[bgs-match-memory-info] memory info read', result);
-			this.battlegroundsMemoryInfo$$.next(result);
-			inProcess = false;
 		});
 	}
 
