75e88d1c7a11a0e740ae43021d9c96dfa946372f
sebastientromp <sebastien.tromp@gmail.com>
2025-08-18 18:21:54 +0200

    tmp comps for web

--- libs/shared/web-shell/src/lib/components/battlegrounds/compositions/battlegrounds-compositions.component.ts
+++ libs/shared/web-shell/src/lib/components/battlegrounds/compositions/battlegrounds-compositions.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { SharedFrameworkCoreModule } from '@firestone/shared/framework/core';
 import { InlineSVGModule } from 'ng-inline-svg-2';
+import { WebBattlegroundsFiltersComponent } from '../filters/_web-battlegrounds-filters.component';
+import { WebBattlegroundsRankFilterDropdownComponent } from '../filters/web-battlegrounds-rank-filter-dropdown.component';
+import { WebBattlegroundsTimeFilterDropdownComponent } from '../filters/web-battlegrounds-time-filter-dropdown.component';
 
 @Component({
 	standalone: true,
 	selector: 'battlegrounds-compositions',
 	templateUrl: './battlegrounds-compositions.component.html',
 	styleUrls: ['./battlegrounds-compositions.component.scss'],
-	imports: [InlineSVGModule, SharedFrameworkCoreModule],
+	imports: [
+		InlineSVGModule,
+		SharedFrameworkCoreModule,
+
+		WebBattlegroundsFiltersComponent,
+		WebBattlegroundsRankFilterDropdownComponent,
+		WebBattlegroundsTimeFilterDropdownComponent,
+	],
 })
 export class BattlegroundsCompositionsComponent {}
