4d68c883053778a23d98701e1d3b7cbf8f579287
sebastientromp <sebastien.tromp@gmail.com>
2025-08-21 13:50:06 +0200

    integrate ow electron to build

--- apps/electron-app/src/main.ts
+++ apps/electron-app/src/main.ts
@@ -1,8 +1,7 @@
-import SquirrelEvents from './app/events/squirrel.events';
-import ElectronEvents from './app/events/electron.events';
-import UpdateEvents from './app/events/update.events';
 import { app, BrowserWindow } from 'electron';
 import App from './app/app';
+import ElectronEvents from './app/events/electron.events';
+import SquirrelEvents from './app/events/squirrel.events';
 
 export default class Main {
 	static initialize() {
