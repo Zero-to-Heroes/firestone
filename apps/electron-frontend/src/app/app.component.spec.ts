c0c81aed15f5e9245ee23991ecbc56d2beed3727
sebastientromp <sebastien.tromp@gmail.com>
2025-08-21 11:05:57 +0200

    hello world electron app

---
+++ apps/electron-frontend/src/app/app.component.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+describe('AppComponent', () => {
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [AppComponent, NxWelcomeComponent],
+		}).compileComponents();
+	});
+
+	it('should render title', () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		fixture.detectChanges();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector('h1')?.textContent).toContain('Welcome electron-frontend');
+	});
+
+	it(`should have as title 'electron-frontend'`, () => {
+		const fixture = TestBed.createComponent(AppComponent);
+		const app = fixture.componentInstance;
+		expect(app.title).toEqual('electron-frontend');
+	});
+});
