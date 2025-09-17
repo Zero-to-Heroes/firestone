4356a1c3553e0fa03175cde00a4f0f0ed906369f
sebastientromp <sebastien.tromp@gmail.com>
2025-09-17 17:28:16 +0200

    CONTENT (tracker): add oracle (Ritual of Life)

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/game-state/card-reveal.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/game-state/card-reveal.ts
@@ -12,39 +12,40 @@ export const revealCard = (deck: DeckState, card: DeckCard) => {
 
 	console.debug('[card-reveal] creatorCardId', creatorCardId);
 	switch (creatorCardId) {
-		case CardIds.Triangulate_GDB_451:
-		case CardIds.MarinTheManager_TolinsGobletToken_VAC_702t2:
 		// case CardIds.BobTheBartender_FindATripleToken_BG31_BOBt4:
-		case CardIds.BobTheBartender_BG31_BOB:
-		case CardIds.EyeOfOrsis:
-		case CardIds.ThistleTea:
-		case CardIds.PristineCompass:
-		case CardIds.LuckySpade:
-		case CardIds.GrandEmpressShekzara:
-		case CardIds.ServiceBell:
-		case CardIds.ServiceBell_CORE_REV_948:
-		case CardIds.ZarogsCrown:
-		case CardIds.MarinTheManager_ZarogsCrownToken_VAC_702t:
-		case CardIds.MimicPod:
-		case CardIds.DollmasterDorian:
 		case CardIds.ArchmageArugal:
-		case CardIds.CheerfulSpirit:
-		case CardIds.ThistleDagger:
-		case CardIds.DoorOfShadows_DoorOfShadowsToken:
-		case CardIds.BronzeSignetTavernBrawl:
+		case CardIds.BobTheBartender_BG31_BOB:
 		case CardIds.BronzeSignet_BronzeSignetTavernBrawlEnchantment:
+		case CardIds.BronzeSignetTavernBrawl:
+		case CardIds.CheerfulSpirit:
+		case CardIds.DollmasterDorian:
+		case CardIds.DoorOfShadows_DoorOfShadowsToken:
+		case CardIds.DryscaleDeputy_WW_383:
+		case CardIds.EyeOfOrsis:
 		case CardIds.FleshBehemoth_RLK_830:
 		case CardIds.FleshBehemoth_RLK_Prologue_RLK_830:
+		case CardIds.GrandEmpressShekzara:
+		case CardIds.LuckySpade:
+		case CardIds.MarinTheManager_TolinsGobletToken_VAC_702t2:
+		case CardIds.MarinTheManager_ZarogsCrownToken_VAC_702t:
 		case CardIds.MeltedMaker:
-		case CardIds.VoidScripture_YOG_507:
-		case CardIds.DryscaleDeputy_WW_383:
-		case CardIds.ThistleTeaSet_TOY_514:
-		case CardIds.SketchArtist_TOY_916:
+		case CardIds.MimicPod:
+		case CardIds.PristineCompass:
 		case CardIds.RangariScout_GDB_841:
-		case CardIds.ShadowflameStalker_FIR_924:
-		case CardIds.SearingReflection_FIR_941:
 		case CardIds.RitualOfLife_DINO_426:
+		case CardIds.RitualOfLife_LastingLifeEnchantment_DINO_426e:
+		case CardIds.SearingReflection_FIR_941:
+		case CardIds.ServiceBell_CORE_REV_948:
+		case CardIds.ServiceBell:
+		case CardIds.ShadowflameStalker_FIR_924:
+		case CardIds.SketchArtist_TOY_916:
 		case CardIds.SoulSearching_WORK_070:
+		case CardIds.ThistleDagger:
+		case CardIds.ThistleTea:
+		case CardIds.ThistleTeaSet_TOY_514:
+		case CardIds.Triangulate_GDB_451:
+		case CardIds.VoidScripture_YOG_507:
+		case CardIds.ZarogsCrown:
 			return updateCardsInDeckAsCopies(deck, card, creatorEntityId, creatorCardId);
 		default:
 			return deck;
