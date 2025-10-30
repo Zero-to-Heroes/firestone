894948de249fcb3c14995e7bf0146760f18ceecd
sebastientromp <sebastien.tromp@gmail.com>
2025-10-30 14:05:14 +0100

    RELEASE: 16.1.6

--- libs/app/common/src/lib/components/new-version-notification.component.ts
+++ libs/app/common/src/lib/components/new-version-notification.component.ts
@@ -19,7 +19,7 @@ import { AppVersion } from '../model/app-version';
 import { isVersionBefore } from '../services/notifications-utils';
 
 const versions: readonly AppVersion[] = [
-	{ version: '16.1.5', date: '2025-10-30' },
+	{ version: '16.1.6', date: '2025-10-30' },
 	{ version: '16.0.6', date: '2025-10-14' },
 	{ version: '15.26.22', date: '2025-10-07' },
 	{ version: '15.26.9', date: '2025-09-29' },
