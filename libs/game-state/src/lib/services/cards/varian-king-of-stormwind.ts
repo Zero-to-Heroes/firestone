d08682037ae136972316795c11ca73edbd68c88d
sebastientromp <sebastien.tromp@gmail.com>
2025-11-03 21:04:22 +0100

    CONTENT (tracker): update multi oracles (Countess Ashmore, Varian King of Stormwind)

---
+++ libs/game-state/src/lib/services/cards/varian-king-of-stormwind.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { CardIds, GameTag } from '@firestone-hs/reference-data';
+import { CardsFacadeService } from '@firestone/shared/framework/core';
+import { DeckCard, GuessedInfo } from '../../models/deck-card';
+import { DeckState } from '../../models/deck-state';
+import { Metadata } from '../../models/metadata';
+import { GeneratingCard } from './_card.type';
+
+export const VarianKingOfStormwind: GeneratingCard = {
+	cardIds: [CardIds.VarianKingOfStormwind],
+	hasSequenceInfo: true,
+	publicTutor: true,
+	guessInfo: (
+		card: DeckCard,
+		deckState: DeckState,
+		opponentDeckState: DeckState,
+		allCards: CardsFacadeService,
+		creatorEntityId: number,
+		options?: {
+			positionInHand?: number;
+			tags?: readonly { Name: GameTag; Value: number }[];
+			metadata?: Metadata;
+			validArenaPool?: readonly string[];
+		},
+	): GuessedInfo | null => {
+		if (card.createdIndex === 0) {
+			return null;
+		} else if (card.createdIndex === 1) {
+			return null;
+		} else if (card.createdIndex === 2) {
+			return null;
+		}
+		return null;
+	},
+};
