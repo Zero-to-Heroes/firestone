966f25a875389191bd0b8d1834573c7a8b22354f
sebastientromp <sebastien.tromp@gmail.com>
2025-11-03 21:02:24 +0100

    CONTENT (tracker): update multi oracles (Hybridization, Ringmaster Whatley, The Curator, Al'Akir the Winds of Time)

---
+++ libs/game-state/src/lib/services/cards/alakir-the-winds-of-time.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { CardIds, CardType, GameTag } from '@firestone-hs/reference-data';
+import { CardsFacadeService } from '@firestone/shared/framework/core';
+import { DeckCard, GuessedInfo } from '../../models/deck-card';
+import { DeckState } from '../../models/deck-state';
+import { Metadata } from '../../models/metadata';
+import { GeneratingCard } from './_card.type';
+
+export const AlakirTheWindsOfTime: GeneratingCard = {
+	cardIds: [CardIds.AlakirTheWindsOfTime_WON_092h],
+	hasSequenceInfo: true,
+	publicTutor: true,
+	guessInfo: (
+		card: DeckCard,
+		deckState: DeckState,
+		opponentDeckState: DeckState,
+		allCards: CardsFacadeService,
+		creatorEntityId: number,
+		options?: {
+			positionInHand?: number;
+			tags?: readonly { Name: GameTag; Value: number }[];
+			metadata?: Metadata;
+			validArenaPool?: readonly string[];
+		},
+	): GuessedInfo | null => {
+		if (card.createdIndex === 0) {
+			return {
+				cardType: CardType.MINION,
+			};
+		} else if (card.createdIndex === 1) {
+			return {
+				cardType: CardType.MINION,
+			};
+		} else if (card.createdIndex === 2) {
+			return {
+				cardType: CardType.MINION,
+			};
+		} else if (card.createdIndex === 2) {
+			return {
+				cardType: CardType.MINION,
+			};
+		}
+		return null;
+	},
+};
