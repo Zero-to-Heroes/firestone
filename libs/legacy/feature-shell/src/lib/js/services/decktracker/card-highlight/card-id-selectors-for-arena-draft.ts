98e0f92f81925fc686d413f91d5bc32f1288cfaa
sebastientromp <sebastien.tromp@gmail.com>
2025-11-06 10:56:30 +0100

    CONTENT (arena): add highlight (RedCard -> Perennial Serpent)

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/card-highlight/card-id-selectors-for-arena-draft.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/card-highlight/card-id-selectors-for-arena-draft.ts
@@ -1,9 +1,8 @@
+import { CardIds } from '@firestone-hs/reference-data';
 import { DeckCard } from '@firestone/game-state';
-import { TempCardIds } from '@firestone/shared/common/service';
 import { CardsFacadeService } from '@firestone/shared/framework/core';
 import { Selector } from './cards-highlight-common.service';
-import { and, drawCard, inDeck } from './selectors';
-import { CardIds } from '@firestone-hs/reference-data';
+import { and, cardIs, drawCard, inDeck } from './selectors';
 
 export const cardIdSelectorForArenaDraft = (
 	cardId: string,
@@ -15,6 +14,8 @@ export const cardIdSelectorForArenaDraft = (
 	switch (cardId) {
 		case CardIds.Chronochiller_TIME_617:
 			return and(inDeck, drawCard);
+		case CardIds.RedCard_TOY_644:
+			return cardIs(CardIds.PerennialSerpent_TIME_022);
 	}
 
 	return null;
