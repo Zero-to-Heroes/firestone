588277b6135a23821bfe8ed96f15dcfe153d7920
sebastientromp <sebastien.tromp@gmail.com>
2025-11-24 21:14:56 +0100

    CONTENT (tracker): update dynamic pool (Natural Talent)

--- libs/game-state/src/lib/services/cards/_mapping.ts
+++ libs/game-state/src/lib/services/cards/_mapping.ts
@@ -1,15 +1,16 @@
 import * as Cards from './_barrel';
-import { Card, GeneratingCard, SpecialCaseParserCard } from './_card.type';
+import { Card, GeneratingCard, SpecialCaseParserCard, StaticGeneratingCard } from './_card.type';
 
-const cards: (GeneratingCard | SpecialCaseParserCard)[] = Object.values(Cards).filter(
+const cards: (GeneratingCard | SpecialCaseParserCard | StaticGeneratingCard)[] = Object.values(Cards).filter(
 	(c) => c && typeof c === 'object' && 'cardIds' in c,
 );
 
-const internalCache: { [cardId: string]: GeneratingCard | SpecialCaseParserCard } = {};
+const internalCache: { [cardId: string]: GeneratingCard | SpecialCaseParserCard | StaticGeneratingCard } = {};
 for (const card of cards) {
 	const cardIds = card.cardIds ?? [];
 	for (const cardId of cardIds) {
 		internalCache[cardId] = card;
 	}
 }
-export const cardsInfoCache: { [cardId: string]: GeneratingCard | SpecialCaseParserCard } = internalCache;
+export const cardsInfoCache: { [cardId: string]: GeneratingCard | SpecialCaseParserCard | StaticGeneratingCard } =
+	internalCache;
--- libs/game-state/src/lib/services/cards/natural-talent.ts
+++ libs/game-state/src/lib/services/cards/natural-talent.ts
@@ -1,22 +1,28 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { CardIds, CardType, GameTag, hasCorrectTribe, Race } from '@firestone-hs/reference-data';
+import { AllCardsService, CardIds, CardType, GameTag, hasCorrectTribe, Race } from '@firestone-hs/reference-data';
 import { CardsFacadeService } from '@firestone/shared/framework/core';
 import { DeckCard, GuessedInfo } from '../../models/deck-card';
 import { DeckState } from '../../models/deck-state';
 import { Metadata } from '../../models/metadata';
 import { hasCorrectType } from '../../related-cards/dynamic-pools';
-import { GeneratingCard } from './_card.type';
+import { GeneratingCard, StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 import { filterCards } from './utils';
 
-export const NaturalTalent: GeneratingCard = {
+export const NaturalTalent: GeneratingCard & StaticGeneratingCard = {
 	cardIds: [CardIds.NaturalTalent_VAC_329],
 	hasSequenceInfo: true,
 	publicCreator: true,
+	dynamicPool: (input: StaticGeneratingCardInput) => {
+		return [
+			...filterCards(input.cardId, input.allCards, (c) => hasCorrectTribe(c, Race.NAGA), input.inputOptions),
+			...filterCards(input.cardId, input.allCards, (c) => hasCorrectType(c, CardType.SPELL), input.inputOptions),
+		];
+	},
 	guessInfo: (
 		card: DeckCard,
 		deckState: DeckState,
 		opponentDeckState: DeckState,
-		allCards: CardsFacadeService,
+		allCards: AllCardsService,
 		creatorEntityId: number,
 		options?: {
 			positionInHand?: number;
