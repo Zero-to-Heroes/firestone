0bec434e8b100e305eec63c7b04acc0e2708b0e1
sebastientromp <sebastien.tromp@gmail.com>
2025-12-14 20:42:51 +0100

    CONTENT (tracker): allow dupes in Wakener of Souls dynamic pool

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/card-highlight/global/wakener-of-souls.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/card-highlight/global/wakener-of-souls.ts
@@ -7,14 +7,10 @@ export const WakenerOfSouls: GlobalHighlightCard = {
 	cardIds: [CardIds.WakenerOfSouls_GDB_468],
 	getRelatedCards: (entityId: number, side: HighlightSide, gameState: GameState, allCards: CardsFacadeService) => {
 		const deckState = side === 'player' ? gameState.playerDeck : gameState.opponentDeck;
-		return (
-			deckState.minionsDeadThisMatch
-				.map((e) => getProcessedCard(e.cardId, e.entityId, deckState, allCards))
-				.filter((c) => hasMechanic(c, GameTag.DEATHRATTLE))
-				.map((e) => e.id)
-				.filter((id) => id != CardIds.WakenerOfSouls_GDB_468)
-				// distinct
-				.filter((value, index, self) => self.indexOf(value) === index)
-		);
+		return deckState.minionsDeadThisMatch
+			.map((e) => getProcessedCard(e.cardId, e.entityId, deckState, allCards))
+			.filter((c) => hasMechanic(c, GameTag.DEATHRATTLE))
+			.map((e) => e.id)
+			.filter((id) => id != CardIds.WakenerOfSouls_GDB_468);
 	},
 };
