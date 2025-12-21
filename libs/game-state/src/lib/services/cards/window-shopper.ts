3a683ed16b257f904681fe79dfef26c18d9d4e80
sebastientromp <sebastien.tromp@gmail.com>
2025-12-21 19:50:35 +0100

    BUG (tracker): update oracle (Window Shopper)

--- libs/game-state/src/lib/services/cards/window-shopper.ts
+++ libs/game-state/src/lib/services/cards/window-shopper.ts
@@ -7,7 +7,7 @@ export const WindowShopper: GeneratingCard = {
 	publicCreator: true,
 	hasSequenceInfo: true,
 	guessCardId: (input: GuessCardIdInput): string | null => {
-		if (input.createdIndex === 0) {
+		if (input.createdIndex === 1) {
 			return CardIds.WindowShopper_WindowShopperToken_TOY_652t;
 		}
 		return null;
