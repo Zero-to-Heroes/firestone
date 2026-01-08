082f15c5112d2a7307054bf58cc1c46bc22ac476
sebastientromp <sebastien.tromp@gmail.com>
2026-01-08 10:41:51 +0100

    BUG (tracker): fix dynamic pool for Gorgonzormu's Delicious Cheese

--- libs/game-state/src/lib/services/cards/gorgonzormu-delicious-cheese.ts
+++ libs/game-state/src/lib/services/cards/gorgonzormu-delicious-cheese.ts
@@ -1,27 +1,27 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { CardIds, CardType } from '@firestone-hs/reference-data';
-import { GuessedInfo } from '../../models/deck-card';
 import { hasCorrectType } from '../../related-cards/dynamic-pools';
-import { GeneratingCard, GuessInfoInput } from './_card.type';
+import { StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 import { filterCards } from './utils';
 
-export const GorgonzormuDeliciousCheese: GeneratingCard = {
+export const GorgonzormuDeliciousCheese: StaticGeneratingCard = {
 	cardIds: [CardIds.Gorgonzormu_DeliciousCheeseToken_VAC_955t],
 	publicCreator: true,
-	guessInfo: (input: GuessInfoInput): GuessedInfo | null => {
-		const createdAtTurn = input.card.metaInfo?.turnAtWhichCardEnteredHand as number;
-		const turnsInHand = (input.gameState.currentTurn as number) - createdAtTurn;
+	dynamicPool: (input: StaticGeneratingCardInput): readonly string[] => {
+		const card = input.inputOptions?.deckState.findCard(input.entityId)?.card;
+		if (!card) {
+			return [];
+		}
+		const createdAtTurn = card.metaInfo?.turnAtWhichCardEnteredHand as number;
+		const turnsInHand = (input.inputOptions?.gameState?.currentTurn as number) - createdAtTurn;
 		const cheeseCost = Math.min(10, 1 + turnsInHand);
 		const possibleCards = filterCards(
 			GorgonzormuDeliciousCheese.cardIds[0],
 			input.allCards,
 			(c) => c.cost === cheeseCost && hasCorrectType(c, CardType.MINION),
-			input.options,
+			input.inputOptions,
 		);
-		return {
-			cost: cheeseCost,
-			cardType: CardType.MINION,
-			possibleCards: possibleCards,
-		};
+		console.debug('[debug] GorgonzormuDeliciousCheese', possibleCards);
+		return possibleCards;
 	},
 };
