959b53b00a5e4062804ae1ea257578e6b287f18d
sebastientromp <sebastien.tromp@gmail.com>
2026-01-13 07:59:10 +0100

    BUG (tracker): fix Birdwatching oracle being too eager

--- libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/action-chains/birdwatching.ts
+++ libs/legacy/feature-shell/src/lib/js/services/decktracker/event-parser/action-chains/birdwatching.ts
@@ -1,9 +1,9 @@
-import { CardIds } from '@firestone-hs/reference-data';
-import { DeckCard, GameState } from '@firestone/game-state';
-import { GameEvent } from '@legacy-import/src/lib/js/models/game-event';
-import { ActionChainParser } from './_action-chain-parser';
-import { DeckManipulationHelper } from '../deck-manipulation-helper';
+import { CardIds, CardType } from '@firestone-hs/reference-data';
+import { GameState } from '@firestone/game-state';
 import { CardsFacadeService } from '@firestone/shared/framework/core';
+import { GameEvent } from '@legacy-import/src/lib/js/models/game-event';
+import { DeckManipulationHelper } from '../deck-manipulation-helper';
+import { ActionChainParser } from './_action-chain-parser';
 
 export class BirdwatchingParser implements ActionChainParser {
 	constructor(
@@ -28,11 +28,27 @@ export class BirdwatchingParser implements ActionChainParser {
 			return currentState;
 		}
 
+		// Crutch: What happens is:
+		// - you discover something with Birdwatching
+		// - if a minion on board is buffed, we assume that it's because the Birdwatching discovered a copy of that minion
+		// With Parallax Cannon, Discovering is enough to buff the weapon and hero's attack, and that's what trips it.
+		// For now I will do a simple patch to only consider minions and ignore heroes / weapons. If later on there is a minion
+		// that gets buffed on board whenever you discover something, I'll probably have to remove the advanced oracle for Birdwatching
+		const refCard = this.allCards.getCard(attackUpdatedEvent.cardId);
+		if (refCard.type?.toUpperCase() !== CardType[CardType.MINION]) {
+			return currentState;
+		}
+
+		console.debug('[debug] birdwatching parser', attackUpdatedEvent);
 		const deckState = currentState.opponentDeck;
 		const drawnCardInHand = deckState.hand.find(
 			(c) => c.lastAffectedByEntityId === attackUpdatedEvent.additionalData.sourceEntityId,
 		);
-		const refCard = this.allCards.getCard(attackUpdatedEvent.cardId);
+		console.debug(
+			'[debug] birdwatching parser drawnCardInHand',
+			drawnCardInHand,
+			deckState.hand.filter((c) => c.lastAffectedByEntityId === attackUpdatedEvent.additionalData.sourceEntityId),
+		);
 		const updatedCardInHand = drawnCardInHand?.update({
 			cardId: refCard.id,
 			cardName: refCard.name,
