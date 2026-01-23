e2ad2dfd468a910cd0b0a14ff1611c8df1486c19
sebastientromp <sebastien.tromp@gmail.com>
2026-01-23 19:14:20 +0100

    BUG (tracker): maybe fix a case where possible cards don't show up on opponent's hand

--- libs/legacy/feature-shell/src/lib/js/components/overlays/opponenthand/opponent-card-info-id.component.ts
+++ libs/legacy/feature-shell/src/lib/js/components/overlays/opponenthand/opponent-card-info-id.component.ts
@@ -164,8 +164,8 @@ export class OpponentCardInfoIdComponent extends AbstractSubscriptionComponent i
 			cardName: this.cardId === card.cardId ? card.cardName : this.allCards.getCard(this.cardId).name,
 		} as DeckCard);
 		this.guessedInfo = isGuessedInfoEmpty(card.guessedInfo) ? null : card.guessedInfo;
-		if (card.guessedInfo?.possibleCards) {
-			this.possibleCards = [...(card.guessedInfo!.possibleCards ?? [])];
+		if (card.guessedInfo?.possibleCards?.length) {
+			this.possibleCards = [...card.guessedInfo!.possibleCards];
 		} else if (this.forged) {
 			// Build the list of possible card classes based on the card classes in the deck that were part of the initial deck
 			// and the hero classes
