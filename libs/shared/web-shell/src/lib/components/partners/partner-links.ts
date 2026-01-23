1a55d95c9968b5ca2b49d5de7a9e317e772efba5
sebastientromp <sebastien.tromp@gmail.com>
2026-01-23 09:45:12 +0100

    CONTENT (web): update partner links

--- libs/shared/web-shell/src/lib/components/partners/partner-links.ts
+++ libs/shared/web-shell/src/lib/components/partners/partner-links.ts
@@ -6,5 +6,13 @@
  *   https://www.firestoneapp.com/partners/theo -> Full Overwolf URL with tracking params
  */
 export const PARTNER_LINKS: Record<string, string> = {
+	kubu: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=oj0tB87',
 	theo: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=HwUscCQ',
+	babybear: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=lL3QL28',
+	mifundi: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=7jF4jZc',
+	slyders: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=muUVtNG',
+	brigitteous: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=cW2d0oz',
+	jambre: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=8umPZKy',
+	solary: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=0doxm1z',
+	dreads: 'https://www.overwolf.com/app/sebastien_tromp-firestone?utm_source=influencer&utm_medium=owaa&utm_campaign=x1L0WDO',
 };
