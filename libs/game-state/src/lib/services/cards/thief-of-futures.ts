971f69952bc576f643b4355804da6f8a89c120f3
sebastientromp <sebastien.tromp@gmail.com>
2026-01-23 12:20:57 +0100

    CONTENT (tracker): update oracles (Thief of Futures)

--- libs/game-state/src/lib/services/cards/thief-of-futures.ts
+++ libs/game-state/src/lib/services/cards/thief-of-futures.ts
@@ -1,15 +1,19 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 // Thief of Futures
 // Battlecry: Steal the top card of your opponent's deck.
-import { CardIds } from '@firestone-hs/reference-data';
+import { CardIds, GameTag } from '@firestone-hs/reference-data';
 import { GuessedInfo } from '../../models/deck-card';
 import { GeneratingCard, GuessInfoInput } from './_card.type';
 
 export const ThiefOfFutures: GeneratingCard = {
 	cardIds: [CardIds.ThiefOfFutures],
 	publicCreator: true,
+	publicTutor: true,
 	guessInfo: (input: GuessInfoInput): GuessedInfo | null => {
-		const possibleCards = input.opponentDeckState.deck.map((c) => c.cardId).filter((c) => !!c);
+		const possibleCards = input.opponentDeckState.deck.map((c) => c.cardId).filter((c) => !!c)
+			// These are revealed right away
+			.filter(c => !input.allCards.getCard(c).mechanics?.includes(GameTag[GameTag.CASTS_WHEN_DRAWN])
+				&& !input.allCards.getCard(c).mechanics?.includes(GameTag[GameTag.SUMMONED_WHEN_DRAWN]))
 		return {
 			possibleCards,
 		};
