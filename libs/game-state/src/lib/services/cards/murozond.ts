bfb9e6d51332cf4083086b2d229ad57f0d4b1f5e
sebastientromp <sebastien.tromp@gmail.com>
2026-01-23 09:45:03 +0100

    DEV: fix runtime issue on website

--- libs/game-state/src/lib/services/cards/murozond.ts
+++ libs/game-state/src/lib/services/cards/murozond.ts
@@ -3,22 +3,19 @@ import { GuessedInfo } from '../../models/deck-card';
 import { fablePackages } from '../card-utils';
 import { GeneratingCard, GuessInfoInput, StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 
-// All cards from Fabled packages (Fabled minions + their bundled cards)
-const fabledPackagePool: readonly string[] = fablePackages.flat();
-
 // Murozond (TOT_332)
 // "<b>Rewind</b> <b>Battlecry:</b> Get a random <b>Fabled</b> minion and its bundled cards."
 export const Murozond: GeneratingCard & StaticGeneratingCard = {
 	cardIds: [CardIds.Murozond_TOT_332],
 	publicCreator: true,
 	dynamicPool: (_input: StaticGeneratingCardInput) => {
-		return fabledPackagePool;
+		return fablePackages.flat();
 	},
 	guessInfo: (_input: GuessInfoInput): GuessedInfo | null => {
 		return {
 			// No cardType specified since Murozond can generate different types:
 			// the Fabled minion itself + bundled cards (which can be spells, weapons, etc.)
-			possibleCards: fabledPackagePool,
+			possibleCards: fablePackages.flat(),
 		};
 	},
 };
