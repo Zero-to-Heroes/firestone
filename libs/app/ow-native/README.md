cd2c5a0740866acbbebb0b59765361c449c18954
sebastientromp <sebastien.tromp@gmail.com>
2026-01-28 10:22:45 +0100

    DEV: add empty ow-native lib

---
+++ libs/app/ow-native/README.md
@@ -0,0 +1,7 @@
+# ow-native
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ow-native` to execute the unit tests.
---
+++ libs/app/ow-native/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+	displayName: 'ow-native',
+	preset: '../../../jest.preset.js',
+	setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+	coverageDirectory: '../../../coverage/libs/app/ow-native',
+	globals: {
+		'ts-jest': {
+			tsconfig: '<rootDir>/tsconfig.spec.json',
+			stringifyContentPathRegex: '\\.(html|svg)$',
+		},
+	},
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	snapshotSerializers: [
+		'jest-preset-angular/build/serializers/no-ng-attributes',
+		'jest-preset-angular/build/serializers/ng-snapshot',
+		'jest-preset-angular/build/serializers/html-comment',
+	],
+};
