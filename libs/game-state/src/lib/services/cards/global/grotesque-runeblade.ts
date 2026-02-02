1ead98770af2edf59538e206a1ff7af6147d96e9
sebastientromp <sebastien.tromp@gmail.com>
2026-02-02 12:12:27 +0100

    CONTENT (tracker): update pool for Grotesque Runeblade to always show the last played card

--- libs/game-state/src/lib/services/cards/global/grotesque-runeblade.ts
+++ libs/game-state/src/lib/services/cards/global/grotesque-runeblade.ts
@@ -7,12 +7,7 @@ export const GrotesqueRuneblade: GlobalHighlightCard = {
 	cardIds: [CardIds.GrotesqueRuneblade_EDR_812],
 	getRelatedCards: (entityId: number, side: HighlightSide, gameState: GameState, allCards: CardsFacadeService) => {
 		const deckState = side === 'player' ? gameState.playerDeck : gameState.opponentDeck;
-		const card = [...deckState.cardsPlayedThisMatch].reverse().find((c) => hasRune(c, allCards));
+		const card = [...deckState.cardsPlayedThisMatch].reverse()[0];
 		return !!card ? [card.cardId] : null;
 	},
-};
-
-const hasRune = (card: { cardId: string }, allCards: CardsFacadeService): boolean => {
-	const referenceCard = allCards.getCard(card.cardId);
-	return !!referenceCard?.additionalCosts?.BLOODRUNE || !!referenceCard?.additionalCosts?.UNHOLYRUNE;
 };
