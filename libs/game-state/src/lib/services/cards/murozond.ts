b76408ee40f93e3cb7d8e682f594a09218b21147
sebastientromp <sebastien.tromp@gmail.com>
2026-02-27 21:49:54 +0100

    DEV: fix runtime circular dependency

--- libs/game-state/src/lib/services/cards/murozond.ts
+++ libs/game-state/src/lib/services/cards/murozond.ts
@@ -6,21 +6,28 @@ import { GeneratingCard, GuessInfoInput, StaticGeneratingCard, StaticGeneratingC
 // Murozond (TOT_332)
 // "<b>Rewind</b> <b>Battlecry:</b> Get a random <b>Fabled</b> minion and its bundled cards."
 // Garona and Broxigar packages cannot be obtained from Murozond
-const murozondFablePackages = fablePackages
-	.filter((p) => p !== kingLlaneFablePackage && p !== broxigarFablePackage)
-	.flat();
+// Lazy-evaluated to avoid circular dependency: card-utils -> _mapping -> _barrel -> murozond -> card-utils
+let _murozondFablePackages: string[] | null = null;
+const getMurozondFablePackages = (): string[] => {
+	if (!_murozondFablePackages) {
+		_murozondFablePackages = fablePackages
+			.filter((p) => p !== kingLlaneFablePackage && p !== broxigarFablePackage)
+			.flat();
+	}
+	return _murozondFablePackages;
+};
 
 export const Murozond: GeneratingCard & StaticGeneratingCard = {
 	cardIds: [CardIds.Murozond_TOT_332],
 	publicCreator: true,
 	dynamicPool: (_input: StaticGeneratingCardInput) => {
-		return murozondFablePackages;
+		return getMurozondFablePackages();
 	},
 	guessInfo: (_input: GuessInfoInput): GuessedInfo | null => {
 		return {
 			// No cardType specified since Murozond can generate different types:
 			// the Fabled minion itself + bundled cards (which can be spells, weapons, etc.)
-			possibleCards: murozondFablePackages,
+			possibleCards: getMurozondFablePackages(),
 		};
 	},
 };
