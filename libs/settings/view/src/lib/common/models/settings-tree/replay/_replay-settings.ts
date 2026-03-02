c0393f0c66b0d5e26055289a50f84c14a31125f5
sebastientromp <sebastien.tromp@gmail.com>
2026-03-02 13:07:08 +0100

    CONTENT (replay): add info about in-game replays in the settings

--- libs/settings/view/src/lib/common/models/settings-tree/replay/_replay-settings.ts
+++ libs/settings/view/src/lib/common/models/settings-tree/replay/_replay-settings.ts
@@ -1,11 +1,12 @@
 import { SettingContext, SettingNode } from '@firestone/settings/services';
 import { replayGeneralSettings } from './replay-settings-general';
+import { replayInGameSettings } from './replay-settings-in-game';
 
 export const replaySettings = (context: SettingContext): SettingNode => {
 	return {
 		id: 'replay-root',
 		name: context.i18n.translateString('settings.menu.replay'),
 		keywords: null,
-		children: [replayGeneralSettings(context)],
+		children: [replayGeneralSettings(context), replayInGameSettings(context)],
 	};
 };
--- libs/shared/framework/common/src/lib/libs/translation-file-version.ts
+++ libs/shared/framework/common/src/lib/libs/translation-file-version.ts
@@ -1,1 +1,1 @@
-export const translationFileVersion = '1179-main';
+export const translationFileVersion = '1182-main';
