7540559e7f14ef3d2d7290dd8afc58c5e5116c97
sebastientromp <sebastien.tromp@gmail.com>
2026-03-09 09:17:22 +0100

    BUG (tracker): fix info leak for Raza the Resealed

--- libs/game-state/src/lib/services/cards/raza-the-resealed.ts
+++ libs/game-state/src/lib/services/cards/raza-the-resealed.ts
@@ -1,18 +1,10 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { CardIds, CardType, GameTag } from '@firestone-hs/reference-data';
-import { CardsFacadeService } from '@firestone/shared/framework/core';
-import { DeckCard, GuessedInfo } from '../../models/deck-card';
-import { DeckState } from '../../models/deck-state';
-import { Metadata } from '../../models/metadata';
-import { hasCorrectType, hasCost } from '../../related-cards/dynamic-pools';
+import { CardIds } from '@firestone-hs/reference-data';
+import { GuessedInfo } from '../../models/deck-card';
 import { GeneratingCard, GuessInfoInput } from './_card.type';
-import { AllCardsService } from '@firestone-hs/reference-data';
-import { filterCards } from './utils';
 
 export const RazaTheResealed: GeneratingCard = {
 	cardIds: [CardIds.RazaTheResealed_TOY_383],
-	hasSequenceInfo: true,
-	publicCreator: true,
 	guessInfo: (input: GuessInfoInput): GuessedInfo | null => {
 		const possibleCards = input.deckState.minionsDeadThisMatch
 			.map((e) => e.cardId)
