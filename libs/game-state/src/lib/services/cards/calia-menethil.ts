ea05d92a8a15bf01a7a50b582f421289130ef836
sebastientromp <sebastien.tromp@gmail.com>
2026-03-16 10:37:02 +0100

    CONTENT (tracker): fix Calia Menethil dynamic pool

--- libs/game-state/src/lib/services/cards/calia-menethil.ts
+++ libs/game-state/src/lib/services/cards/calia-menethil.ts
@@ -1,17 +1,16 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { CardIds, CardType } from '@firestone-hs/reference-data';
 import { hasCorrectType } from '../../related-cards/dynamic-pools';
+import { getProcessedCard } from '../card-utils';
 import { StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
-import { filterCards } from './utils';
 
 export const CaliaMenethil: StaticGeneratingCard = {
 	cardIds: [CardIds.CaliaMenethil_CORE_CATA_002],
 	dynamicPool: (input: StaticGeneratingCardInput) => {
-		return filterCards(
-			CaliaMenethil.cardIds[0],
-			input.allCards,
-			(c) => hasCorrectType(c, CardType.MINION),
-			input.inputOptions,
-		);
+		const deckState = input.inputOptions.deckState;
+		return deckState.minionsDeadThisMatch
+			.map((e) => getProcessedCard(e.cardId, e.entityId, deckState, input.allCards))
+			.filter((c) => hasCorrectType(c, CardType.MINION))
+			.map((e) => e.id);
 	},
 };
