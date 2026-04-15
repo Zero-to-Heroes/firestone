15655e8db45a5b21a673e8d201d52250d7de4258
sebastientromp <sebastien.tromp@gmail.com>
2026-04-15 11:28:11 +0200

    CONTENT (tracker): fix Glaciate's pool including Colossal minions

--- libs/game-state/src/lib/services/cards/glaciate.ts
+++ libs/game-state/src/lib/services/cards/glaciate.ts
@@ -2,13 +2,14 @@
 // "Discover an 8-Cost minion. Summon and Freeze it."
 
 import { CardIds, CardType, ReferenceCard } from '@firestone-hs/reference-data';
-import { canBeDiscoveredByClass, hasCost, hasCorrectType } from '../../related-cards/dynamic-pools';
+import { canBeDiscoveredByClass, hasCorrectType, hasCost } from '../../related-cards/dynamic-pools';
 import { StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 import { filterCards } from './utils';
 
 export const Glaciate: StaticGeneratingCard = {
 	cardIds: [CardIds.Glaciate, CardIds.Glaciate_CORE_AV_107],
 	publicCreator: true,
+	summonInPlay: true,
 	dynamicPool: (input: StaticGeneratingCardInput) => {
 		return filterCards(
 			Glaciate.cardIds[0],
