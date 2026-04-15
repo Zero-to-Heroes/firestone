201a01aa3aa2c2ab216c0834e23ecc35d44fc31f
sebastientromp <sebastien.tromp@gmail.com>
2026-04-15 14:32:00 +0200

    CONTENT (tracker): fix Soldier of Sinestra's pool

--- libs/game-state/src/lib/services/cards/soldier-of-sinestra.ts
+++ libs/game-state/src/lib/services/cards/soldier-of-sinestra.ts
@@ -1,24 +1,30 @@
-// Soldier of Sinestra (CATA_158t) - Token Minion
-// Created by Maniacal Follower (CATA_158)
-// Battlecry: Summon a random minion that costs (X).
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { CardIds, CardType, GameTag } from '@firestone-hs/reference-data';
-import { hasCorrectType } from '../../related-cards/dynamic-pools';
-import { getEntityTag } from '../../services/parser-entity-utils';
-import { StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
+import { CardIds, CardType } from '@firestone-hs/reference-data';
+import { GuessedInfo } from '../../models/deck-card';
+import { fromAnotherClass, hasCorrectType } from '../../related-cards/dynamic-pools';
+import { GeneratingCard, GuessInfoInput, StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 import { filterCards } from './utils';
 
-export const SoldierOfSinestra: StaticGeneratingCard = {
+export const SoldierOfSinestra: StaticGeneratingCard & GeneratingCard = {
+	publicCreator: true,
 	cardIds: [CardIds.ManiacalFollower_SoldierOfSinestraToken_CATA_158t],
+	guessInfo: (input: GuessInfoInput): GuessedInfo | null => {
+		const possibleCards = filterCards(
+			SoldierOfSinestra.cardIds[0],
+			input.allCards,
+			(c) => hasCorrectType(c, CardType.SPELL) && fromAnotherClass(c, input.deckState.getCurrentClass()),
+			input.options,
+		);
+		return {
+			cardType: CardType.SPELL,
+			possibleCards: possibleCards,
+		};
+	},
 	dynamicPool: (input: StaticGeneratingCardInput) => {
-		const entity = input.entityId
-			? input.inputOptions.gameState.parserState?.CurrentEntities.get(input.entityId)
-			: null;
-		const cost = entity ? getEntityTag(entity, GameTag.TAG_SCRIPT_DATA_NUM_1, 1) : 1;
 		return filterCards(
 			SoldierOfSinestra.cardIds[0],
 			input.allCards,
-			(c) => c.cost === cost && hasCorrectType(c, CardType.MINION),
+			(c) => hasCorrectType(c, CardType.SPELL) && fromAnotherClass(c, input.inputOptions.currentClass),
 			input.inputOptions,
 		);
 	},
