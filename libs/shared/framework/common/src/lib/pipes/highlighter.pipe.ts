7fe14304c84385ccd723ea4c84d8ea986755b148
sebastientromp <sebastien.tromp@gmail.com>
2026-06-02 16:08:26 +0200

    chore: remove verbose log

--- libs/shared/framework/common/src/lib/pipes/highlighter.pipe.ts
+++ libs/shared/framework/common/src/lib/pipes/highlighter.pipe.ts
@@ -6,11 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HighlighterPipe implements PipeTransform {
 	transform(value: any, args: any): unknown {
-		console.debug('highlighter', value, args);
 		if (!args) return value;
 		const re = new RegExp(args, 'igm');
 		value = value.replace(re, '<span class="highlighted-text">$&</span>');
-		console.debug('highlighter return', value, args);
 		return value;
 	}
 }
