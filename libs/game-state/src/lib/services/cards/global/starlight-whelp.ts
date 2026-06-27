87d9392872c25d1e58e22d2b9b3316e60e68d6f3
sebastientromp <sebastien.tromp@gmail.com>
2026-06-27 19:05:29 +0200

    fix (tracker): don't show The Coin as a possible card in Starlight Whelp's pool

--- libs/game-state/src/lib/services/cards/global/starlight-whelp.ts
+++ libs/game-state/src/lib/services/cards/global/starlight-whelp.ts
@@ -9,6 +9,7 @@ export const StarlightWhelp: GlobalHighlightCard = {
 		const deckState = side === 'player' ? gameState.playerDeck : gameState.opponentDeck;
 		return (
 			deckState.cardsInStartingHand
+				?.filter((c) => !allCards.getCard(c.cardId).isCoin)
 				?.map((c) => c.cardId ?? deckState.findCard(c.entityId)?.card?.cardId)
 				// To make sure we get the non-transformed version of the card
 				// TODO: this might still be an issue with transformed cards, eg a card turned into
