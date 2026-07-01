e6df3cbe27af96f2c727d1fd796519fea15ab215
sebastientromp <sebastien.tromp@gmail.com>
2026-07-01 16:35:11 +0200

    change (tracker): add true oracle for Contraband Wands

--- libs/game-state/src/lib/services/cards/contraband-wands.ts
+++ libs/game-state/src/lib/services/cards/contraband-wands.ts
@@ -3,16 +3,12 @@
  * Contraband Wands (JAIL_312)
  * Get 3 Arcane Missiles.
  */
-import { CardIds, CardType } from '@firestone-hs/reference-data';
+import { CardIds } from '@firestone-hs/reference-data';
 
-import { GuessedInfo } from '../../models/deck-card';
-import { GeneratingCard, GuessInfoInput } from './_card.type';
+import { GeneratingCard, GuessCardIdInput } from './_card.type';
 
 export const ContrabandWands: GeneratingCard = {
 	cardIds: [CardIds.ContrabandWands_JAIL_312],
 	publicCreator: true,
-	guessInfo: (_input: GuessInfoInput): GuessedInfo | null => ({
-		cardType: CardType.SPELL,
-		possibleCards: [CardIds.ArcaneMissilesLegacy],
-	}),
+	guessCardId: (_input: GuessCardIdInput): string | null => CardIds.ArcaneMissilesLegacy,
 };
