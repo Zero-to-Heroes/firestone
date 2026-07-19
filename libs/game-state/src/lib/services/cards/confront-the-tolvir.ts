d4f73804fad7f2556c6a160ce67347bedbeefa0c
sebastientromp <sebastien.tromp@gmail.com>
2026-07-19 18:43:14 +0200

    fix (tracker): Confront the Tol'Vir now uses base cost for its dynamic pool

--- libs/game-state/src/lib/services/cards/confront-the-tolvir.ts
+++ libs/game-state/src/lib/services/cards/confront-the-tolvir.ts
@@ -1,23 +1,12 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { CardIds } from '@firestone-hs/reference-data';
-import { CardsFacadeService } from '@firestone/shared/framework/core';
-import { getCost } from '../card-utils';
 import { StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
 
 export const ConfrontTheTolvir: StaticGeneratingCard = {
 	cardIds: [CardIds.ConfrontTheTolvir_CATA_560],
 	dynamicPool: (input: StaticGeneratingCardInput) => {
 		return input.inputOptions.deckState.cardsPlayedThisMatch
-			.filter((c) => {
-				const deckCard = input.inputOptions.deckState.findCard(c.entityId)?.card;
-				if (!deckCard) {
-					return input.allCards.getCard(c.cardId)?.cost === 1;
-				}
-				return (
-					getCost(deckCard, input.inputOptions.deckState, input.allCards as unknown as CardsFacadeService) ===
-					1
-				);
-			})
+			.filter((c) => input.allCards.getCard(c.cardId)?.cost === 1)
 			.map((c) => c.cardId);
 	},
 };
