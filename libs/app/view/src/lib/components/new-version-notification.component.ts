3865dda3376b402b9de8d7334cbdb43925df768b
sebastientromp <sebastien.tromp@gmail.com>
2026-07-20 18:17:42 +0200

    RELEASE: 18.12.0

--- libs/app/view/src/lib/components/new-version-notification.component.ts
+++ libs/app/view/src/lib/components/new-version-notification.component.ts
@@ -24,6 +24,7 @@ import { BehaviorSubject, debounceTime, distinctUntilChanged, map, Observable, S
 import { AppVersion } from '../model/app-version';
 
 const versions: readonly AppVersion[] = [
+	{ version: '18.12.0', date: '2026-07-20', force: true },
 	{ version: '18.11.5', date: '2026-07-17', force: true },
 	{ version: '18.11.0', date: '2026-07-15', force: true },
 	{ version: '18.10.1', date: '2026-06-30' },
