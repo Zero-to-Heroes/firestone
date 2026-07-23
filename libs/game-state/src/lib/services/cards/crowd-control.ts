be92b8202e5ebd9cbc5b44a3a82370b692debe2d
sebastientromp <sebastien.tromp@gmail.com>
2026-07-23 09:18:49 +0200

    fix (tracker): remove highlights for Crowd Control

--- libs/game-state/src/lib/services/cards/crowd-control.ts
+++ libs/game-state/src/lib/services/cards/crowd-control.ts
@@ -4,11 +4,9 @@
  * Deal 2 damage to all minions. If your deck has 25 or more cards, deal 2 more.
  */
 import { CardIds } from '@firestone-hs/reference-data';
-import { and, inDeck, side } from '../card-highlight/selectors';
 
-import { Card, SelectorCard } from './_card.type';
+import { Card } from './_card.type';
 
-export const CrowdControl: SelectorCard = {
+export const CrowdControl: Card = {
 	cardIds: [CardIds.CrowdControl_JAIL_307],
-	selector: (inputSide) => and(side(inputSide), inDeck),
 };
