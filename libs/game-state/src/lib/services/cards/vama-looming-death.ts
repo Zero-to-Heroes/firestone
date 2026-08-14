14e4777c2b330ed8b9f34b7a8dbb50fe57c511f6
sebastientromp <sebastien.tromp@gmail.com>
2026-08-14 22:51:58 +0200

    change (tracker): Vama Looming Death now also highlights minions on the opponent's board

--- libs/game-state/src/lib/services/cards/vama-looming-death.ts
+++ libs/game-state/src/lib/services/cards/vama-looming-death.ts
@@ -5,10 +5,25 @@
  */
 import { CardIds } from '@firestone-hs/reference-data';
 
-import { and, inDeck, inHand, inPlay, minion, not, or, paladin, side } from '../card-highlight/selectors';
+import {
+	and,
+	highlightConditions,
+	inDeck,
+	inHand,
+	inPlay,
+	minion,
+	not,
+	or,
+	paladin,
+	side,
+} from '../card-highlight/selectors';
 import { SelectorCard } from './_card.type';
 
 export const VamaLoomingDeath: SelectorCard = {
 	cardIds: [CardIds.VamaLoomingDeath_JAIL_118],
-	selector: (inputSide) => and(side(inputSide), or(inHand, inDeck, inPlay), minion, not(paladin)),
+	selector: (inputSide) =>
+		highlightConditions(
+			and(inPlay, minion, not(paladin)),
+			and(side(inputSide), or(inHand, inDeck), minion, not(paladin)),
+		),
 };
