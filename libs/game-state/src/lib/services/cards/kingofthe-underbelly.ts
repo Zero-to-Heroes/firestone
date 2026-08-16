23f819ad999ad40af637209e427471572cc8251b
sebastientromp <sebastien.tromp@gmail.com>
2026-08-16 08:41:26 +0200

    new (tracker): correctly show the sideboard when mousing over King of the Underbelly

--- libs/game-state/src/lib/services/cards/kingofthe-underbelly.ts
+++ libs/game-state/src/lib/services/cards/kingofthe-underbelly.ts
@@ -1,51 +1,41 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /**
- * Kingofthe Underbelly (JAIL_831)
+ * King of the Underbelly (JAIL_831)
  * While building your deck, pick 3 contraband Beasts. Battlecry: Discover one. It costs (3) less.
  */
-import { CardClass, CardIds, CardType, hasCorrectTribe, isArena, Race } from '@firestone-hs/reference-data';
+import { CardIds, CardType, isArena, Race } from '@firestone-hs/reference-data';
 
 import { GuessedInfo } from '../../models/deck-card';
-import { canBeDiscoveredByClass, hasCorrectClass, hasCorrectType } from '../../related-cards/dynamic-pools';
 import { GeneratingCard, GuessInfoInput, StaticGeneratingCard, StaticGeneratingCardInput } from './_card.type';
-import { filterCards } from './utils';
 
-const beastDiscoverFilter = (
-	c: Parameters<typeof hasCorrectType>[0],
-	currentClass: Parameters<typeof canBeDiscoveredByClass>[1],
-) =>
-	hasCorrectType(c, CardType.MINION) &&
-	hasCorrectTribe(c, Race.BEAST) &&
-	(!currentClass || !hasCorrectClass(c, CardClass[currentClass]));
+const ARENA_BEASTS = [CardIds.Ursol_EDR_259, CardIds.TheGreatDracorex_DINO_401, CardIds.Tortolla_EDR_471];
 
 export const KingoftheUnderbelly: GeneratingCard & StaticGeneratingCard = {
 	cardIds: [CardIds.KingoftheUnderbelly_JAIL_831],
 	publicCreator: true,
 	dynamicPool: (input: StaticGeneratingCardInput) => {
 		if (isArena(input.inputOptions.gameState.metadata.gameType)) {
-			return [CardIds.Ursol_EDR_259, CardIds.TheGreatDracorex_DINO_401, CardIds.Tortolla_EDR_471];
+			return ARENA_BEASTS;
 		}
-		return filterCards(
-			CardIds.KingoftheUnderbelly_JAIL_831,
-			input.allCards,
-			(c) => beastDiscoverFilter(c, input.inputOptions.deckState.getCurrentClass()),
-			input.inputOptions,
+		return (
+			input.inputOptions.deckState.sideboards?.find((s) => s.keyCardId === CardIds.KingoftheUnderbelly_JAIL_831)
+				?.cards ?? []
 		);
 	},
 	guessInfo: (input: GuessInfoInput): GuessedInfo | null => {
-		let possibleCards = filterCards(
-			CardIds.KingoftheUnderbelly_JAIL_831,
-			input.allCards,
-			(c) => beastDiscoverFilter(c, input.deckState.getCurrentClass()),
-			input.options,
-		);
 		if (isArena(input.gameState.metadata.gameType)) {
-			possibleCards = [CardIds.Ursol_EDR_259, CardIds.TheGreatDracorex_DINO_401, CardIds.Tortolla_EDR_471];
+			return {
+				cardType: CardType.MINION,
+				races: [Race.BEAST],
+				possibleCards: ARENA_BEASTS,
+			};
 		}
+		const possibleCards =
+			input.deckState.sideboards?.find((s) => s.keyCardId === CardIds.KingoftheUnderbelly_JAIL_831)?.cards ?? [];
 		return {
 			cardType: CardType.MINION,
 			races: [Race.BEAST],
-			possibleCards: possibleCards,
+			...(possibleCards.length ? { possibleCards } : {}),
 		};
 	},
 };
